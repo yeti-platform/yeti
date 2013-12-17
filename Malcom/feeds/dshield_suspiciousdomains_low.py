de1745accee7e2d7f61b3e676d0de9668f59c09d
Jipe <jipedevs@gmail.com>
2013-12-17 23:55:25 +0100

    New feeds
    New feed from Malwaredomains
    Dshield’s feeds splitted by severity

--- Malcom/feeds/dshield_suspiciousdomains_low.py
+++ Malcom/feeds/dshield_suspiciousdomains_low.py
@@ -1,20 +1,16 @@
 import urllib2
-from Malcom.model.datatypes import Hostname
+from Malcom.model.datatypes import Ip 
 from feed import Feed
 import Malcom.auxiliary.toolbox as toolbox
 
-class SuspiciousDomains(Feed):
-	"""
-	This gets data from https://isc.sans.edu/suspicious_domains.html
-	Sensitivity level: high (for now)
-	"""
+class DShieldSuspiciousDomainsLow(Feed):
 	def __init__(self, name):
-		super(SuspiciousDomains, self).__init__(name)
+		super(DShieldSuspiciousDomainsLow, self).__init__(name)
 		self.enabled = True
 
 	def update(self):
 		try:
-			feed = urllib2.urlopen("https://isc.sans.edu/feeds/suspiciousdomains_High.txt").readlines()
+			feed = urllib2.urlopen("http://www.dshield.org/feeds/suspiciousdomains_Low.txt").readlines()
 			self.status = "OK"
 		except Exception, e:
 			self.status = "ERROR: " + str(e)
@@ -31,12 +27,10 @@ class SuspiciousDomains(Feed):
 		try:
 			hostname = toolbox.find_hostnames(line)[0]
 		except Exception, e:
-			# if find_hostname raises an exception, it means no hostname
-			# was found in the line, so we return
 			return
 
-		# Create the new URL and store it in the DB
-		hostname = Hostname(hostname=hostname, tags=['SuspiciousDomains', 'evil'])
+		# Create the new ip and store it in the DB
+		hostname = Hostname(hostname=hostname, tags=['dshield', 'low'])
 
 		hostname, status = self.analytics.save_element(hostname, with_status=True)
 		if status['updatedExisting'] == False:
---
+++ Malcom/feeds/dshield_suspiciousdomains_medium.py
@@ -0,0 +1,39 @@
+import urllib2
+from Malcom.model.datatypes import Ip 
+from feed import Feed
+import Malcom.auxiliary.toolbox as toolbox
+
+class DShieldSuspiciousDomainsMedium(Feed):
+	def __init__(self, name):
+		super(DShieldSuspiciousDomainsMedium, self).__init__(name)
+		self.enabled = True
+
+	def update(self):
+		try:
+			feed = urllib2.urlopen("http://www.dshield.org/feeds/suspiciousdomains_Medium.txt").readlines()
+			self.status = "OK"
+		except Exception, e:
+			self.status = "ERROR: " + str(e)
+			return False
+		
+		for line in feed:	
+			self.analyze(line)
+		return True
+
+	def analyze(self, line):
+		if line.startswith('#') or line.startswith('\n'):
+			return
+
+		try:
+			hostname = toolbox.find_hostnames(line)[0]
+		except Exception, e:
+			return
+
+		# Create the new ip and store it in the DB
+		hostname = Hostname(hostname=hostname, tags=['dshield', 'medium'])
+
+		hostname, status = self.analytics.save_element(hostname, with_status=True)
+		if status['updatedExisting'] == False:
+			self.elements_fetched += 1
+
+
