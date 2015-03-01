d6842dd95c1970ecdf9c2b46514eed563ead04ac
Thomas Chopitea <tomchop@gmail.com>
2015-03-01 14:57:25 +0100

    Changed function locations
    Now filenames make more sense

--- Malcom/web/static/custom_js/d3_functions.js
+++ Malcom/web/static/custom_js/d3_functions.js
@@ -307,9 +307,7 @@ function uncomm_all() {
   highlighted_nodes = node.classed('comms', false)
 }
 
-
 comm_intervals = {}
-
 
 function start() {
 
@@ -373,7 +371,7 @@ function start() {
     //   if (d.attribs == '') { return "#AAA" } else {return color(d.attribs);}
     // })
 
-  // text on links
+    // text on links
 
    path_text = svg.selectAll(".path-text").data(force.links(), function (d) { return d._id.$oid });
    if (link_labels) {
@@ -382,9 +380,6 @@ function start() {
    else {
     path_text.remove()
    }
-
-
-
 
   // l = link.enter().append('svg:g').attr('class', 'link-container')
   // lines = l.append('line', '.node') .attr("class", "link")
@@ -406,8 +401,6 @@ function start() {
         .on("drag", dragmove)
         .on("dragend", dragend);
 
-
-
   // create new svg:g
   n = node.enter().append("svg:g")
     .attr("class", "node").call(node_drag)
@@ -425,7 +418,7 @@ function start() {
                 d.radius = r
                 return r;
               })
-     .attr('class', function (d) { 
+     .attr('class', function (d) {
       console.log(d)
         c = d.type
         for (var i in d.tags) { c += ' ' + d.tags[i] }
@@ -500,7 +493,6 @@ function tick(e) {
 
 }
 
-
 function linkArc(d) {
   var dx = d.target.x - d.source.x,
       dy = d.target.y - d.source.y,
@@ -513,4 +505,105 @@ function linkArc(d) {
         return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y
       }
 
+}
+
+function highlight_query(query) {
+
+  query = query.trim()
+
+  if (query == 'clear') {
+    node.classed('dim', false)
+    link.classed('dim', false)
+    path_text.classed('dim', false)
+    return
+  }
+
+  node.classed('dim', true)
+  link.classed('dim', true)
+  path_text.classed('dim', true)
+
+  if (query.search(':') != -1) {
+    args = query.split(':')
+    query = "(d." + args[0] + ".search('"+args[1]+"') != -1)"
+  }
+  else {
+    query = "(d.value.search('"+query+"') != -1)"
+  }
+  change_opacity(query)
+}
+
+function change_opacity(query) {
+
+  ids = []
+
+  hl = node.filter(function (d, i) {
+        if (eval(query)) {
+          ids.push(d._id.$oid)
+          return true;
+        }
+        else
+          return false;
+      })
+      .classed('dim', false)[0]
+
+  other_ids = []
+
+  hl = link.filter(function (d, i) {
+        if ((ids.indexOf(d.src.$oid) != -1) || (ids.indexOf(d.dst.$oid) != -1)) {
+          other_ids.push(d.src.$oid)
+          other_ids.push(d.dst.$oid)
+          return true
+        }
+      })
+      .classed('dim', false)[0]
+
+  node.filter(function (d, i) {
+    if (other_ids.indexOf(d._id.$oid) != -1)
+      return true
+    else
+      return false
+  }).classed('dim', false)
+}
+
+
+function display_data(d)
+{
+  console.log("Displayed data: "+d)
+  $('#node_info').empty();
+  $(".whois").empty();
+
+  display_generic(d);
+}
+
+function display_generic(d) {
+  if (d.fields != undefined) {
+    for (var display in d.fields) {
+      key = d.fields[display][0]
+      label = d.fields[display][1]
+      if (d[key] == undefined)
+        value = "N/A"
+      else {
+        value = d[key]
+
+        if (['date_updated', 'date_created', 'last_analysis'].indexOf(key) != -1)
+          value = format_date(new Date(value.$date))
+        if (key == 'tags')
+          if (d.tags.length == 0)
+            value = '-'
+          else
+            value = d.tags.join(', ')
+      }
+
+      $("#node_info").append("<tr><th>"+label+"</th><td>"+value+"</td></tr>");
+    }
+  }
+  else {
+    for (var key in d) {
+      if (['fixed', 'selected', 'previouslyselected', 'type', 'tags', 'date_created', 'date_retreived', 'date_updated', 'last_analysis', "_id", "group", "incoming_links", "index", "px", "py", "x", "y", "radius", 'weight'].indexOf(key.toLowerCase()) == -1) {
+        val = d[key]
+        if (val == undefined) { val = "N/A"}
+        $("#node_info").append("<tr><th>"+key.charAt(0).toUpperCase() + key.slice(1) +"</th><td>"+val+"</td></tr>");
+      }
+    }
+  }
 }
--- Malcom/web/static/custom_js/data.js
+++ Malcom/web/static/custom_js/data.js
@@ -1,133 +1,168 @@
-
-function highlight_query(query) {
-
-	query = query.trim()
-
-	if (query == 'clear') {
-		node.classed('dim', false)
-		link.classed('dim', false)
-		path_text.classed('dim', false)
-		return
-	}
-
-	node.classed('dim', true)
-	link.classed('dim', true)
-	path_text.classed('dim', true)
-
-	if (query.search(':') != -1) {
-		args = query.split(':')
-		query = "(d." + args[0] + ".search('"+args[1]+"') != -1)"
-	}
-	else {
-		query = "(d.value.search('"+query+"') != -1)"
-
-	}
-
-	change_opacity(query)
-
+function clear_db() {
+	var r=confirm("You sure?");
+	if (r==true)
+	  {
+	 	document.location.href=url_static_prefix+'dataset/clear';
+	  }
 }
 
-function change_opacity(query) {
-	
-	ids = []
-	
-	hl = node.filter(function (d, i) { 
-				if (eval(query)) {
-					ids.push(d._id.$oid)
-					return true;
-				}
-				else
-					return false;
-			})
-			.classed('dim', false)[0]
-
-	other_ids = []
-
-	hl = link.filter(function (d, i) {
-				if ((ids.indexOf(d.src.$oid) != -1) || (ids.indexOf(d.dst.$oid) != -1)) {
-					other_ids.push(d.src.$oid)
-					other_ids.push(d.dst.$oid)
-					return true
-				}
-			})
-			.classed('dim', false)[0]
-
-	node.filter(function (d, i) {
-		if (other_ids.indexOf(d._id.$oid) != -1)
-			return true
-		else
-			return false
-	}).classed('dim', false)
-}
-
-
-
-function display_data(d)
-{
-	console.log("Displayed data: "+d)
-	$('#node_info').empty();
-	$(".whois").empty();
-
-	display_generic(d);
-}
-
-function display_generic(d) {
-	if (d.fields != undefined) {
-		for (var display in d.fields) {
-			key = d.fields[display][0]
-			label = d.fields[display][1]
-			if (d[key] == undefined)
-				value = "N/A"
-			else {
-				value = d[key]
-				
-				if (['date_updated', 'date_created', 'last_analysis'].indexOf(key) != -1)
-					value = format_date(new Date(value.$date))
-				if (key == 'tags')
-					if (d.tags.length == 0) 
-						value = '-'
-					else
-						value = d.tags.join(', ')
-			}
-
-			$("#node_info").append("<tr><th>"+label+"</th><td>"+value+"</td></tr>");	
+function dataset_remove(id) {
+	$.ajax({
+		dataType: "json",
+		url: url_static_prefix+'api/dataset/remove/'+id,
+		success:function(data){
+			if (data['n'] == 1)
+				$('#row_'+id).remove()
+			else
+				console.log("ERROR: Could not remove element "+id)
 		}
-	}
-	else {
-		for (var key in d) {
-			if (['fixed', 'selected', 'previouslyselected', 'type', 'tags', 'date_created', 'date_retreived', 'date_updated', 'last_analysis', "_id", "group", "incoming_links", "index", "px", "py", "x", "y", "radius", 'weight'].indexOf(key.toLowerCase()) == -1) {
-				val = d[key]
-				if (val == undefined) { val = "N/A"}
-				$("#node_info").append("<tr><th>"+key.charAt(0).toUpperCase() + key.slice(1) +"</th><td>"+val+"</td></tr>");
-			}
-		}
-	}
+	});
 }
 
+function get_dataset_csv(query, url) {
+	queries = query.split(' ');
+	querydict = {};
 
-function format_date(date, millis)
-{
-	// hours part from the timestamp
-	var hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
-	// minutes part from the timestamp
-	var minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
-	// seconds part from the timestamp
-	var seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
-	if (millis == true) {
-		m = date.getMilliseconds()
-		if (m < 10)
-			m = "00"+m
-		if (m > 10 && m < 100)
-			m = "0"+m
-		seconds += "." + m
+	for (var i in queries) {
+		splitted = queries[i].split('=')
+		if (splitted.length > 1)
+			querydict[splitted[0]] = splitted[1];
+		else if (splitted[0] != "")
+			querydict['value'] = splitted[0]
 	}
 
-	var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
-	var month = date.getMonth()+1 < 10 ? '0' + (date.getMonth()+1) : (date.getMonth()+1)
-	var year = date.getFullYear();
+	if ($('#fuzzy').prop('checked') == true)
+		querydict['fuzzy'] = 'true'
 
-	// will display time in 10:30:23 format
-	var formattedTime = year+"-"+month+"-"+day +" ("+hours + ':' + minutes + ':' + seconds+")";
+	url = url_static_prefix + url +"?"+ $.param(querydict)
 
-	return formattedTime;
+	location.href = url
+}
+
+function change_page(arg, url) {
+	console.log('Loading page ' + arg)
+	query = $('#query').val()
+	location.hash = arg
+	get_dataset(query, url)
+}
+
+function get_dataset(query, url) {
+
+	queries = query.split(' ');
+	querydict = {};
+
+	for (var i in queries) {
+		splitted = queries[i].split('=')
+		if (splitted.length > 1)
+			querydict[splitted[0]] = splitted[1];
+		else if (splitted[0] != "")
+			querydict['value'] = splitted[0]
+	}
+
+	// un # dans l'url
+	page = location.hash.split("#")[1]
+
+	if (page == undefined) {
+		page = 0;
+	}
+
+	params = {}
+	querydict['page'] = page
+
+	if ($('#fuzzy').prop('checked') == true)
+		querydict['fuzzy'] = 'true'
+
+	$.ajax({
+	  dataType: "json",
+	  type: 'GET',
+	  url: url,
+	  data: $.param(querydict),
+	  beforeSend: function(data) {
+	  	$('#loading-spinner').addClass('show')
+	  },
+	  complete: function(data) {
+	  	$('#loading-spinner').removeClass('show')
+	  },
+	  success: function(data){
+	  	// empty the table and populate it
+	  	dataset = $('#dataset')
+	  	dataset.empty()
+	  	head = $("<tr>")
+	  	// get the headers
+	  	for (var i in data.fields) {
+	  		h = $("<th>").text(data.fields[i][1])
+	  		if (['date_created', 'date_updated', 'last_analysis'].indexOf(data.fields[i][0]) != -1)
+	  			h.addClass('timestamp')
+	  		head.append(h)
+	  	}
+
+	  	head.append($("<th>").text(''))
+
+	  	dataset.append(head)
+
+	  	// loop over the elements
+
+	  	for (var i in data.elements) {
+	  		elt = data.elements[i]
+	  		tags_links = new Array()
+	  		//create row
+	  		row = $("<tr id='row_"+elt['_id']['$oid']+"'></tr>")
+	  		for (var key in data.fields) {
+	  				k = data.fields[key][0]
+	  				v = elt[k]
+	  				if (v == "" || v == undefined)
+	  					row.append($("<td />").text('-'))
+	  				else if (k == 'tags')
+	  					row.append($("<td />").addClass('tags_links'))
+	  				else if (['date_created', 'date_updated', 'last_analysis'].indexOf(k) != -1)
+	  					row.append($("<td />").text(format_date(new Date(v.$date))).addClass('timestamp'))
+	  				else
+	  					row.append("<td><a href='"+url_static_prefix+"nodes/"+k+"/"+encodeURIComponent(v)+"'>"+v+"</a></td>")
+	  		}
+
+	  		row.append("<td><span class='glyphicon glyphicon-remove' onclick='javascript:dataset_remove(\""+elt['_id']['$oid']+"\")'></span></td>")
+
+	  		tags_links = row.find('.tags_links')
+
+	  		for (var c in elt['tags']) {
+	  			a = $('<a>')
+	  			a.attr('href', url_static_prefix+'nodes/tags/'+elt['tags'][c])
+	  			a.text(elt['tags'][c])
+	  			if (c != 0)
+	  				tags_links.append(', ')
+	  			tags_links.append(a)
+	  		}
+
+	  		// append the created row to the table
+	  		dataset.append(row)
+	  	}
+
+	  	// adjust pagination
+	  	per_page = 50;
+	  	if (data.total_results != 'many')	{
+		  	total_pages = Math.floor(data.total_results / per_page) + 1;
+		  	next_page = (page*1+1) <= total_pages ? page*1 + 1 : page*1;
+	  	}
+	  	else {
+	  		total_pages = 'many';
+	  		next_page = page*1+1;
+	  	}
+	  	previous_page = page - 1 >= 0 ? page - 1 : 0;
+
+
+	  	prev = $("#pagination-prev")
+	  	next = $("#pagination-next")
+	  	$("#pagination-page").text("Page "+(page*1+1)+" of "+ (total_pages))
+	  	$("#total-results").text(data.total_results)
+
+	  	prev.attr('data-nav', previous_page)
+	  	next.attr('data-nav', next_page)
+	  }
+	});
+}
+
+function real_time(url, key) {
+	if (!$('#fuzzy').prop('checked') || key == 13) {
+		get_dataset($('#query').val(), url)
+	}
 }
