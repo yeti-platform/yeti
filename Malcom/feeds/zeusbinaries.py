4dc950ca352a53a9f8ceaeec43bacd67d4a3ccee
Thomas Chopitea <tomchop@gmail.com>
2015-05-22 09:32:52 +0200

    Convert indentation to spaces

--- Malcom/feeds/zeusbinaries.py
+++ Malcom/feeds/zeusbinaries.py
@@ -13,27 +13,27 @@ import Malcom.auxiliary.toolbox as toolbox
 
 class ZeusTrackerBinaries(Feed):
 
-	def __init__(self, name):
-		super(ZeusTrackerBinaries, self).__init__(name)
-		self.name = "ZeusTrackerBinaries"
-		self.source = "https://zeustracker.abuse.ch/monitor.php?urlfeed=binaries"
-		self.description = "This feed shows the latest 50 ZeuS binary URLs."
-		
-	def update(self):
-		for dict in self.update_xml('item', ["title", "link", "description", "guid"]):
-			self.analyze(dict)
+    def __init__(self, name):
+        super(ZeusTrackerBinaries, self).__init__(name)
+        self.name = "ZeusTrackerBinaries"
+        self.source = "https://zeustracker.abuse.ch/monitor.php?urlfeed=binaries"
+        self.description = "This feed shows the latest 50 ZeuS binary URLs."
 
-	def analyze(self, dict):
-		evil = dict
+    def update(self):
+        for dict in self.update_xml('item', ["title", "link", "description", "guid"]):
+            self.analyze(dict)
 
-		url = Url(re.search("URL: (?P<url>\S+),", dict['description']).group('url'))
-		evil['id'] = md5.new(re.search(r"id=(?P<id>[a-f0-9]+)", dict['guid']).group('id')).hexdigest()
-		
-		try:
-			date_string = re.search(r"\((?P<date>[0-9\-]+)\)", dict['title']).group('date')
-			evil['date_added'] = datetime.datetime.strptime(date_string, "%Y-%m-%d")
-		except AttributeError, e:
-			print "Date not found!"
-		
-		url.add_evil(evil)
-		self.commit_to_db(url)
+    def analyze(self, dict):
+        evil = dict
+
+        url = Url(re.search("URL: (?P<url>\S+),", dict['description']).group('url'))
+        evil['id'] = md5.new(re.search(r"id=(?P<id>[a-f0-9]+)", dict['guid']).group('id')).hexdigest()
+
+        try:
+            date_string = re.search(r"\((?P<date>[0-9\-]+)\)", dict['title']).group('date')
+            evil['date_added'] = datetime.datetime.strptime(date_string, "%Y-%m-%d")
+        except AttributeError, e:
+            print "Date not found!"
+
+        url.add_evil(evil)
+        self.commit_to_db(url)
