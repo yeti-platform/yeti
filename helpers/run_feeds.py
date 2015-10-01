c555c53290c8894c80dc7991081dd5d7591fda8c
Thomas Chopitea <tomchop@gmail.com>
2015-10-01 11:34:12 +0200

    Add argument to run single feed

--- helpers/run_feeds.py
+++ helpers/run_feeds.py
@@ -1,9 +1,17 @@
+import sys
+
 from core.feed import Feed
 import core.config.celeryimports
 
 
 if __name__ == '__main__':
-    all_feeds = Feed.objects()
+
+    if len(sys.argv) == 1:
+        all_feeds = Feed.objects()
+    elif len(sys.argv) >= 2:
+        all_feeds = [Feed.objects.get(name=sys.argv[1])]
+
+    print all_feeds
     for n in all_feeds:
         print "Testing: {}".format(n)
         n.update()
