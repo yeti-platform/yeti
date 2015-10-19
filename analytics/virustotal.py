cba8ef4aa4eeb2902e44e6f1114394ac8002e335
Thomas Chopitea <tomchop@gmail.com>
2015-10-19 21:20:37 +0200

    Virustotal OneShotAnalytics
    Dummy, does nothing

---
+++ analytics/virustotal.py
@@ -0,0 +1,18 @@
+import logging
+
+from core.analytics import OneShotAnalytics
+
+
+class QueryVirusTotal(OneShotAnalytics):
+
+    settings = {
+        "name": "QueryVirusTotal",
+        "description": "Queries VT for a single hash, adding context to the element "
+                       "and linking to other elements"
+    }
+
+    ACTS_ON = ["Hash"]
+
+    @staticmethod
+    def analyze(hash):
+        logging.info("Querying hash {} on VT".format(hash))
