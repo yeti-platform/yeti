00f83dad3a0cec2bccb4de878b477bbcf850e52d
Thomas Chopitea <tomchop@gmail.com>
2015-10-19 21:20:18 +0200

    Raise exception on invalid URL

--- core/datatypes/url.py
+++ core/datatypes/url.py
@@ -11,8 +11,6 @@ class Url(Element):
     def clean(self):
         """Ensures that URLs are canonized before saving"""
         try:
-            if not is_url(self.value):
-                raise ValidationError("Invalid URL (is_url={}): {}".format(is_url(self.value), self.value))
             if re.match("[a-zA-Z]+://", self.value) is None:
                 self.value = "http://{}".format(self.value)
             self.value = urlnorm.norm(self.value)
