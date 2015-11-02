971fdb2257bee43981a81a9402bc80a53d9a06bb
Thomas Chopitea <tomchop@gmail.com>
2015-11-02 18:18:52 +0100

    Merged Link and Element
    (Cyclic import error)

--- core/datatypes/__init__.py
+++ core/datatypes/__init__.py
@@ -3,8 +3,7 @@ from mongoengine import *
 connect('malcom-v2')
 
 from core.datatypes.tag import Tag
-from core.datatypes.element import Element
-from core.datatypes.link import LinkHistory, Link
+from core.datatypes.element import Element, LinkHistory, Link
 from core.datatypes.ip import Ip
 from core.datatypes.url import Url
 from core.datatypes.hostname import Hostname
