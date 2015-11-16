d6055fe2254ca5f546de3304b48733c6fa236942
Thomas Chopitea <tomchop@gmail.com>
2015-11-16 18:46:06 +0100

    Work on API

--- core/web/api/api.py
+++ core/web/api/api.py
@@ -1,37 +1,44 @@
-from flask import Blueprint, request, jsonify
-from flask_negotiate import consumes, produces
+from flask import Blueprint, request, jsonify, render_template
+from flask.ext.negotiation import Render
+from flask.ext.negotiation.renderers import renderer, template_renderer, json_renderer
 
 from core.entities import Entity
 from core.observables import Observable
 from core.indicators import Indicator
 
-api = Blueprint('api', __name__)
+api = Blueprint("api", __name__, template_folder="templates")
 
+render = Render(renderers=[template_renderer, json_renderer])
 
 @api.route("/")
 def index():
     return "API-OK\n"
 
-
 @api.route("/observables", methods=["POST"])
-# @consumes("application/json")
-# @produces("application/json")
 def observables():
     q = request.json
 
-    matches = {}
-    for o in q['observables']:
-        matches[o] = {}
+    data = {"matches": [], "unknown": []}
+
+    for o in q["observables"]:
+        found = False
         for i in Indicator.objects():
             if i.match(o):
+                found = True
+                match = i.info()
+                match['observable'] = o
+                match['related'] = []
                 for type, nodes in i.neighbors().items():
                     for l, node in nodes:
-                        indicator = node.info()
+                        node_data = node.info()
                         if l.description:
-                            indicator['link_description'] = l.description
+                            node_data["link_description"] = l.description
                         else:
-                            indicator['link_description'] = l.tag
-                        indicator["entity"] = type
-                        matches[o][i.name] = matches[o].get(i.name, []) + [indicator]
+                            node_data["link_description"] = l.tag
+                        node_data["entity"] = type
+                        match['related'].append(node_data)
+                data["matches"].append(match)
+        if not found:
+            data["unknown"].append(o)
 
-    return jsonify({"matches": matches})
+    return render(data, "observables.html")
