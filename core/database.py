6ed68c23348c4ac3ae9c02d29d137861974c0843
Thomas Chopitea <tomchop@gmail.com>
2015-12-09 16:42:29 +0100

    Bugfixes

--- core/database.py
+++ core/database.py
@@ -1,6 +1,67 @@
 from datetime import datetime
 
 from mongoengine import *
+from mongoengine import Q
+
+
+class LinkHistory(EmbeddedDocument):
+
+    tag = StringField()
+    description = StringField()
+    first_seen = DateTimeField(default=datetime.now)
+    last_seen = DateTimeField(default=datetime.now)
+
+
+class Link(Document):
+
+    src = ReferenceField("Node", required=True)
+    dst = ReferenceField("Node", required=True, unique_with='src')
+    history = SortedListField(EmbeddedDocumentField(LinkHistory), ordering='last_seen', reverse=True)
+
+    def __unicode__(self):
+        return u"{} {} {} ({})".format(self.src, self.tag, self.dst, self.description)
+
+    @property
+    def tag(self):
+        if len(self.history) > 0:
+            return self.history[0].tag
+
+    @property
+    def description(self):
+        if len(self.history) > 0:
+            return self.history[0].description
+
+    @property
+    def last_seen(self):
+        if len(self.history) > 0:
+            return self.history[0].last_seen
+
+    @staticmethod
+    def connect(src, dst):
+        try:
+            l = Link(src=src, dst=dst).save()
+        except NotUniqueError:
+            l = Link.objects.get(src=src, dst=dst)
+        return l
+
+    def info(self):
+        return {"tag": self.tag, "description": self.description}
+
+    def add_history(self, tag, description=None, first_seen=None, last_seen=None):
+        # this is race-condition prone... think of a better way to do this
+        if not first_seen:
+            first_seen = datetime.utcnow()
+        if not last_seen:
+            last_seen = datetime.utcnow()
+
+        if len(self.history) == 0:
+            return self.modify(push__history=LinkHistory(tag=tag, description=description, first_seen=first_seen, last_seen=last_seen))
+
+        last = self.history[0]
+        if description == last.description:  # Description is unchanged, update timestamp
+            return self.modify(set__history__0__last_seen=last_seen)
+        else:  # Link description has changed, insert in link history
+            return self.modify(push__history=LinkHistory(tag=tag, description=description, first_seen=first_seen, last_seen=last_seen))
 
 
 class Node(Document):
@@ -39,62 +100,9 @@ class Node(Document):
             info[node.type] = info.get(node.type, []) + [(link, node)]
         return info
 
+    def delete(self):
+        Link.objects(Q(src=self.id) | Q(dst=self.id)).delete()
+        super(Node, self).delete()
+
     def to_dict(self):
         return self._fields
-
-
-class LinkHistory(EmbeddedDocument):
-
-    tag = StringField()
-    description = StringField()
-    first_seen = DateTimeField(default=datetime.now)
-    last_seen = DateTimeField(default=datetime.now)
-
-
-class Link(Document):
-
-    src = ReferenceField(Node, required=True, reverse_delete_rule=CASCADE)
-    dst = ReferenceField(Node, required=True, reverse_delete_rule=CASCADE, unique_with='src')
-    history = SortedListField(EmbeddedDocumentField(LinkHistory), ordering='last_seen', reverse=True)
-
-    def __unicode__(self):
-        return u"{} {} {} ({})".format(self.src, self.tag, self.dst, self.description)
-
-    @property
-    def tag(self):
-        return self.history[0].tag
-
-    @property
-    def description(self):
-        return self.history[0].description
-
-    @property
-    def last_seen(self):
-        return self.history[0].last_seen
-
-    @staticmethod
-    def connect(src, dst):
-        try:
-            l = Link(src=src, dst=dst).save()
-        except NotUniqueError:
-            l = Link.objects.get(src=src, dst=dst)
-        return l
-
-    def info(self):
-        return {"tag": self.tag, "description": self.description}
-
-    def add_history(self, tag, description=None, first_seen=None, last_seen=None):
-        # this is race-condition prone... think of a better way to do this
-        if not first_seen:
-            first_seen = datetime.utcnow()
-        if not last_seen:
-            last_seen = datetime.utcnow()
-
-        if len(self.history) == 0:
-            return self.modify(push__history=LinkHistory(tag=tag, description=description, first_seen=first_seen, last_seen=last_seen))
-
-        last = self.history[0]
-        if description == last.description:  # Description is unchanged, update timestamp
-            return self.modify(set__history__0__last_seen=last_seen)
-        else:  # Link description has changed, insert in link history
-            return self.modify(push__history=LinkHistory(tag=tag, description=description, first_seen=first_seen, last_seen=last_seen))
