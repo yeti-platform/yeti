bfd274849593ab160e77a770378e599f6783485d
Thomas Chopitea <tomchop@gmail.com>
2015-12-12 01:25:16 +0100

    Minor cleanup

--- core/web/api/observable.py
+++ core/web/api/observable.py
@@ -1,7 +1,9 @@
 from core.web.api.crud import CrudApi
 from core.observables import Observable
 
+
 class ObservableApi(CrudApi):
+
     search_template = 'observables.html'
     api_frontend = 'observableapi'
     objectmanager = Observable
