897de98407ee949c51e7c393fabce32d26e756d5
Thomas Chopitea <tomchop@gmail.com>
2015-12-13 20:00:53 +0100

    Testfeeds script

---
+++ testfeeds.py
@@ -0,0 +1,15 @@
+import sys
+
+from core.feed import Feed, update_feed
+from core.scheduling import Scheduler
+
+if __name__ == '__main__':
+    Scheduler()
+    # feeds = {f.name: f for f in }
+    for f in Feed.objects():
+        print repr(f)
+
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+        # f = Feed.objects.get(name=name)
+        update_feed(name)
