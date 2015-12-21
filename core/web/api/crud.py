b02113687821c6d5ff71c974e480532029e9b7dd
Thomas Chopitea <tomchop@gmail.com>
2015-12-22 00:25:50 +0100

    Basic update API

--- core/web/api/crud.py
+++ core/web/api/crud.py
@@ -1,4 +1,7 @@
+from flask import request
 from flask_restful import Resource
+from flask_restful import abort as restful_abort
+from mongoengine.errors import DoesNotExist
 
 from core.web.api.api import render
 
@@ -12,3 +15,12 @@ class CrudApi(Resource):
             data = [d.info() for d in self.objectmanager.objects.all()]
 
         return render(data, template=self.template)
+
+    def post(self, id=None):
+        print request.json
+        if not id:
+            return render(self.objectmanager.get_or_create(**request.json).info())
+        else:
+            self.objectmanager.update(id, request.json)
+
+        return render({"status": "ok"})
