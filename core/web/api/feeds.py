64b6bd660f8d56f4449a88ac5e7cef25641a992d
Thomas Chopitea <tomchop@gmail.com>
2015-12-24 17:46:10 +0100

    Use IDs instead of names

--- core/web/api/feeds.py
+++ core/web/api/feeds.py
@@ -1,3 +1,5 @@
+from flask_restful import abort as restful_abort
+
 from core.web.api.crud import CrudApi
 from core.feed import Feed, update_feed
 from core.web.api.api import render
@@ -8,12 +10,14 @@ class FeedApi(CrudApi):
     template = 'feeds_api.html'
     objectmanager = Feed
 
-    def post(self, name, action):
+    def post(self, id, action):
+        if action not in ["refresh", "toggle"]:
+            restful_abort(400, {"error": "action must be either refresh or toggle"})
         if action == "refresh":
-            update_feed.delay(name)
-            return render({"name": name})
+            update_feed.delay(id)
+            return render({"id": id})
         elif action == "toggle":
-            f = Feed.objects.get(name=name)
+            f = Feed.objects.get(id=id)
             f.enabled = not f.enabled
             f.save()
-            return render({"name": name, "status": f.enabled})
+            return render({"id": id, "status": f.enabled})
