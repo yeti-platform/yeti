6004952fb2e725e82c7188a8b15ca22d63b1b880
Thomas Chopitea <tomchop@gmail.com>
2015-12-31 11:11:53 +0100

    Generalized CRUD JS API

---
+++ core/web/frontend/staticfiles/yeti/js/crud.js
@@ -0,0 +1,75 @@
+$(function(){
+
+  $("form.yeti-crud").each(function(){
+
+    refresh_table($(this));
+
+    $(this).find(".crud-search").click(function() {
+      form = $(this).closest("form").first();
+      refresh_table(form)
+    });
+
+    $(".crud-filter").keydown(function (event) {
+  		if (event.which == 13) {
+  			event.preventDefault();
+        form = $(this).closest("form").first();
+  			refresh_table(form);
+  		}
+  	});
+
+    $(".crud-paginator").click(function(event){
+      event.preventDefault();
+      direction = $(this).data('direction')
+      form = $(this).closest("form").first();
+      change_page(form, direction);
+      refresh_table(form);
+    });
+
+  });
+
+});
+
+function change_page(form, direction) {
+  page = form.find(".crud-pagination").data('page');
+  newpage = page+direction;
+  if (newpage < 1) {
+    newpage = 1;
+  }
+  form.find(".crud-pagination").data('page', newpage);
+  form.find(".crud-pagenumber").text(newpage);
+}
+
+function refresh_table(form) {
+
+  queries = form.find(".crud-filter").first().val().split(' ');
+	filter = {};
+
+  for (var i in queries) {
+		splitted = queries[i].split('=');
+		if (splitted.length > 1)
+			filter[splitted[0]] = splitted[1];
+		else if (splitted[0] != "")
+			filter['value'] = splitted[0];
+	}
+
+  params = {'regex': form.find('.crud-regex').prop('checked') ? true : false,
+            'page': form.find(".crud-pagination").data('page'),
+            }
+
+  query = {'filter': filter, 'params': params}
+
+  $.ajax({
+    method: "POST",
+    data: JSON.stringify(query),
+    contentType: "application/json",
+    url: form.data("url"),
+    success: function(observables) {
+      table = style_table($(observables))
+      $("#"+form.data('target')).html(table);
+    }
+  });
+}
+
+function style_table(table) {
+  return table
+}
