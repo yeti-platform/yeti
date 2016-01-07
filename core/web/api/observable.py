c9c4975f606f54056acdd1170333e9d06090578a
Thomas Chopitea <tomchop@gmail.com>
2016-01-07 18:05:27 +0100

    add_text used in observable creation via API

--- core/web/api/observable.py
+++ core/web/api/observable.py
@@ -1,9 +1,20 @@
+from flask import request
+
 from core.web.api.crud import CrudApi, CrudSearchApi
 from core.observables import Observable
+from core.web.api.api import render
 
 
 class ObservableApi(CrudApi):
     objectmanager = Observable
+
+    def post(self, id=None):
+        if not id:
+            return render(self.objectmanager.add_text(request.json['value']).to_mongo())
+        else:
+            self.objectmanager.update(id, request.json)
+
+        return render({"status": "ok"})
 
 
 class ObservableSearchApi(CrudSearchApi):
