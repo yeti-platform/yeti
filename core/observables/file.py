cc6143c6316e75ec48a1e34bdfcba70ae0b5ec94
Thomas Chopitea <tomchop@gmail.com>
2016-02-02 17:57:36 +0100

    Verbose names

--- core/observables/file.py
+++ core/observables/file.py
@@ -6,6 +6,6 @@ from core.observables import Hash
 
 class File(Observable):
 
-    mime_type = StringField()
-    hashes = ListField(ReferenceField(Hash))
-    body = FileField()
+    mime_type = StringField(verbose_name="MIME type")
+    hashes = ListField(ReferenceField(Hash), verbose_name="Hashes")
+    body = FileField(verbose_name="File content")
