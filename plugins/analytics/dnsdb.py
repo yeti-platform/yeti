5cc821b6499e3169c57ade9801393b85da83736f
Gael Muller <muller.gael@gmail.com>
2016-02-04 18:46:44 +0100

    better management of 404 in DNSDB lookups

--- plugins/analytics/dnsdb.py
+++ plugins/analytics/dnsdb.py
@@ -64,23 +64,26 @@ class DNSDBApi(object):
         url = "{}/{}/{}/{}".format(DNSDBApi.API_URL, type, obs_type, observable.value)
 
         r = requests.get(url, headers=headers)
-        r.raise_for_status()
 
-        records = []
+        if r.status_code == 200:
+            records = []
+            for record in r.iter_lines():
+                record = json.loads(record)
+                if record['rrtype'] in DNSDBApi.RECORD_TYPES:
+                    if 'time_first' in record:
+                        record['first_seen'] = datetime.utcfromtimestamp(record['time_first'])
+                        record['last_seen'] = datetime.utcfromtimestamp(record['time_last'])
+                    else:
+                        record['first_seen'] = datetime.utcfromtimestamp(record['zone_time_first'])
+                        record['last_seen'] = datetime.utcfromtimestamp(record['zone_time_last'])
 
-        for record in r.iter_lines():
-            record = json.loads(record)
-            if record['rrtype'] in DNSDBApi.RECORD_TYPES:
-                if 'time_first' in record:
-                    record['first_seen'] = datetime.utcfromtimestamp(record['time_first'])
-                    record['last_seen'] = datetime.utcfromtimestamp(record['time_last'])
-                else:
-                    record['first_seen'] = datetime.utcfromtimestamp(record['zone_time_first'])
-                    record['last_seen'] = datetime.utcfromtimestamp(record['zone_time_last'])
+                    records.append(record)
 
-                records.append(record)
-
-        return records
+            return records
+        elif r.status_code == 404:
+            return []
+        else:
+            r.raise_for_status()
 
 
 class DNSDBReversePassiveDns(OneShotAnalytics, DNSDBApi):
