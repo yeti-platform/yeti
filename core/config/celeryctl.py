f88e2a82302759c487783aee5bd67347f1780d65
Thomas Chopitea <tomchop@gmail.com>
2016-02-17 14:40:39 +0100

    Feed queue

--- core/config/celeryctl.py
+++ core/config/celeryctl.py
@@ -5,14 +5,14 @@ celery_app = Celery('yeti')
 
 class CeleryConfig:
     BROKER_URL = 'mongodb://localhost:27017/'
-    CELERY_RESULT_BACKEND = 'mongodb://localhost:27017/'
     CELERY_TASK_SERIALIZER = 'json'
     CELERY_ACCEPT_CONTENT = ['json']
     CELERY_IMPORTS = ('core.config.celeryimports', 'core.analytics_tasks', 'core.exports')
     CELERY_TIMEZONE = 'UTC'
 
     CELERY_ROUTES = {
-        'core.analytics_tasks.single': {'queue': 'oneshot'}
+        'core.analytics_tasks.single': {'queue': 'oneshot'},
+        'core.feed.update_feed': {'queue': 'feeds'}
     }
 
 celery_app.config_from_object(CeleryConfig)
