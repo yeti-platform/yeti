74abf5a1275439b24b31bec9f8bd16771d59b318
Thomas Chopitea <tomchop@gmail.com>
2016-02-22 18:25:06 +0100

    Leave prefetch multipliers to the workers

--- core/config/celeryctl.py
+++ core/config/celeryctl.py
@@ -4,7 +4,8 @@ celery_app = Celery('yeti')
 
 
 class CeleryConfig:
-    BROKER_URL = 'mongodb://localhost:27017/'
+    # BROKER_URL = 'mongodb://localhost:27017/'
+    BROKER_URL = 'redis://localhost:6379/0'
     CELERY_TASK_SERIALIZER = 'json'
     CELERY_ACCEPT_CONTENT = ['json']
     CELERY_IMPORTS = ('core.config.celeryimports', 'core.analytics_tasks', 'core.exports', 'core.feed')
