c2102d80a28d8ff2b975e18ab3df866f0ae2c59d
Thomas Chopitea <tomchop@gmail.com>
2016-03-03 11:34:11 +0100

    Bugfix

--- core/config/celeryctl.py
+++ core/config/celeryctl.py
@@ -8,7 +8,7 @@ class CeleryConfig:
     BROKER_URL = 'redis://localhost:6379/0'
     CELERY_TASK_SERIALIZER = 'json'
     CELERY_ACCEPT_CONTENT = ['json']
-    CELERY_IMPORTS = ('core.config.celeryimports', 'core.analytics_tasks', 'core.exports', 'core.feed')
+    CELERY_IMPORTS = ('core.config.celeryimports', 'core.analytics_tasks', 'core.exports.export', 'core.feed')
     CELERY_TIMEZONE = 'UTC'
 
     CELERY_ROUTES = {
