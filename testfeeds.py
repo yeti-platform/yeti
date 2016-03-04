0e8d618bc96cec4b53da9865d5c56a51d2ab9b9b
Thomas Chopitea <tomchop@gmail.com>
2016-03-04 16:07:28 +0100

    Bugfix

--- testfeeds.py
+++ testfeeds.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     Scheduler()
     # feeds = {f.name: f for f in }
     for f in Feed.objects():
-        print repr(f)
+        print repr(f.name)
 
     if len(sys.argv) > 1:
         name = sys.argv[1]
-        # f = Feed.objects.get(name=name)
-        update_feed(name)
+        f = Feed.objects.get(name=name)
+        update_feed(f.id)
