809c1d79753f82ba1509ccffd0755e6f4a42c56e
Thomas Chopitea <tomchop@gmail.com>
2016-03-08 18:43:45 +0100

    Filter from db

--- plugins/analytics/propagate_blocklist.py
+++ plugins/analytics/propagate_blocklist.py
@@ -15,14 +15,13 @@ class PropagateBlocklist(ScheduledAnalytics):
 
     ACTS_ON = 'Url'  # act on Urls only
 
-    CUSTOM_FILTER = Q(tags__not__size=0)  # filter only tagged elements
+    CUSTOM_FILTER = Q(tags__name="blocklist")  # filter only tagged elements
 
     EXPIRATION = None
 
     @staticmethod
     def each(obj):
-        if obj.has_tag("blocklist"):
-            n = obj.neighbors(neighbor_type="Hostname").values()
-            if n:
-                for link in n[0]:
-                    link[1].tag('blocklist')
+        n = obj.neighbors(neighbor_type="Hostname").values()
+        if n:
+            for link in n[0]:
+                link[1].tag('blocklist')
