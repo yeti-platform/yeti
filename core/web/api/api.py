5860ce8dff1a46574997a0d89d6029a5632de0fa
Thomas Chopitea <tomchop@gmail.com>
2016-03-24 12:41:28 +0100

    Make json encoder recursive

--- core/web/api/api.py
+++ core/web/api/api.py
@@ -5,6 +5,7 @@ from json import dumps
 
 from core.web.json import to_json
 from core.helpers import iterify
+from core.database import Node, Link
 
 
 api = Blueprint("api", __name__, template_folder="templates")
@@ -12,21 +13,38 @@ api = Blueprint("api", __name__, template_folder="templates")
 
 @renderer('application/json')
 def bson_renderer(objects, template=None, ctx=None):
+    data = recursive_encoder(objects)
+    return dumps(data, default=to_json)
 
-    if not isinstance(objects, dict):
-        #  list of nodes can't be sent via json, use the info() function
+
+def recursive_encoder(objects, template=None, ctx=None):
+
+    if isinstance(objects, dict):
+        for (key, value) in objects.items():
+            objects[key] = recursive_encoder(value)
+        return objects
+
+    elif isinstance(objects, list):
+        return [recursive_encoder(o) for o in objects]
+
+    elif isinstance(objects, tuple):
+        return tuple(recursive_encoder(o) for o in objects)
+
+    elif isinstance(objects, (Node, Link)):
         data = []
         for o in iterify(objects):
             info = o.info()
-            info['uri'] = o.uri
+            if hasattr(o, 'uri'):
+                info['uri'] = o.uri
             data.append(info)
 
         if len(data) == 1:
             data = data[0]
-    else:
-        data = objects
 
-    return dumps(data, default=to_json)
+        return data
+    else:
+        return data
+
 
 render = Render(renderers=[template_renderer, bson_renderer])
 render_json = Render(renderers=[bson_renderer])
