8f309bc02a7e3e141e8180656d15d5ad2c5a9cb4
Thomas Chopitea <tomchop@gmail.com>
2016-04-22 11:31:29 +0200

    Error messages when indicator does not compile

--- core/indicators/regex.py
+++ core/indicators/regex.py
@@ -8,7 +8,13 @@ class Regex(Indicator):
     def __init__(self, *args, **kwargs):
         super(Regex, self).__init__(*args, **kwargs)
         if self.pattern:
-            self.compiled_regex = re.compile(self.pattern)
+            try:
+                self.compiled_regex = re.compile(self.pattern)
+                self.error = False
+            except Exception as e:
+                self.compiled_regex = None
+                self.error = e
 
     def match(self, value):
-        return True if self.compiled_regex.search(value) else False
+        if not self.error:
+            return True if self.compiled_regex.search(value) else False
--- core/indicators/yara_rules.py
+++ core/indicators/yara_rules.py
@@ -8,8 +8,14 @@ class Yara(Indicator):
     def __init__(self, *args, **kwargs):
         super(Yara, self).__init__(*args, **kwargs)
         if self.pattern:
-            self.compiled_yara = yara.compile(source=self.pattern)
+            try:
+                self.compiled_yara = yara.compile(source=self.pattern)
+                self.error = False
+            except Exception as e:
+                self.compiled_yara = None
+                self.error = e
 
     def match(self, value):
-        matches = self.compiled_yara.match(data=value)
-        return True if matches else False
+        if not self.error:
+            matches = self.compiled_yara.match(data=value)
+            return True if matches else False
