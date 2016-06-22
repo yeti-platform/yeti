61bc3b585e35b3696b34ec21c03e90318c69b55d
Gael Muller <muller.gael@gmail.com>
2016-06-22 15:29:54 +0200

    remove zombie code

--- core/entities/actor.py
+++ core/entities/actor.py
@@ -19,10 +19,6 @@ class Actor(Entity):
         form.aliases = StringListField("Aliases")
         return form
 
-    def add_alias(alias):
-        self.modify(add_to_set__aliases=alias)
-        self.reload()
-
     def generate_tags(self):
         return [self.name.lower()]
 
