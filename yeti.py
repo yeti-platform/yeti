4590157382f74797cc84d222756b17863fa52417
Thomas Chopitea <tomchop@gmail.com>
2016-07-03 09:52:36 +0200

    Change default logging level

--- yeti.py
+++ yeti.py
@@ -5,7 +5,7 @@ import logging
 
 from core.web import webapp
 
-logging.basicConfig(format='%(levelname)s:%(module)s:%(message)s', level=logging.INFO)
+logging.basicConfig(format='%(levelname)s:%(module)s:%(message)s', level=logging.ERROR)
 
 if __name__ == '__main__':
     webapp.run(host="0.0.0.0")
