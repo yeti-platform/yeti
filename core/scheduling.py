74a11aa54d185915955fce34199c5499d98c99f7
Thomas Chopitea <tomchop@gmail.com>
2016-09-07 14:39:21 +0200

    Bugfix

--- core/scheduling.py
+++ core/scheduling.py
@@ -77,7 +77,7 @@ class Scheduler(BaseScheduler):
 
         for sched in ScheduleEntry.objects.all():
             if sched.enabled:
-                self._loaded_entries[str(sched.id)] = sched
+                self._loaded_entries[sched.name] = sched
 
         for subdir in subdirs:
             modules_dir = os.path.join(base_dir, subdir)
