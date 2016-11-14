44ff0117ce7fa0814fc5a68b5b3d34777d3de54e
Thomas Chopitea <tomchop@gmail.com>
2016-11-14 10:51:38 +0100

    User login / logout

---
+++ core/auth/local/__init__.py
@@ -0,0 +1,1 @@
+from core.auth.local.views import auth
--- core/user.py
+++ core/user.py
@@ -1,21 +1,34 @@
 from __future__ import unicode_literals
 
 from mongoengine import StringField, DictField, BooleanField
+from mongoengine import DoesNotExist
 from flask_mongoengine.wtf import model_form
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from core.database import YetiDocument
+
+DEFAULT_ROLES = {
+    "observables": ["read", "write", "tag"],
+    "indicators": ["read", "write"],
+    "entities": ["read", "write"],
+    "admin": True,
+}
 
 
 class User(YetiDocument):
     available_settings = dict()
 
-    login = StringField(required=True, unique=True)
+    username = StringField(required=True, unique=True)
+    password = StringField(required=True)
     enabled = BooleanField(required=True, default=True)
-    settings = DictField()
+    roles = DictField(verbose_name="Roles", default=DEFAULT_ROLES)
+    settings = DictField(verbose_name="Settings")
 
     def is_authenticated(self):
-        print repr(self.enabled), self.enabled == True
         return self.enabled
+
+    def is_admin(self):
+        return self.roles.get('admin', False)
 
     def is_active(self):
         return True
@@ -32,6 +45,9 @@ class User(YetiDocument):
                 return False
         return True
 
+    def __unicode__(self):
+        return u"<User: {}>".format(self.username)
+
     @classmethod
     def get_form(klass):
         return model_form(User)
@@ -46,4 +62,21 @@ class User(YetiDocument):
     # This is only used because user management / access control is not yet implemented
     @staticmethod
     def get_default_user():
-        return User.objects(login='yeti').modify(upsert=True, new=True, login='yeti')
+        return User.objects(username='yeti').modify(upsert=True, new=True, username='yeti', password="yeti")
+
+    @staticmethod
+    def create_user(username, password, roles=DEFAULT_ROLES):
+        u = User(username=username, roles=roles)
+        u.password = generate_password_hash(password)
+        return u.save()
+
+    @staticmethod
+    def authenticate(username, password):
+        try:
+            u = User.objects.get(username=username)
+            if check_password_hash(u.password, password):
+                return u
+            else:
+                return False
+        except DoesNotExist:
+            return False
