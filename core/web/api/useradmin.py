70b93151cde97160e643e6d389f0db4d6ed1a09e
Thomas Chopitea <tomchop@gmail.com>
2016-11-20 20:07:27 +0100

    Extra controls to user list view

--- core/web/api/useradmin.py
+++ core/web/api/useradmin.py
@@ -1,10 +1,26 @@
-# from flask import request, abort, make_response, send_file, url_for
-from core.web.api.crud import CrudSearchApi, CrudApi
+from flask_classy import route
 
+from core.web.api.crud import CrudSearchApi
 from core.user import User
-# from core.web.api.api import render
+from core.web.api.api import render
+from core.web.helpers import requires_role, get_object_or_404
 
 
 class UserAdminSearch(CrudSearchApi):
     template = 'user_api.html'
     objectmanager = User
+
+    @route('/toggle/<id>', methods=["POST"])
+    @requires_role('admin')
+    def toggle(self, id):
+        user = get_object_or_404(User, id=id)
+        user.enabled = not user.enabled
+        user.save()
+        return render({"enabled": user.enabled, "id": id})
+
+    @route('/remove/<id>', methods=["POST"])
+    @requires_role('admin')
+    def remove(self, id):
+        user = get_object_or_404(User, id=id)
+        user.delete()
+        return render({"id": id})
