ad5d8e20dceacfca99c6f63ce98dd155075c019a
Thomas Chopitea <tomchop@gmail.com>
2017-03-06 21:35:02 +0100

    Gracefully catch GeoIP error

--- plugins/analytics/public/process_ip.py
+++ plugins/analytics/public/process_ip.py
@@ -8,9 +8,17 @@ from core.errors import ObservableValidationError
 import geoip2.database
 from geoip2.errors import AddressNotFoundError
 
-path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "GeoLite2-City.mmdb")
 
-reader = geoip2.database.Reader(path)
+try:
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "GeoLite2-City.mmdb")
+    reader = geoip2.database.Reader(path)
+except IOError as e:
+    logging.error(
+        "Could not open GeoLite2-City.mmdb. Will proceed without GeoIP data")
+    logging.error(e)
+    reader = False
+
+
 
 
 class ProcessIp(ScheduledAnalytics):
@@ -27,11 +35,12 @@ class ProcessIp(ScheduledAnalytics):
     @staticmethod
     def each(ip):
         try:
-            response = reader.city(ip.value)
-            ip.geoip = {
-                "country": response.country.iso_code,
-                "city": response.city.name
-            }
+            if reader:
+                response = reader.city(ip.value)
+                ip.geoip = {
+                    "country": response.country.iso_code,
+                    "city": response.city.name
+                }
             ip.save()
         except ObservableValidationError:
             logging.error("An error occurred when trying to add {} to the database".format(ip.value))
