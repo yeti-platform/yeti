1c67791b2c32289da312b4da8b12606f401237b6
Thomas Chopitea <tomchop@gmail.com>
2017-03-06 21:30:57 +0100

    Documented python API

--- pyeti/pyeti.py
+++ pyeti/pyeti.py
@@ -14,24 +14,77 @@ class YetiApi(object):
         self._test_connection()
 
     def analysis_match(self, observables):
+        """Matches a list of observables against Yeti indicators.
+
+        Args:
+            observables: An array of strings representing observables
+
+        Returns:
+            JSON representation of match response.
+        """
         json_payload = {"observables": observables}
         return self._make_post("analysis/match", json_payload)
 
     def observable_search(self, count=50, offset=1, regex=False, **kwargs):
+        """Search for observables.
+
+        Args:
+            count: How many Observables you want to fetch.
+            offset: How many sets of *count* Observables you want to skip
+                    (total skipped = offset * count)
+            regex: Use regular expressions to Search.
+            kwargs: Remaining keyword arguments will be transformed in a JSON
+                    object that will act as the filter.
+
+        Returns:
+            Array of JSON representations of matching Observables.
+        """
         json_payload = {"filter": kwargs, "params": {"page": offset, "range": count, "regex": regex}}
         return self._make_post("observablesearch/", json_payload)
 
     def observable_details(self, id):
+        """Get details on an Observable.
+        Args:
+            id: A string representing the observable's ObjectID
+
+        Returns:
+            JSON representation of the requested Observable.
+        """
         return self._make_get("observable/{}".format(id))
 
     def observable_add(self, value, tags=[], context={}, source="API"):
-        json_payload = {"tags": tags, "value": value, "source": source, "context": context}
+        """Add an observable to the dataset
+
+        Args:
+            value: the Observable value
+            tags: An array of strings representing tags
+            context: A dictionary object with context information
+            source: A string representing the source of the data. Defaults to
+                    "API".
+
+        Returns:
+            JSON representation of the created observable.
+        """
+        json_payload = {"tags": tags,
+                        "value": value,
+                        "source": source,
+                        "context": context}
         return self._make_post('observable/', payload=json_payload)
 
-    def observable_bulk_add(self, observables, tags=[], source=None):
+    def observable_bulk_add(self, observables, tags=[]):
+        """Add an observable to the dataset
+
+        Args:
+            value: the Observable value
+            tags: An array of strings representing tags
+            context: A dictionary object with context information
+            source: A string representing the source of the data. Defaults to
+                    "API".
+
+        Returns:
+            JSON representation of the created observable.
+        """
         json_payload = {"tags": tags, "observables": observables}
-        if source:
-            json_payload['source'] = source
         return self._make_post('observable/bulk', payload=json_payload)
 
     def _test_connection(self):
