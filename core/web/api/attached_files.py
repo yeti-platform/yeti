3f012ca8bdfc75a6dbf78a5bae8a26f32a9ed934
Thomas Chopitea <tomchop@gmail.com>
2017-04-09 13:11:34 +0200

    Remane Files to AttachedFiles

--- core/web/api/attached_files.py
+++ core/web/api/attached_files.py
@@ -1,3 +1,5 @@
+from bson.json_util import loads
+
 from flask import request, abort, make_response, send_file, url_for
 from flask_classy import FlaskView
 
@@ -6,12 +8,17 @@ from core.web.helpers import get_object_or_404
 from core.web.api.api import render
 
 
-class Files(FlaskView):
+class AttachedFiles(FlaskView):
     def post(self):
-        if 'file' not in request.files:
-            abort(400)
+        if 'file' in request.files:
+            f = AttachedFile.from_upload(request.files['file'])
+        else:
+            data = loads(request.data)
+            if 'file' in data:
+                f = AttachedFile.from_upload(data['file'])
+            else:
+                abort(400)
 
-        f = AttachedFile.from_upload(request.files['file'])
         return render({'filename': url_for('api.Files:get', id=f.id)})
 
     def get(self, id):
