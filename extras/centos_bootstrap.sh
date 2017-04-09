b367b2d558012c81694cda53d1d8eec6c7c1f659
zcatbear <jamieford34@gmail.com>
2017-04-09 09:56:49 -0500

    Removed quotation marks from mongodb yum repo as they are not needed

--- extras/centos_bootstrap.sh
+++ extras/centos_bootstrap.sh
@@ -3,12 +3,12 @@
 ### Create the MongoDB Yum repository
 
 cat << EOF > /etc/yum.repos.d/mongodb-org-3.4.repo
-"[mongodb-org-3.4]
+[mongodb-org-3.4]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.4/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc"
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
 EOF
 ### Update the OS
 yum update -y && yum upgrade -y
