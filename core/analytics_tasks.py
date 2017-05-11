15989430c470b90e8b656462d0c6c57ef5035088
Thomas Chopitea <tomchop@gmail.com>
2017-05-11 20:22:05 +0200

    Bugfix

--- core/analytics_tasks.py
+++ core/analytics_tasks.py
@@ -15,9 +15,9 @@ from mongoengine import DoesNotExist
 def each(module_name, observable_json):
     o = Observable.from_json(observable_json)
     mod = loaded_modules[module_name]
-    logging.debug("Launching {} on {}".format(mod.__class__.__name__, o))
+    logging.debug("Launching {} on {}".format(mod.__name__, o))
     mod.each(o)
-    o.analysis_done(mod.__class__.__name__)
+    o.analysis_done(mod.__name__)
 
 
 @celery_app.task
