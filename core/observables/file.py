ceb32eb2cefadc04fdf7cf5c474a96d307a1618f
Thomas Chopitea <tomchop@gmail.com>
2017-05-14 10:00:22 +0200

    Clean up File edit view

--- core/observables/file.py
+++ core/observables/file.py
@@ -1,21 +1,30 @@
 from __future__ import unicode_literals
 
+from flask import url_for
+from flask_mongoengine.wtf import model_form
 from mongoengine import *
 
+
 from core.observables import Observable
-from core.observables import Hash
+from core.database import StringListField
 
 
 class File(Observable):
 
-    value = StringField(verbose_name="SHA256 hash")
-
+    value = StringField(verbose_name="Value")
     mime_type = StringField(verbose_name="MIME type")
     hashes = DictField(verbose_name="Hashes")
     body = ReferenceField("AttachedFile")
     filenames = ListField(StringField(), verbose_name="Filenames")
 
     DISPLAY_FIELDS = Observable.DISPLAY_FIELDS + [("mime_type", "MIME Type")]
+    exclude_fields = Observable.exclude_fields + ['hashes', 'body']
+
+    @classmethod
+    def get_form(klass):
+        form = model_form(klass, exclude=klass.exclude_fields)
+        form.filenames = StringListField("Filenames")
+        return form
 
     @staticmethod
     def check_type(txt):
