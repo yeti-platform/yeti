b10584e4f1b49c1d96d219756d8ca3f423a29bbb
Thomas Chopitea <tomchop@gmail.com>
2017-06-02 22:42:09 +0200

    Add migration

---
+++ core/internals/migrations/migration_1.py
@@ -0,0 +1,10 @@
+from core.user import User
+
+__description__ = "Migrate file and attached files permissions."
+
+def migrate():
+    for u in User.objects.all():
+        u.permissions['attachedfiles'] = u.permissions['files']
+        u.permissions['file'] = {'read': True, 'write': True}
+        u.permissions.pop('files')
+        u.save()
