806606a3aef51c852afee1ed5bf8661f0cbac054
Gael Muller <muller.gael@gmail.com>
2017-06-03 10:16:53 +0200

    redirect to observable search results after file upload

--- core/web/frontend/staticfiles/yeti/js/filedrag.js
+++ core/web/frontend/staticfiles/yeti/js/filedrag.js
@@ -15,44 +15,8 @@ $(function () {
       span.html("<strong>" + files.length + "</strong> files selected");
     }
     else if (files.length == 1) {
-      span.html("Selected <strong>" + files[0].name + "</strong>");
+      span.html("<strong>1</strong> file selected");
     }
   });
 
-
-  $("form.dropzone").on('submit', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-
-    form = $(this)
-    form.find(".btn.upload").prop('disabled', true)
-    form.find('span').html("<strong>Uploading...</strong>")
-    ajaxFormData = new FormData();
-
-    files = form.find("input[type=file]")[0].files
-
-    for (var i = 0; i < files.length; i++) {
-      ajaxFormData.append("files", files[i], files[i].name);
-    }
-
-    ajaxFormData.append("unzip", form.find(".checkbox.zip input").prop('checked'))
-
-    $.ajax({
-      method: "POST",
-      headers: {"Accept": "application/json"},
-      processData: false,
-      contentType: false,
-      url: $(this).attr('action'),
-      data: ajaxFormData,
-      success: function(data) {
-        $("#file-upload-modal").modal('toggle');
-        notify("Succesfully uploaded " + data.length + " files.", "success");
-        form.find('span').html("<strong>Drag &amp; drop</strong> or <strong>click</strong> to add files")
-        form.find("input").val('');
-        form.find(".btn.upload").prop('disabled', false)
-      }
-    });
-
-  })
-
-})
+});
