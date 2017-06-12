24bdabab2e9605948443262f61d35c3f5f7b5606
Gael Muller <muller.gael@gmail.com>
2017-06-12 18:46:18 +0200

    autoreload template when in debug mode

--- yeti.py
+++ yeti.py
@@ -6,21 +6,29 @@ import argparse
 
 logging.basicConfig(format='%(levelname)s:%(module)s:%(message)s', level=logging.ERROR)
 
+
 def webserver(args):
     from core.web import webapp
+
+    # Enable debug and autoreload in dev
     webapp.debug = args.debug
+    if webapp.debug:
+        webapp.config['TEMPLATES_AUTO_RELOAD'] = True
+
     syncdb()
     print "[+] Yeti started. Point browser to http://localhost:5000/{}".format(" (debug)" if args.debug else "")
     webapp.run(host="0.0.0.0")
+
 
 def syncdb(args=None):
     from core.internals import Internals
     Internals.syncdb()
 
 COMMANDS = {
- 'webserver': webserver,
- 'syncdb': syncdb,
+    'webserver': webserver,
+    'syncdb': syncdb,
 }
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -29,7 +37,7 @@ def main():
     webserver = subparsers.add_parser('webserver', help='Launch yeti webserver')
     webserver.add_argument('--debug', action='store_true', help="Run Flask in debug mode")
 
-    syncdb = subparsers.add_parser('syncdb', help='Sync database with Yeti version')
+    subparsers.add_parser('syncdb', help='Sync database with Yeti version')
 
     args = parser.parse_args()
     command = args.command
