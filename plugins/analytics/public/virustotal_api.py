224c9beb21b4955ad66dee244aad2461aefaba8d
patito <daniel.axelrod@scitum.com.mx>
2017-10-12 17:49:36 -0500

    Virustotal with no external dependencies

--- plugins/analytics/public/virustotal_api.py
+++ plugins/analytics/public/virustotal_api.py
@@ -1,9 +1,24 @@
 from pprint import pformat
-import virustotal
 from core.analytics import OneShotAnalytics
+from core.observables import Hostname, Ip
+import urllib
+
+
+BASE_IP_URL = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
+BASE_URL_URL = 'https://www.virustotal.com/vtapi/v2/url/report'
+BASE_IP_PARAMS = {'ip': None, 'apikey': None}
+BASE_URL_PARAMS = {'resource': None, 'apikey': None}
 
 
 class VirustotalApi(object):
+    """
+    Base class for querying the VirusTotal API.
+    This is the public API, so there is a limit for up to 3
+    requests per minute.
+
+    TODO: Register a redis key with the last query time and prevent
+    limit rejection, as it could cause api key deactivation.
+    """
     settings = {
         "virutotal_api_key": {
             "name": "Virustotal API Key",
@@ -12,13 +27,32 @@ class VirustotalApi(object):
     }
 
     @staticmethod
-    def fetch(observable, api_key):
+    def get_ip_report(ip, api_key):
         try:
-            vt = virustotal.VirusTotal(api_key)
-            r = vt.get(observable.value)
-        except Exception:
-            raise
-        return r
+            params = BASE_IP_PARAMS
+            params['ip'] = ip
+            params['apikey'] = api_key
+            response = urllib.urlopen('%s?%s' % (BASE_IP_URL, urllib.urlencode(params))).read()
+            if response.code == 200:
+                # self.last_query['time'] = datetime.now()
+                # self.last_query['count'] += 1
+                return response
+        except Exception as e:
+            print 'Exception while getting ip report %s' % e.message
+
+    @staticmethod
+    def get_url_report(url, api_key):
+        try:
+            params = BASE_URL_PARAMS
+            params['resource'] = url
+            params['apikey'] = api_key
+            response = urllib.urlopen('%s?%s' % (BASE_URL_URL, urllib.urlencode(params))).read()
+            if response.code == 200:
+                # self.last_query['time'] = datetime.now()
+                # self.last_query['count'] += 1
+                return response
+        except Exception as e:
+            print 'Exception while getting url report %s' % e.message
 
 
 class VirusTotalQuery(OneShotAnalytics, VirustotalApi):
@@ -27,12 +61,19 @@ class VirusTotalQuery(OneShotAnalytics, VirustotalApi):
         "description": "Perform a Virustotal query."
     }
 
-    ACTS_ON = ["Ip", "Hostname"]
+    ACTS_ON = "Ip"
 
     @staticmethod
     def analyze(observable, results):
         links = set()
-        result = VirustotalApi.fetch(observable, results.settings['virutotal_api_key'])
+        apikey = results.settings['virutotal_api_key']
+        result = None
+
+        if isinstance(observable, Hostname):
+            result = VirustotalApi.get_url_report(observable, apikey)
+        elif isinstance(observable, Ip):
+            result = VirustotalApi.get_ip_report(observable, apikey)
+
         results.update(raw=pformat(result))
 
         if 'tags' in result and result['tags'] is not None:
