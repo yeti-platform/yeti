c7bb0a2f1d711c8a070c2aaae1388115d0064fc6
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-28 08:20:20 -0800

    Add Greensnow IP Blocklist Feed

---
+++ plugins/feeds/public/greensnow_blocklist_ip.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+import logging
+
+from core.observables import Ip
+from core.feed import Feed
+from core.errors import ObservableValidationError
+
+
+class GreensnowBlocklistIP(Feed):
+    default_values = {
+        'frequency': timedelta(hours=1),
+        'source': 'https://blocklist.greensnow.co/greensnow.txt',
+        'name': 'GreensnowBlocklistIP',
+        'description': 'Attacks / bruteforce that are monitored are: Scan Port, FTP, POP3, mod_security, IMAP, SMTP, SSH, cPanel ...'
+    }
+
+    def update(self):
+        for line in self.update_lines():
+            self.analyze(line)
+
+    def analyze(self, line):
+        if line.startswith('#'):
+            return
+
+        try:
+            line = line.strip()
+            parts = line.split()
+
+            ip = str(parts[0]).strip()
+            context = {
+                'source': self.name
+            }
+
+            try:
+                ip = Ip.get_or_create(value=ip)
+                ip.add_context(context)
+                ip.add_source('feed')
+                ip.tag(['blocklist'])
+            except ObservableValidationError as e:
+                logging.error(e)
+        except Exception as e:
+            logging.debug(e)
