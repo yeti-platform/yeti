4ab2511fff0398b5023cd58d234cf86674e065d0
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-27 18:02:10 -0800

    Add hostsfile SPAM (GRM) list

---
+++ plugins/feeds/public/hostsfile_grm.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+import logging
+
+from core.observables import Hostname
+from core.feed import Feed
+from core.errors import ObservableValidationError
+
+
+class HostsFileGRM(Feed):
+    default_values = {
+        'frequency': timedelta(hours=4),
+        'source': 'https://hosts-file.net/grm.txt',
+        'name': 'HostsFileGRM',
+        'description': 'Contains sites spamming sites listed sites listed in the hpHosts database.'
+    }
+
+    def update(self):
+        for line in self.update_lines():
+            self.analyze(line)
+
+    def analyze(self, line):
+        if line.startswith('#'):
+            return
+
+        try:
+            line = line.strip()
+            parts = line.split()
+
+            hostname = str(parts[1]).strip()
+            context = {
+                'source': self.name
+            }
+
+            try:
+                host = Hostname.get_or_create(value=hostname)
+                host.add_context(context)
+                host.add_source('feed')
+                host.tag(['spam', 'blocklist'])
+            except ObservableValidationError as e:
+                logging.error(e)
+        except Exception as e:
+            logging.debug(e)
