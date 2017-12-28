70e1b03b9c2f1df88ba7fd1ffed274d5adab91a1
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-27 18:24:44 -0800

    Fix Codacy Errors: Unused Python Modules
    Issue found: Unused datetime imported from datetime

--- plugins/feeds/public/blocklist_de_port_22.py
+++ plugins/feeds/public/blocklist_de_port_22.py
@@ -5,39 +5,38 @@ from core.observables import Ip
 from core.feed import Feed
 from core.errors import ObservableValidationError
 
+
 class BlocklistdePort22(Feed):
-	default_values = {
-		'frequency': timedelta(hours=1),
-		'source': 'https://lists.blocklist.de/lists/22.txt',
-		'name': 'BlocklistdePort22',
-		'description': 'Blocklist.de IMAP IP blocklist: IPs performing attacks on port 22 (SSH)'
+    default_values = {
+        'frequency': timedelta(hours=1),
+        'source': 'https://lists.blocklist.de/lists/22.txt',
+        'name': 'BlocklistdePort22',
+        'description': 'Blocklist.de IMAP IP blocklist: IPs performing attacks on port 22 (SSH)'
+    }
 
-	}
+    def update(self):
+        for line in self.update_lines():
+            self.analyze(line)
 
+    def analyze(self, line):
+        if line.startswith('#'):
+            return
 
-	def update(self):
-		for line in self.update_lines():
-			self.analyze(line)
+        try:
+            line = line.strip()
+            parts = line.split()
 
-	def analyze(self, line):
-		if line.startswith('#'):
-			return
+            ip = str(parts[0]).strip()
+            context = {
+                'source': self.name
+            }
 
-		try:
-			line = line.strip()
-			parts = line.split()
-
-			ip = str(parts[0]).strip()
-			context = {
-				'source': self.name
-			}
-
-			try:
-				ip = Ip.get_or_create(value=ip)
-				ip.add_context(context)
-				ip.add_source('feed')
-				ip.tag(['blocklist', 'ssh'])
-			except ObservableValidationError as e:
-				logging.error(e)
-		except Exception as e:
-			logging.debug(e)
+            try:
+                ip = Ip.get_or_create(value=ip)
+                ip.add_context(context)
+                ip.add_source('feed')
+                ip.tag(['blocklist', 'ssh'])
+            except ObservableValidationError as e:
+                logging.error(e)
+        except Exception as e:
+            logging.debug(e)
