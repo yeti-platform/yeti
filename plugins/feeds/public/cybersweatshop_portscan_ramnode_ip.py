06bc0be696280d4495487468a4f0681200a6f402
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-27 18:27:08 -0800

    Fix Indent in Cybersweatshop Ramnode

--- plugins/feeds/public/cybersweatshop_portscan_ramnode_ip.py
+++ plugins/feeds/public/cybersweatshop_portscan_ramnode_ip.py
@@ -7,44 +7,43 @@ from core.observables import Ip
 
 
 def strip(str):
-	return str.strip()
+    return str.strip()
 
 
 class CyberSweatShopPortscanRamnodeIP(Feed):
+    default_values = {
+        "frequency": timedelta(minutes=60),
+        "name": "CyberSweatShopPortscanRamnodeIP",
+        "source": "https://cybersweat.shop/iprep/iprep_ramnode.txt",
+        "description": "CyberSweatShop; IP addresses have been detected performing TCP SYN to 81.4.103.251 to a non-listening service or daemon",
+    }
 
-	default_values = {
-		"frequency": timedelta(minutes=60),
-		"name": "CyberSweatShopPortscanRamnodeIP",
-		"source": "https://cybersweat.shop/iprep/iprep_ramnode.txt",
-		"description": "CyberSweatShop; IP addresses have been detected performing TCP SYN to 81.4.103.251 to a non-listening service or daemon",
-	}
+    def update(self):
+        for line in self.update_csv(delimiter=';', quotechar='_'):
+            self.analyze(line)
 
-	def update(self):
-		for line in self.update_csv(delimiter=';', quotechar='_'):
-			self.analyze(line)
+        self.source = self.default_values['source']
 
-		self.source = self.default_values['source']
+    def analyze(self, line):
+        if not line or line[0].startswith("#"):
+            return
 
-	def analyze(self, line):
-		if not line or line[0].startswith("#"):
-			return
+        try:
+            ip, last_seen, honeypot_history, honeypot_history_totals = tuple(map(strip, line))
+            context = {
+                "last_seen": last_seen,
+                "honeypot_history": honeypot_history,
+                "honeypot_history_totals": honeypot_history_totals,
+                "source": self.name
+            }
 
-		try:
-			ip, last_seen, honeypot_history, honeypot_history_totals = tuple(map(strip, line))
-			context = {
-				"last_seen": last_seen,
-				"honeypot_history": honeypot_history,
-				"honeypot_history_totals": honeypot_history_totals,
-				"source": self.name
-			}
+            try:
+                ip = Ip.get_or_create(value=ip)
+                ip.add_context(context)
+                ip.add_source('feed')
+                ip.tag(['blocklist', 'portscan', 'honeypot'])
+            except ObservableValidationError as e:
+                logging.error("Invalid line: {}\nLine: {}".format(e, line))
 
-			try:
-				ip = Ip.get_or_create(value=ip)
-				ip.add_context(context)
-		ip.add_source('feed')
-		ip.tag(['blocklist', 'portscan', 'honeypot'])
-			except ObservableValidationError as e:
-				logging.error("Invalid line: {}\nLine: {}".format(e, line))
-
-		except ValueError:
-			logging.error("Error unpacking line: {}".format(line))
+        except ValueError:
+            logging.error("Error unpacking line: {}".format(line))
