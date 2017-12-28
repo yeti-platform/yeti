d404e5b78bb1ca42283800eadcf0f5763155ed5f
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-27 17:54:36 -0800

    Add CERT Austria Conflicker Domains

---
+++ plugins/feeds/public/cert_conflicker_domains.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+import logging
+
+from core.observables import Hostname
+from core.feed import Feed
+from core.errors import ObservableValidationError
+
+
+class CertConflickerDomains(Feed):
+    default_values = {
+        'frequency': timedelta(hours=24),
+        'source': 'http://www.cert.at/static/downloads/data/conficker/all_domains.txt',
+        'name': 'CertConflickerDomains',
+        'description': 'CERT (Computer Emergency Response Team) Austria Conflicker Domains'
+    }
+
+    def update(self):
+        for line in self.update_lines():
+            self.analyze(line)
+
+    def analyze(self, line):
+        if line.startswith('#'):
+            return
+
+        try:
+            line = line.strip()
+            parts = line.split()
+
+            hostname = str(parts[0]).strip()
+            context = {
+                'source': self.name
+            }
+
+            try:
+                hostname = Hostname.get_or_create(value=hostname)
+                hostname.add_context(context)
+                hostname.add_source('feed')
+                hostname.tag(['conflicker'])
+            except ObservableValidationError as e:
+                logging.error(e)
+        except Exception as e:
+            logging.debug(e)
