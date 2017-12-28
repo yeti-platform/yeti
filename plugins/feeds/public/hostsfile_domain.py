a93be70b92013c719ce49e91b74d9f3f0c173797
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-28 08:39:17 -0800

    Add hostsfile Malware, Fraud, Phishing, Spam, Exploits Feed

---
+++ plugins/feeds/public/hostsfile_domain.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+import logging
+
+from core.observables import Hostname
+from core.feed import Feed
+from core.errors import ObservableValidationError
+
+
+class HostsFileHosts(Feed):
+    default_values = {
+        'frequency': timedelta(hours=24),
+        'source': 'http://issviews.com/hphosts/hosts.txt',
+        'name': 'HostsFileHosts',
+        'description': 'Contains sites assoicated in; malware, fraud, phishing, spam, exploits, and/or phrama.  Contents come from hpHosts database.'
+    }
+
+    def update(self):
+        for line in self.update_lines():
+            self.analyze(line)
+
+    def analyze(self, line):
+        if line.startswith('#'):
+            return
+
+        try:
+            line = line.strip()
+            parts = line.split()
+
+            hostname = str(parts[1]).strip()
+            context = {
+                'source': self.name
+            }
+
+            try:
+                host = Hostname.get_or_create(value=hostname)
+                host.add_context(context)
+                host.add_source('feed')
+                host.tag(['blocklist', 'malware', 'fraud', 'phishing', 'spam', 'exploits', 'pharma'])
+            except ObservableValidationError as e:
+                logging.error(e)
+        except Exception as e:
+            logging.debug(e)
