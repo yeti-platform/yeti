3a45ca0f72f9816618f2342a6791c260b9e2ab45
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-30 14:41:46 -0800

    Added Hostfiles HFS Feed.

---
+++ plugins/feeds/public/hostsfile_hfs_domain.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+import logging
+
+from core.observables import Hostname
+from core.feed import Feed
+from core.errors import ObservableValidationError
+
+
+class HostsFileHFSDomains(Feed):
+    default_values = {
+        'frequency': timedelta(hours=1),
+        'source': 'https://hosts-file.net/hfs.txt',
+        'name': 'HostsFileHFSDomains',
+        'description': 'Contains spamming sites listed sites listed in the hpHosts database by Domain Name.'
+    }
+
+    def update(self):
+        for line in self.update_lines():
+            self.analyze(line)
+
+    def analyze(self, line):
+        if line.startswith('#'):
+            return
+
+        try:
+            hostname = str(parts[1]).strip()
+            context = {
+                'source': self.name
+            }
+
+            try:
+                host = Hostname.get_or_create(value=hostname)
+                host.add_context(context)
+                host.add_source('feed')
+                host.tag(['spam'])
+            except ObservableValidationError as e:
+                logging.error(e)
+        except Exception as e:
+            logging.debug(e)
