98e08123dd5b1e57e8167a345642d42563dd80e8
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-30 14:32:08 -0800

    Synatx fix (remove strip), Class Name

--- plugins/feeds/public/ci_army_blocklist_ip.py
+++ plugins/feeds/public/ci_army_blocklist_ip.py
@@ -6,12 +6,12 @@ from core.feed import Feed
 from core.errors import ObservableValidationError
 
 
-class CIArmyBlocklistip(Feed):
+class CIArmyBlocklistIP(Feed):
     default_values = {
         'frequency': timedelta(hours=4),
         'source': 'http://cinsscore.com/list/ci-badguys.txt',
-        'name': 'CIArmyBlocklistip',
-        'description': 'CINS Army IP Blocklist: The CINS Army list is a subset of the CINS Active Threat Intelligence ruleset, and consists of IP addresses that meet one of two basic criteria: 1) The IPs recent Rogue Packet score factor is very poor, or 2) The IP has tripped a designated number of trusted alerts across a given number of our Sentinels deployed around the world'
+        'name': 'CIArmyBlocklistIP',
+        'description': 'CINS Army IP Blocklist: The CINS Army list is a subset of the CINS Active Threat Intelligence ruleset, and consists of IP addresses that meet one of two basic criteria: 1) The IPs recent Rogue Packet score factor is very poor, or 2) The IP has tripped a designated number of trusted alerts across a given number of our Sentinels deployed around the world.'
     }
 
     def update(self):
@@ -23,9 +23,7 @@ class CIArmyBlocklistip(Feed):
             return
 
         try:
-            line = line.strip()
             parts = line.split()
-
             ip = str(parts[0]).strip()
             context = {
                 'source': self.name
