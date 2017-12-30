70df1f0b0a8129326127cb7b553e7cad0504d57e
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-30 15:07:10 -0800

    Added Hostsfile Misleading Marketing

---
+++ plugins/feeds/public/hostsfile_mmt_domain.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+import logging
+
+from core.observables import Hostname
+from core.feed import Feed
+from core.errors import ObservableValidationError
+
+
+class HostsFileMMTDomains(Feed):
+    default_values = {
+        'frequency': timedelta(hours=1),
+        'source': 'https://hosts-file.net/mmt.txt',
+        'name': 'HostsFileMMTDomains',
+        'description': 'Contains misleading marketing (e.g. fake Flash update adverts) sites listed in the hpHosts database.'
+    }
+
+    def update(self):
+        for line in self.update_lines():
+            self.analyze(line)
+
+    def analyze(self, line):
+        if line.startswith('#'):
+            return
+
+        try:
+            line = line.strip()
+            parts = line.split()
+
+            hostname = str(parts[1]).strip()
+            context = {
+                'source': self.name
+            }
+
+            try:
+                host = Hostname.get_or_create(value=hostname)
+                host.add_context(context)
+                host.add_source('feed')
+                host.tag(['misleading marketing'])
+            except ObservableValidationError as e:
+                logging.error(e)
+        except Exception as e:
+            logging.debug(e)
