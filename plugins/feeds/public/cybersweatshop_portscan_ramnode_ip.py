ac3df98a3aa9841c14a787b3a9042b8882cdac14
Vincent Passaro <vincent.passaro@gmail.com>
2017-12-30 14:33:11 -0800

    Tuple fix (remove strip)

--- plugins/feeds/public/cybersweatshop_portscan_ramnode_ip.py
+++ plugins/feeds/public/cybersweatshop_portscan_ramnode_ip.py
@@ -25,7 +25,7 @@ class CyberSweatShopPortscanRamnodeIP(Feed):
             return
 
         try:
-            ip, last_seen, honeypot_history, honeypot_history_totals = tuple(map(strip, line))
+            ip, last_seen, honeypot_history, honeypot_history_totals = tuple(line)
             context = {
                 "last_seen": last_seen,
                 "honeypot_history": honeypot_history,
