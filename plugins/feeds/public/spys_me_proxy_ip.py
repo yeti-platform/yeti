97acf56e8d48cb983e5e5ad2b700122f4131897a
Vincent Passaro <vincent.passaro@gmail.com>
2018-01-01 12:17:14 -0800

    Add Spysme Proxy Feed

---
+++ plugins/feeds/public/spys_me_proxy_ip.py
@@ -0,0 +1,48 @@
+from datetime import timedelta
+import logging
+
+from core.errors import ObservableValidationError
+from core.feed import Feed
+from core.observables import Ip
+
+
+class SpyMeAnonProxyIP(Feed):
+    default_values = {
+        'frequency': timedelta(hours=1),
+        'source': 'http://spys.me/proxy.txt',
+        'name': 'SpyMeAnonProxyIP',
+        'description': 'Spy.me Anonymous Proxy List: IP address:Port Country-Anonymity(Noa/Anm/Hia)-SSL_support(S)-Google_passed(+).'
+
+    }
+
+    def update(self):
+        for line in self.update_lines():
+            self.analyze(line)
+
+    def analyze(self, line):
+        if line.startswith('#'):
+            return
+
+        try:
+            line = line.replace(":", " ")
+            parts = line.split()
+            ip = str(parts[0]).strip()
+            port = str(parts[1]).strip()
+            spycountry = str(parts[2]).strip()
+            spygoogle = str(parts[3]).strip()
+            context = {
+                'source': self.name,
+                'port': port,
+                'country_anon_ssl': spycountry,
+                'google_passed': spygoogle
+            }
+
+            try:
+                ip = Ip.get_or_create(value=ip)
+                ip.add_context(context)
+                ip.add_source('feed')
+                ip.tag(['anonproxy'])
+            except ObservableValidationError as e:
+                logging.error(e)
+        except Exception as e:
+            logging.debug(e)
