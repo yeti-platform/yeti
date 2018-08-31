dc28351c4664f7124f46db5134652b912cbda1fc
Sebdraven <slarinier@gmail.com>
2018-08-31 17:20:27 +0200

    add stuff to make links between objects

--- plugins/feeds/public/otx_alienvault.py
+++ plugins/feeds/public/otx_alienvault.py
@@ -1,7 +1,10 @@
 from datetime import timedelta
+from urlparse import urlparse
 
 from core import Feed
 from core.config.config import yeti_config
+from core.entities import Exploit
+from core.observables import Hash, Hostname, Url
 
 
 class OTXAlienvault(Feed):
@@ -23,7 +26,10 @@ class OTXAlienvault(Feed):
             self.analyze(item)
 
     def analyze(self, item):
-        md5_indicators = OTXAlienvault.__choose_type_indic(item['indicators'],
+
+        context = dict(source=self.name)
+
+        md5_indic = OTXAlienvault.__choose_type_indic(item['indicators'],
                                                            'FileHash-MD5')
 
         sha256_indic = OTXAlienvault.__choose_type_indic(item['indicators'],
@@ -43,8 +49,62 @@ class OTXAlienvault(Feed):
         hostnames = OTXAlienvault.__choose_type_indic(item['indicators'],
                                                       'domains')
 
-        pass
+        exploits = OTXAlienvault.__choose_type_indic(item['indicatore'], 'CVE')
 
+        hostnames_obs = {
+        h['indicator']: Hostname.get_or_create(value=h['indicator']) for h in
+        hostnames}
+        urls_obs = {url['indicator']: Url.get_or_create(value=url['indicator'])
+                    for url in urls}
+        domains_obs = {
+        domain['indicator']: Hostname.get_or_create(value=domain['indicator'])
+        for domain in domains}
+
+        cve_obs = [Exploit.get_or_create(value=ex['indicator']) for ex in
+                   exploits]
+
+        tags = item['tags']
+
+        context['links'] = item['references']
+
+        sh256_obs = [Hash.get_or_create(value=sha256_f['indicator']) for
+                     sha256_f in sha256_indic]
+        md5_obs = [Hash.get_or_create(value=md5_f['indicator']) for md5_f in
+                   md5_indic]
+        sha1 = [Hash.get_or_create(value=sha1_f['indicator']) for sha1_f in
+                sha1_indic]
+
+    def __create_links_url_domains_hostnames(self, domains_obs, hostnames_obs,
+                                             urls_obs):
+        for url, ind in urls_obs.items():
+            self.__search_link_between_url_and_hostames(url, ind,
+                                                        domains_obs)
+            self.__search_link_between_url_and_hostames(url, ind,
+                                                        hostnames_obs)
+
+    def __search_link_between_url_and_hostames(self, url, ind, hostnames):
+        r = urlparse(url)
+        if r.netloc in list(hostnames.values()):
+            hostnames[r.netloc].activelink(ind, 'domain', self.name)
+        else:
+            for domain, obs in hostnames.items():
+                if domain in url:
+                    obs.activelink(ind, 'domains', self.name)
+
+    @staticmethod
+    def __add_contex(context, obsevables):
+        for o in obsevables:
+            o.add_context(context)
+
+    @staticmethod
+    def __add_source(observables):
+        for o in observables:
+            o.add_source('feed')
+
+    @staticmethod
+    def __add_tags(tags, observables):
+        for o in observables:
+            o.tag(tags)
     @staticmethod
     def __choose_type_indic(indicators, type_indic):
         return list(filter(lambda x: x['type'] == type_indic, indicators))
