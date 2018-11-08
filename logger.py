8f3633fcb517bf6d0cdf0ebf99ae6172e79da9cf
miniub <alexandrelim313@gmail.com>
2018-11-08 18:10:32 +0100

    Add configuration of userlogs file from configs

--- logger.py
+++ logger.py
@@ -2,13 +2,14 @@ import logging
 
 from logging import FileHandler
 from logging import Formatter
+from core.config.config import yeti_config
 
 LOG_FORMAT = (
     "%(asctime)s [%(levelname)s]: %(message)s")
 LOG_LEVEL = logging.INFO
 
 # user logger
-USER_LOG_FILE = "log"
+USER_LOG_FILE = yeti_config.get('logging', 'filename')
 
 
 userLogger = logging.getLogger("userLogger.messaging")
