f232f0f36751d467886b859b442b231757315dca
bdu <benoit.durivault@sekoia.fr>
2019-01-28 15:54:51 +0100

    api: add link fix

--- core/web/api/links.py
+++ core/web/api/links.py
@@ -6,7 +6,7 @@ from bson.json_util import loads
 from flask import request
 
 from core.web.api.crud import CrudSearchApi, CrudApi
-from core import database, observables
+from core import database, observables, entities, indicators
 from core.web.api.api import render
 from core.helpers import iterify
 from core.investigation import Investigation
@@ -76,15 +76,40 @@ class Link(CrudApi):
 
         :<json object params: JSON object containing object ids to link
         """
-
         params = request.json
+        type_src = params.pop('type_src', None)
+        type_dst = params.pop('type_dst', None)
+        db_objects_src = None
+        db_objects_dst = None
 
-        if params.pop('type_src') == 'observable':
-            db_objects = observables.Observable
+        if type_src == 'observable':
+            db_objects_src = observables.Observable
+        elif type_src == 'entity':
+            db_objects_src = entities.Entity
+        elif type_src == 'indicator':
+            db_objects_src = indicators.Indicator
 
-            src = db_objects.objects.get(id=params.pop("link_src"))
-            dst = db_objects.objects.get(id=params.pop("link_dst"))
-            link = src.link_to(dst, params.pop("description"), params.pop("source"))
+        if type_dst == 'observable':
+            db_objects_dst = observables.Observable
+        elif type_dst == 'entity':
+            db_objects_dst = entities.Entity
+        elif type_dst == 'indicator':
+            db_objects_dst = indicators.Indicator
+
+        if (db_objects_src is not None) and (db_objects_dst is not None):
+            src = db_objects_src.objects.get(id=params.pop("link_src"))
+            dst = db_objects_dst.objects.get(id=params.pop("link_dst"))
+
+            if (params.pop("first_seen", None) is not None) and \
+               (params.pop("last_seen", None) is not None):
+                link = src.link_to(dst,
+                                   params.pop("description"),
+                                   params.pop("source"),
+                                   params.pop("first_seen", None),
+                                   params.pop("last_seen", None))
+            else:
+                link = src.active_link_to(dst, params.pop("description"),
+                                          params.pop("source"))
 
         else:
             link = None
