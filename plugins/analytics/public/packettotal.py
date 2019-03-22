73d6efcac73b7ba3bc09997200c409f459918f53
threathive <adam@threathive.com>
2019-03-22 15:32:20 -0700

    Create packettotal.py
    A simple plugin to query packettotal.com for pcap files related to an indicator of interest. It will return a text node that has links to related iocs from the pcap analysis report.

---
+++ plugins/analytics/public/packettotal.py
@@ -0,0 +1,147 @@
+import json
+import logging
+
+import requests
+
+from core.analytics import OneShotAnalytics
+from core.errors import ObservableValidationError
+from core.observables import Hostname, Ip, Hash, Url, Text
+from core.config.config import yeti_config
+
+class PacketTotalAPI(object):
+    """Base class for querying the PacketTotal API."""
+    settings = {
+        'packettotal_api_key': {
+            'name': 'PacketTotal API Key',
+            'description': 'API Key provided by packettotal.com'
+        }
+    }
+
+    API = 'https://api.packettotal.com/v1/'
+
+    @staticmethod
+    def search(observable, api_key):
+        #"""
+        #:param observable: The extended observable class
+        #:param api_key: The api key obtained from PacketTotal
+        #:return:  packettotal json response or None if error
+        #"""
+
+        params = {'query': observable.value}
+
+        try:
+            res = requests.get(
+                '{}search'.format(PacketTotalAPI.API),
+                headers={'x-api-key': api_key},
+                params=params,
+                verify=False,
+                proxies=yeti_config.proxy
+            )
+
+            if res.ok:
+                return res.json()
+            else:
+                return None
+
+        except Exception as e:
+            logging.error('Exception while getting packettotal report {}'.format(e.message))
+            return None
+
+
+    @staticmethod
+    def fetch_analysis(pcap_id, api_key):
+        #"""
+        #:param observable: The extended observable class
+        #:param api_key: The api key obtained from PacketTotal
+        #:return:  packettotal json response or None if error
+        #"""
+
+        try:
+            res = requests.get(
+                '{}pcaps/{}/analysis'.format(PacketTotalAPI.API, pcap_id),
+                headers={'x-api-key': api_key},
+                verify=False,
+                proxies=yeti_config.proxy
+            )
+
+            if res.ok:
+                return res.json()
+            else:
+                return None
+
+        except Exception as e:
+            logging.error('Exception while getting packettotal report {}'.format(e.message))
+            return None
+
+
+class PacketTotalQuery(PacketTotalAPI, OneShotAnalytics):
+    default_values = {
+        'name': 'PacketTotal',
+        'description': 'Perform a PacketTotal query.',
+    }
+
+    ACTS_ON = ['Ip', 'Hostname', 'Hash', 'Url']
+
+    @staticmethod
+    def analyze(observable, results):
+        links = set()
+        pcap_hits = PacketTotalAPI.search(observable, results.settings['packettotal_api_key'])
+        result = {}
+
+        if pcap_hits:
+            for result in pcap_hits.get('results', []):
+                pcap_id = result.get('id','')
+                pcap_analysis = PacketTotalAPI.fetch_analysis(pcap_id,  results.settings['packettotal_api_key'])
+                if pcap_analysis:
+                    analysis_url = Text.get_or_create(
+                        value = 'https://packettotal.com/app/analysis?id={pcap_id}'.format(pcap_id=pcap_id)
+                    )
+                    links.update(
+                        observable.active_link_to(analysis_url, 'analysis_link', 'packettotal_query')
+                    )
+
+                    analysis = pcap_analysis.get('analysis_summary', {})
+                    ids_signatures = analysis.get("signatures", [])
+                    for signature in ids_signatures:
+                        o_sig = Text.get_or_create(value = signature)
+                        links.update(
+                            analysis_url.active_link_to(
+                                o_sig,
+                                'triggered_ids_signature',
+                                'packettotal_query'
+                            )
+                        )
+
+
+                    destination_addresses = analysis.get('top_talkers', {}).get('destination_ips', {})
+                    for dst_addr, percentage in destination_addresses.items():
+                        try:
+                            o_ip = Ip.get_or_create(value = dst_addr)
+                            links.update(
+                                analysis_url.active_link_to(
+                                    o_ip,
+                                    'potentially_related_ip',
+                                    'packettotal_query'
+                                )
+                            )
+                        except Exception as e:
+                            logging.error('hit an error when trying to create a new ip {}'.format(e.message))
+
+                    dns_queries = analysis.get('analysis_summary', {}).get('dns_statistics', {}).get('queries', {})
+                    for dns_query, percentage in dns_queries.items():
+                        try:
+                            o_hostname = Hostname.get_or_create(value = dns_query)
+                            links.update(
+                                analysis_url.active_link_to(
+                                    o_hostname, 
+                                    'potentially_related_hostname',
+                                    'packettotal_query'
+                                )
+                            )
+                        except Exception as e:
+                            logging.error('hit an error when trying to create a new ip {}'.format(e.message))
+
+        result['source'] = 'packettotal_query'
+        observable.add_context(result)
+
+        return list(links)
