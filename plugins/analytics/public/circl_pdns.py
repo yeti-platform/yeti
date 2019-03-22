af6fe71b3b85df59cd9e1515aff9dfa194d2d9a7
threathive <adam@threathive.com>
2019-03-22 06:34:55 -0700

    Update circl_pdns.py
    Updated indents / spaces ..

--- plugins/analytics/public/circl_pdns.py
+++ plugins/analytics/public/circl_pdns.py
@@ -11,37 +11,36 @@ class CirclPDNSApi(object):
         "circl_username": {
             "name": "Circl.lu username",
             "description": "Username for Circl.lu API."
-        },
+            },
         "circl_password": {
             "name": "Circl.lu password",
             "description": "Password for Circl.lu API."
-        }
+            }
     }
 
     @staticmethod
     def fetch(observable, settings):
-	auth = (
+        auth = (
             settings["circl_username"],
             settings["circl_password"]
         )
         API_URL = "https://www.circl.lu/pdns/query/"
         headers = {'accept': 'application/json'}
         results = []
-	r = requests.get(API_URL + observable.value, auth=auth , headers=headers, proxies=yeti_config.proxy)
+        r = requests.get(API_URL + observable.value, auth=auth , headers=headers, proxies=yeti_config.proxy)
         if r.ok:
-		for l in r.text.split('\n'):
-			if len(l) == 0:
-                    		return results
-	                else:
-        	            obj = json.loads(l)
-                	    results.append(obj)
+            for l in r.text.split('\n'):
+                if len(l) == 0:
+                    return results
+                else:
+                    obj = json.loads(l)
+                    results.append(obj)
         return results
-
 
 class CirclPDNSApiQuery(OneShotAnalytics, CirclPDNSApi):
     default_values = {
         "name": "Circl.lu PDNS",
-	      "group" : "PDNS",
+        "group" : "PDNS",
         "description": "Perform passive DNS lookups on domain names or ip address."
     }
 
@@ -51,40 +50,39 @@ class CirclPDNSApiQuery(OneShotAnalytics, CirclPDNSApi):
     def analyze(observable, results):
         links = set()
         json_result = CirclPDNSApi.fetch(observable, results.settings)
-
         json_string = json.dumps(
             json_result, sort_keys=True, indent=4, separators=(',', ': '))
-        results.update(raw=json_string)
 
+        results.update(raw=json_string)
         result = {}
-        result['source'] = 'Circl_pdns_query'
+        result['source'] = 'circl_pdns_query'
         result['raw'] = json_string
 
         if isinstance(observable, Ip):
-		for record in json_result:
-			new = Observable.add_text(record['rrname'])
-			new.add_source('analytics')
-			links.update(
-				observable.link_to(
-                	  	new,
-	                  	source='DNSDB Passive DNS',
-        	          	description='{} record'.format(record['rrtype']),
-	                	first_seen= datetime.fromtimestamp(record['time_first']),
-		                last_seen= datetime.fromtimestamp(record['time_last'])
-        		))
-			
-	elif isinstance(observable, Hostname):
-		for record in json_result:
-	        	new = Observable.add_text(record["rdata"])
-        	      	new.add_source('analytics')
-	              	links.update(
-        	        	observable.link_to(
-                        	new,
-	                      	source='DNSDB Passive DNS',
-        	              	description ='{} record'.format(record['rrtype']),
-        		        first_seen = datetime.fromtimestamp(record['time_first']),
-	                	last_seen = datetime.fromtimestamp(record['time_last'])
-			))
+            for record in json_result:
+                new = Observable.add_text(record['rrname'])
+                new.add_source('analytics')
+                links.update(
+                    observable.link_to(
+                        new,
+                        source='DNSDB Passive DNS',
+                        description='{} record'.format(record['rrtype']),
+                        first_seen= datetime.fromtimestamp(record['time_first']),
+                        last_seen= datetime.fromtimestamp(record['time_last'])
+                ))
 
-	observable.add_context(result)
-        return list(links)
+        elif isinstance(observable, Hostname):
+            for record in json_result:
+                new = Observable.add_text(record["rdata"])
+                new.add_source('analytics')
+                links.update(
+                    observable.link_to(
+                        new,
+                        source='DNSDB Passive DNS',
+                        description ='{} record'.format(record['rrtype']),
+                        first_seen = datetime.fromtimestamp(record['time_first']),
+                        last_seen = datetime.fromtimestamp(record['time_last'])
+                ))
+
+    observable.add_context(result)
+    return list(links)
