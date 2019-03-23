3a234cb0f4ad19d1c96fe29295fd0c439ca74dd1
threathive <adam@threathive.com>
2019-03-22 21:11:51 -0700

    Update haveibeenpwned.py
    Updated and ran linter

--- plugins/analytics/public/haveibeenpwned.py
+++ plugins/analytics/public/haveibeenpwned.py
@@ -1,11 +1,9 @@
 import json
 import logging
 
-import datetime
 import requests
 
 from core.analytics import OneShotAnalytics
-from core.errors import ObservableValidationError
 from core.observables import Email, Text
 
 
@@ -13,42 +11,44 @@ class HaveIBeenPwnedAPI(object):
     """Base class for querying the HaveIBeenPwned API."""
 
     API = 'https://haveibeenpwned.com/api/v2/'
-    HEADERS = { 'User-Agent' : 'Yeti Analytics Worker'}
+    HEADERS = {
+        'User-Agent': 'Yeti Analytics Worker'
+    }
 
     @staticmethod
     def search_breaches(observable):
         if isinstance(observable, Email):
-            url = '{api}breachedaccount/{account}'.format(api=HaveIBeenPwnedAPI.API, account=observable.value)
+            url = '{api}breachedaccount/{account}'.format(
+                api=HaveIBeenPwnedAPI.API, account=observable.value)
+            
             try:
-                res = requests.get(url,headers=HaveIBeenPwnedAPI.HEADERS)
+                res = requests.get(url, headers=HaveIBeenPwnedAPI.HEADERS)
                 if res.ok:
                     return res.json()
-
             except Exception as e:
-                logging.error('Exception while getting email report {}'.format(e.message))
-                return None
-
-
+                logging.error('Exception while getting \
+                    email report {}'.format(e.message))
 
     @staticmethod
     def search_pastes(observable):
         if isinstance(observable, Email):
-            url = '{api}pasteaccount/{account}'.format(api=HaveIBeenPwnedAPI.API,account=observable.value)
+            url = '{api}pasteaccount/{account}'.format(
+                api=HaveIBeenPwnedAPI.API, account=observable.value)
 
             try:
-                res = requests.get(url,headers=HaveIBeenPwnedAPI.HEADERS)
+                res = requests.get(url, headers=HaveIBeenPwnedAPI.HEADERS)
                 if res.ok:
                     return res.json()
-
             except Exception as e:
-                logging.error('Exception while getting email report {}'.format(e.message))
-                return None
+                logging.error(
+                    'Exception while getting \
+                     email report {}'.format(e.message))
 
 
 class HaveIBeenPwnedSearchBreaches(HaveIBeenPwnedAPI, OneShotAnalytics):
     default_values = {
         'name': 'HaveIBeenPwned-Breaches',
-	'group' : 'Search Leaks',
+        'group': 'Search Leaks',
         'description': 'Perform a HaveIBeenPwnedAPI query.',
     }
 
@@ -65,26 +65,29 @@ class HaveIBeenPwnedSearchBreaches(HaveIBeenPwnedAPI, OneShotAnalytics):
         result['source'] = 'HaveIBeenPwned_query'
         result['raw'] = json_string
 
-	for hit in json_result:
-		if hit.get('Name'):
-			tags = [ 'comprimised', hit.get('Name', '').lower() + '_breach' ]
-		        observable.tag(tags)
-                        o_breach = Text.get_or_create( value =  'Account compromised in {} breach on or around {}'.format(hit.get('Name', ''), hit.get('AddedDate', '') ) )
-                        links.update(
-                            observable.active_link_to(o_breach, 'found in breach', 'haveibeenpwned_hit')
-                        )
+        for hit in json_result:
+            if hit.get('Name'):
+                tags = ['comprimised', hit.get('Name', '').lower() + '_breach']
+                observable.tag(tags)
+                o_breach = Text.get_or_create(
+                    value='Account compromised in {} breach on or around {}'
+                    .format(
+                        hit.get('Name', ''),
+                        hit.get('AddedDate', '')))
 
+                links.update(
+                    observable.active_link_to(
+                        o_breach, 'found in breach', 'haveibeenpwned_hit')
+                )
 
         observable.add_context(result)
         return list(links)
 
 
-
-
 class HaveIBeenPwnedSearchPastes(HaveIBeenPwnedAPI, OneShotAnalytics):
     default_values = {
         'name': 'HaveIBeenPwned-Pastes',
-	'group' : 'Search Leaks',
+        'group': 'Search Leaks',
         'description': 'Perform a HaveIBeenPwnedAPI query.',
     }
 
@@ -96,20 +99,26 @@ class HaveIBeenPwnedSearchPastes(HaveIBeenPwnedAPI, OneShotAnalytics):
         json_result = HaveIBeenPwnedAPI.search_pastes(observable)
         json_string = json.dumps(
             json_result, sort_keys=True, indent=4, separators=(',', ': '))
+        
         results.update(raw=json_string)
         result = {}
         result['source'] = 'HaveIBeenPwned_query'
         result['raw'] = json_string
 
-	for hit in json_result:
-		if hit.get('Source'):
-			tags = [ 'comprimised', 'found_on_pastebin']
-		        observable.tag(tags)
-                        o_breach = Text.get_or_create( value =  'Account compromised found on pastebin on or around {} paste id {} , paste title {}'.format(hit.get('Date', '') , hit.get('Id', ''), hit.get('Title', '') ) )
-                        links.update(
-                            observable.active_link_to(o_breach, 'found on pastebin', 'haveibeenpwned_hit')
-                        )
-
+        for hit in json_result:
+            if hit.get('Source'):
+                tags = ['comprimised', 'found_on_pastebin']
+                observable.tag(tags)
+                o_breach = Text.get_or_create(
+                    value='Account compromised found \
+                    on pastebin on or around {} paste id {} , paste title {}'
+                    .format(
+                        hit.get('Date', ''),
+                        hit.get('Id', ''), hit.get('Title', '')))
+                links.update(
+                    observable.active_link_to(
+                        o_breach, 'found on pastebin', 'haveibeenpwned_hit')
+                )
 
         observable.add_context(result)
         return list(links)
