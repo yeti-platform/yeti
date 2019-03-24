54925e52bc77f631a09e27cda36ea74c2d4cc215
threathive <adam@threathive.com>
2019-03-23 23:31:57 -0700

    Update packettotal.py

--- plugins/analytics/public/packettotal.py
+++ plugins/analytics/public/packettotal.py
@@ -105,54 +105,54 @@ class PacketTotalQuery(PacketTotalAPI, OneShotAnalytics):
                     analysis_url, 'analysis_link', 'packettotal_query')
                 )
 
-                analysis = pcap_analysis.get('analysis_summary', {})
+            analysis = pcap_analysis.get('analysis_summary', {})
 
-                for signature in analysis.get('signatures', []):
-                    o_sig = Text.get_or_create(value=signature)
+            for signature in analysis.get('signatures', []):
+                o_sig = Text.get_or_create(value=signature)
+                links.update(
+                    analysis_url.active_link_to(
+                        o_sig,
+                        'triggered_ids_signature',
+                        'packettotal_query'
+                    )
+                )
+
+            destination_addresses = analysis.get(
+                'top_talkers', {}).get('destination_ips', {})
+
+            for dst_addr, percentage in destination_addresses.items():
+                try:
+                    o_ip = Ip.get_or_create(value=dst_addr)
                     links.update(
                         analysis_url.active_link_to(
-                            o_sig,
-                            'triggered_ids_signature',
+                            o_ip,
+                            'potentially_related_ip',
                             'packettotal_query'
                         )
                     )
+                except Exception as e:
+                    logging.error(
+                        'Error attempting to create IP {}'
+                        .format(e.message))
 
-                destination_addresses = analysis.get(
-                    'top_talkers', {}).get('destination_ips', {})
+            dns_queries = analysis.get(
+                'dns_statistics', {}).get('queries', {})
 
-                for dst_addr, percentage in destination_addresses.items():
-                    try:
-                        o_ip = Ip.get_or_create(value=dst_addr)
-                        links.update(
-                            analysis_url.active_link_to(
-                                o_ip,
-                                'potentially_related_ip',
-                                'packettotal_query'
-                            )
+            for dns_query, percentage in dns_queries.items():
+                try:
+                    o_hostname = Hostname.get_or_create(
+                        value=dns_query)
+                    links.update(
+                        analysis_url.active_link_to(
+                            o_hostname,
+                            'potentially_related_hostname',
+                            'packettotal_query'
                         )
-                    except Exception as e:
-                        logging.error(
-                            'Error attempting to create IP {}'
-                            .format(e.message))
-
-                dns_queries = analysis.get(
-                    'dns_statistics', {}).get('queries', {})
-
-                for dns_query, percentage in dns_queries.items():
-                    try:
-                        o_hostname = Hostname.get_or_create(
-                            value=dns_query)
-                        links.update(
-                            analysis_url.active_link_to(
-                                o_hostname,
-                                'potentially_related_hostname',
-                                'packettotal_query'
-                            )
-                        )
-                    except Exception as e:
-                        logging.error(
-                            'Error attempting to create hostname {}'
-                            .format(e.message))
+                    )
+                except Exception as e:
+                    logging.error(
+                        'Error attempting to create hostname {}'
+                        .format(e.message))
 
         result['source'] = 'packettotal_query'
         observable.add_context(result)
