a293be94dcb0d1f3fbdb67456f246300af59d1fb
threathive <adam@threathive.com>
2019-03-24 21:01:03 -0700

    Create circl_pdns.py (#365)
    * Create circl_pdns.py
    
    Simple PDNS plugin using Circl.lu pdns api. Requires api access to use.
    
    * Update circl_pdns.py
    
    Fixed indention weirdness..
    
    * Update circl_pdns.py
    
    fixed new vs observable variable name..
    
    * Update circl_pdns.py
    
    Fixed missing ) and spaces.
    
    * Update circl_pdns.py
    
    Updated indents / spaces ..
    
    * Update circl_pdns.py
    
    * Update circl_pdns.py
    
    Cleaning up
    
    * Update circl_pdns.py
    
    Updated and ran linter
    
    * Update plugins/analytics/public/circl_pdns.py
    
    Co-Authored-By: threathive <adam@threathive.com>
    
    * Update circl_pdns.py
    
    Cleaned up based on your suggestions.

---
+++ plugins/analytics/public/circl_pdns.py
@@ -0,0 +1,95 @@
+import json
+from datetime import datetime
+
+import requests
+
+from core.analytics import OneShotAnalytics
+from core.config.config import yeti_config
+from core.observables import Hostname, Ip, Observable
+
+
+class CirclPDNSApi(object):
+    settings = {
+        "circl_username": {
+            "name": "Circl.lu username",
+            "description": "Username for Circl.lu API."
+        },
+        "circl_password": {
+            "name": "Circl.lu password",
+            "description": "Password for Circl.lu API."
+        }
+    }
+
+    @staticmethod
+    def fetch(observable, settings):
+        auth = (
+            settings["circl_username"],
+            settings["circl_password"]
+        )
+        API_URL = "https://www.circl.lu/pdns/query/"
+        headers = {'accept': 'application/json'}
+        results = []
+        r = requests.get(API_URL + observable.value, auth=auth,
+                         headers=headers, proxies=yeti_config.proxy)
+        if r.ok:
+            for l in r.text.split('\n'):
+                if not len:
+                    return results
+
+                obj = json.loads(l)
+                results.append(obj)
+        return results
+
+
+class CirclPDNSApiQuery(OneShotAnalytics, CirclPDNSApi):
+    default_values = {
+        "name": "Circl.lu PDNS",
+        "group": "PDNS",
+        "description": "Perform passive DNS \
+        lookups on domain names or ip address."
+    }
+
+    ACTS_ON = ["Hostname", "Ip"]
+
+    @staticmethod
+    def analyze(observable, results):
+        links = set()
+        json_result = CirclPDNSApi.fetch(observable, results.settings)
+        json_string = json.dumps(
+            json_result, sort_keys=True, indent=4, separators=(',', ': '))
+
+        results.update(raw=json_string)
+        result = {}
+        result['source'] = 'circl_pdns_query'
+        result['raw'] = json_string
+
+        if isinstance(observable, Ip):
+            for record in json_result:
+                new = Observable.add_text(record['rrname'])
+                new.add_source('analytics')
+                links.update(
+                    observable.link_to(
+                        new,
+                        source='Circl.lu Passive DNS',
+                        description='{} record'.format(record['rrtype']),
+                        first_seen=datetime.fromtimestamp(
+                            record['time_first']),
+                        last_seen=datetime.fromtimestamp(record['time_last'])
+                    ))
+
+        elif isinstance(observable, Hostname):
+            for record in json_result:
+                new = Observable.add_text(record["rdata"])
+                new.add_source('analytics')
+                links.update(
+                    observable.link_to(
+                        new,
+                        source='Circl.lu Passive DNS',
+                        description='{} record'.format(record['rrtype']),
+                        first_seen=datetime.fromtimestamp(
+                            record['time_first']),
+                        last_seen=datetime.fromtimestamp(record['time_last'])
+                    ))
+
+        observable.add_context(result)
+        return list(links)
