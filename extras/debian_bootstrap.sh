63220fce28890a83581fca1b59ee70c5dfacf7ea
1maad <37084477+1maad@users.noreply.github.com>
2019-04-03 21:22:48 +0200

    Upgrade of node.js
    Without it, the yarn install fail on debian

--- extras/debian_bootstrap.sh
+++ extras/debian_bootstrap.sh
@@ -10,6 +10,9 @@ fi
 $APT update -y
 $APT install apt-transport-https dirmngr
 
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+$APT install nodejs -y
+
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
