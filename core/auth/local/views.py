288e4ee05f844c58ad145246a86d7f465665dfaf
doomedraven <doomedraven@users.noreply.github.com>
2019-04-14 12:43:54 +0200

    Update views.py

--- core/auth/local/views.py
+++ core/auth/local/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, flash, abort
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.security import check_password_hash
 
 from core.auth.local.user_management import authenticate, create_user, set_password
