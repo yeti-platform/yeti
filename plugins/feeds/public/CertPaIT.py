011106def63e277039527b2c8feb7e8a61b2b73e
doomedraven <doomedraven@users.noreply.github.com>
2019-05-11 21:00:17 +0200

    Update CertPaIT.py

--- plugins/feeds/public/CertPaIT.py
+++ plugins/feeds/public/CertPaIT.py
@@ -16,14 +16,16 @@ class CertPaIT(Feed):
     }
 
     regexes = (
-        r"IsDLL: (?P<isdll>\w+)",
-        r"Packers: (?P<packers>\w+)",
-        r"AntiDBG: (?P<antidbg>\w+)",
-        r"AntiVM: (?P<antivm>\w+)",
-        r"Signed: (?P<signed>\w+)",
-        r"XOR: (?P<xor>\w+)",
+        re.compile(r"IsDLL: (?P<isdll>\w+)"),
+        re.compile(r"Packers: (?P<packers>\w+)"),
+        re.compile(r"AntiDBG: (?P<antidbg>\w+)"),
+        re.compile(r"AntiVM: (?P<antivm>\w+)"),
+        re.compile(r"Signed: (?P<signed>\w+)"),
+        re.compile(r"XOR: (?P<xor>\w+)"),
     )
 
+    re_generic_details = re.compile('<p>Filename: <b>(?P<filename>.*)\\</b\\><br>Filetype: (?P<filetype>.*)</p>')
+    
     def update(self):
         for item in self.update_xml('item', ['title', 'link', 'pubDate', 'description']):
             self.analyze(item)
@@ -35,12 +37,11 @@ class CertPaIT(Feed):
         context['source'] = self.name
         context['url'] = item['link']
 
-        matched = re.match('<p>Filename: <b>(?P<filename>.*)\\</b\\><br>Filetype: (?P<filetype>.*)</p>', item['description'])
+        matched = re_generic_details.match(item['description'])
         if matched:
             context.update(matched.groupdict())
 
-        for regex in self.regexes:
-            regex_compiled = re.compile(regex)
+        for regex_compiled in self.regexes:
             matched = regex_compiled.search(item['description'])
             if matched:
                 context.update(matched.groupdict())
@@ -48,9 +49,8 @@ class CertPaIT(Feed):
         try:
             if md5:
                 hash_data = Hash.get_or_create(value=md5)
-                if hash_data.new is True or self.name not in hash_data.sources:
-                    hash_data.add_context(context)
-                    hash_data.add_source(self.name)
+                hash_data.add_context(context)
+                hash_data.add_source(self.name)
 
         except ObservableValidationError as e:
             logging.error(e)
