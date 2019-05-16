09ed0b9b0fbf602b09e3f9c434cae62c8e884617
doomedraven <doomedraven@users.noreply.github.com>
2019-05-16 21:45:07 +0200

    Update panunit42_github_iocs.py

--- plugins/feeds/public/panunit42_github_iocs.py
+++ plugins/feeds/public/panunit42_github_iocs.py
@@ -8,6 +8,15 @@ from core.observables import Hash, Url, Hostname, Ip, MacAddress, Email, Certifi
 from core.errors import ObservableValidationError
 from core.config.config import yeti_config
 
+BLACKLIST_DOMAINS = [
+    'technet.microsoft.com',
+    'cloudblogs.microsoft.com',
+    'capec.mitre.org',
+    'attack.mitre.org',
+    'securelist.com',
+    'blog.avast.com',
+]
+
 class PanUnit42GithubIocs(Feed):
 
     default_values = {
@@ -27,7 +36,6 @@ class PanUnit42GithubIocs(Feed):
     }
 
     blacklist = ('Makefile', 'LICENSE', 'README.adoc')
-    blacklist_domains = ('technet.microsoft.com', 'cloudblogs.microsoft.com', 'capec.mitre.org',  'attack.mitre.org', 'securelist.com', 'blog.avast.com')
 
     def process_content(self, content, filename):
         context = dict(source=self.name)
@@ -49,19 +57,18 @@ class PanUnit42GithubIocs(Feed):
                 logging.error(e)
                 return
 
-            if observables:
-                for key in observables:
-                    for ioc in filter(None, observables[key]):
-                        if key == 'Url' and  any([domain in ioc for domain in self.blacklist_domains]):
-                            continue
-                        try:
-                            ioc_data = self.refs[key].get_or_create(value=ioc)
-                            ioc_data.add_context(context)
-                            ioc_data.add_source(self.name)
-                        except ObservableValidationError as e:
-                             logging.error(e)
-                        except UnicodeDecodeError as e:
+            for key in observables:
+                for ioc in filter(None, observables[key]):
+                    if key == 'Url' and  any([domain in ioc for domain in BLACKLIST_DOMAINS]):
+                        continue
+                    try:
+                        ioc_data = self.refs[key].get_or_create(value=ioc)
+                        ioc_data.add_context(context)
+                        ioc_data.add_source(self.name)
+                    except ObservableValidationError as e:
                             logging.error(e)
+                    except UnicodeDecodeError as e:
+                        logging.error(e)
 
     def update(self):
         for content in self.update_github():
