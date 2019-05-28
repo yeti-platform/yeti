8ae6c35bb091c38afdba5d39aaf7bdd6141121bb
doomedraven <doommedraven@gmail.com>
2019-05-28 12:48:53 +0200

    remove helpers.py

--- core/helpers.py
+++ core/helpers.py
@@ -28,7 +28,7 @@ def refang(url):
     substitutes = ('me[o0]w', 'h..p')
     schema_re = re.compile(
         "^(?P<fake>{})(?P<real>s?://)".format("|".join(substitutes)))
-    domain_re = re.compile(r"(\[\.\]|\[\.|\.\]|,)")
+    domain_re = re.compile(r"(\[\.\]|,)")
     url = schema_re.sub(http, url)
     url = domain_re.sub(".", url)
     return url
