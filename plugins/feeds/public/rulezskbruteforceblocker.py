44da906437d4230f85e9d4303b320ea05fd73a42
doomedraven <doomedraven@users.noreply.github.com>
2019-05-28 14:06:57 +0200

    Create rulezskbruteforceblocker.py

---
+++ plugins/feeds/public/rulezskbruteforceblocker.py
@@ -0,0 +1,42 @@
+import logging
+from dateutil import parser
+from datetime import datetime, timedelta
+from core.observables import Ip
+from core.feed import Feed
+from core.config.config import yeti_config
+from core.errors import ObservableValidationError
+
+class RulezSKBruteforceBlocker(Feed):
+
+    default_values = {
+        "frequency": timedelta(hours=24),
+        "name": "RulezSKBruteforceBlocker",
+        "source": "http://danger.rulez.sk/projects/bruteforceblocker/blist.php",
+        "description": "This feed contains daily list of IPs from rules.sk",
+    }
+
+    def update(self):
+        since_last_run = datetime.now() - self.frequency
+        try:
+            r = self._make_request(headers={"User-Agent": "yeti-project"})
+            lines = r.content.splitlines()[1:-1]
+            for line in lines:
+                ip, date, count, id = filter(None, line.split("\t"))
+                ip_date = parser.parse(date.replace("# ", ""))
+                if ip_date > since_last_run:
+                    self.analyze(ip, ip_date, line)
+        except Exception as e:
+            logging.error(e)
+
+    def analyze(self, ip, date, raw):
+        context = {}
+        context['date_added'] = date
+        context['source'] = self.name
+        context['raw'] = raw
+
+        try:
+            ip = Ip.get_or_create(value=ip)
+            ip.add_context(context)
+            ip.add_source(self.name)
+        except ObservableValidationError as e:
+            logging.error(e)
