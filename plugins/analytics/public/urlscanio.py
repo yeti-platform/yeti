ffe68b6310926a4a7086f81e15e82cadf53e1347
doomedraven <doomedraven@users.noreply.github.com>
2019-05-30 13:25:22 +0200

    Update urlscanio.py

--- plugins/analytics/public/urlscanio.py
+++ plugins/analytics/public/urlscanio.py
@@ -1,22 +1,15 @@
 import json
 import requests
 from core.analytics import OneShotAnalytics
-from core.observables import Hash, Ip, Hostname, Url, Text, AutonomousSystem
+from core.observables import Hash, Ip, Hostname, Url, Text
 from core.config.config import yeti_config
 from core.errors import GenericYetiError, ObservableValidationError
 
 class UrlScanIoApi(object):
     """
         https://urlscan.io/about-api/
+    """
 
-
-    settings = {
-        "urlscanio_api_key": {
-            "name": "UrlScanIO API Key",
-            "description": "API Key provided by urlscan.io."
-        }
-    }
-        """
     API_URL = "https://urlscan.io/api/v1/search/"
 
     @staticmethod
@@ -24,44 +17,45 @@ class UrlScanIoApi(object):
         links = set()
 
         for page in json_result:
-            if page.get("page"):
+            if not page.get("page"):
+                continue
 
-                if not isinstance(observable, Ip) and page['page'].get('ip'):
-                    new_ip = Ip.get_or_create(value=page['page']['ip'])
-                    links.update(
-                        new_ip.active_link_to(observable,
-                            'ip', 'UrlScanIo Query')
-                    )
+            if not isinstance(observable, Ip) and page['page'].get('ip'):
+                new_ip = Ip.get_or_create(value=page['page']['ip'])
+                links.update(
+                    new_ip.active_link_to(observable,
+                        'ip', 'UrlScanIo Query')
+                )
 
-                if not isinstance(observable, Hostname) and page['page'].get('domain'):
-                    new_host = Hostname.get_or_create(value=page['page']['domain'])
-                    links.update(
-                        new_host.active_link_to(observable,
-                            'hostname', 'UrlScanIo Query')
-                    )
+            if not isinstance(observable, Hostname) and page['page'].get('domain'):
+                new_host = Hostname.get_or_create(value=page['page']['domain'])
+                links.update(
+                    new_host.active_link_to(observable,
+                        'hostname', 'UrlScanIo Query')
+                )
 
-                if not isinstance(observable, Url) and page['page'].get('url'):
-                    new_url = Url.get_or_create(value=page['page']['url'])
-                    links.update(
-                        new_url.active_link_to(observable,
-                            'url', 'UrlScanIo Query')
-                    )
+            if not isinstance(observable, Url) and page['page'].get('url'):
+                new_url = Url.get_or_create(value=page['page']['url'])
+                links.update(
+                    new_url.active_link_to(observable,
+                        'url', 'UrlScanIo Query')
+                )
 
-                if page['page'].get('asn'):
-                    asn = AutonomousSystem.get_or_create(value=page['page']['asn'])
-                    links.update(asn.active_link_to(observable, 'asn#', 'UrlScanIo Query'))
+            if page['page'].get('asn'):
+                asn = Text.get_or_create(value=page['page']['asn'])
+                links.update(asn.active_link_to(observable, 'asn#', 'UrlScanIo Query'))
 
-                if page['page'].get('asnname'):
-                    asnname = Text.get_or_create(value=page['page']['asnname'])
-                    links.update(asnname.active_link_to(observable,
-                        'asn_name', 'UrlScanIoQuerycanIo Query')
-                    )
+            if page['page'].get('asnname'):
+                asnname = Text.get_or_create(value=page['page']['asnname'])
+                links.update(asnname.active_link_to(observable,
+                    'asn_name', 'UrlScanIoQuerycanIo Query')
+                )
 
-                if page['page'].get('server'):
-                    server = Text.get_or_create(value=page['page']['server'])
-                    links.update(server.active_link_to(observable,
-                        'server', 'UrlScanIo Query')
-                    )
+            if page['page'].get('server'):
+                server = Text.get_or_create(value=page['page']['server'])
+                links.update(server.active_link_to(observable,
+                    'server', 'UrlScanIo Query')
+                )
 
         return list(links)
 
