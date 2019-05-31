ba94c5177a7f4a8042e7912c8516bf8cd8ed5cf0
doomedraven <doomedraven@users.noreply.github.com>
2019-05-31 11:23:12 +0200

    Create unlock.py
    The diff with current unlock is that this unlock only 1 feed instead of all, and that can generate some problems

---
+++ tests/unlock.py
@@ -0,0 +1,22 @@
+import sys
+import logging
+from os import path
+
+YETI_ROOT = path.normpath(path.dirname(path.dirname(path.abspath(__file__))))
+sys.path.append(YETI_ROOT)
+
+from core.feed import Feed, update_feed
+from core.scheduling import Scheduler
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    Scheduler()
+
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+        f = Feed.objects.get(name=name)
+        print "Running {}...".format(f.name)
+        f.lock = False
+        f.save()
+        print("{} unlocked".format(f.name))
