2dfda7492a230f30e00d6dedb5c0b42d9b2f49cf
Thomas Chopitea <tomchop@gmail.com>
2019-06-09 13:29:36 +0200

    fallback dir

--- core/logger.py
+++ core/logger.py
@@ -1,4 +1,7 @@
+"""Set up logging for Yeti."""
+
 import logging
+import os
 
 from logging import FileHandler
 from logging import Formatter
@@ -10,6 +13,9 @@ LOG_LEVEL = logging.INFO
 
 # user logger
 USER_LOG_FILE = yeti_config.get('logging', 'filename')
+# Fall back to tmp if the logging directory does not exist
+if not os.path.isdir(os.path.dirname(USER_LOG_FILE)):
+    USER_LOG_FILE = '/tmp/yeti.log'
 
 
 userLogger = logging.getLogger("userLogger.messaging")
