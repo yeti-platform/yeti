2f2a8832a97a2b18ff12be5ae795aa792fc4da61
doomedraven <doommedraven@gmail.com>
2019-06-22 13:38:56 +0200

    no gid no problem

--- core/web/api/groupadmin.py
+++ core/web/api/groupadmin.py
@@ -9,17 +9,17 @@ class GroupAdminSearch(CrudSearchApi):
     template = 'group_api.html'
     objectmanager = Group
 
-    @route('/toggle/<gid>', methods=["POST"])
+    @route('/toggle/<id>', methods=["POST"])
     @requires_role('admin')
     def toggle(self, id):
-        group = get_object_or_404(Group, id=gid)
+        group = get_object_or_404(Group, id=id)
         group.enabled = not group.enabled
         group.save()
-        return render({"enabled": group.enabled, "id": gid})
+        return render({"enabled": group.enabled, "id": id})
 
-    @route('/remove/<gid>', methods=["POST"])
+    @route('/remove/<id>', methods=["POST"])
     @requires_role('admin')
     def remove(self, id):
-        group = get_object_or_404(Group, id=gid)
+        group = get_object_or_404(Group, id=id)
         group.delete()
-        return render({"id": gid})
+        return render({"id": id})
