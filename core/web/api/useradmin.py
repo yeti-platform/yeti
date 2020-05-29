3acf2e84692bfe4c1d5376ca6d75056b9d6e06be
Thomas Chopitea <tomchop@gmail.com>
2020-05-29 21:36:19 +0200

    Only admins can list roles

--- core/web/api/useradmin.py
+++ core/web/api/useradmin.py
@@ -1,13 +1,50 @@
 from flask_classy import route
 
 from core.web.api.crud import CrudSearchApi, CrudApi
+from flask import request, url_for, abort, send_file, make_response
+from flask_classy import FlaskView, route
+from flask_login import current_user
 from core.user import User
 from core.web.api.api import render
 from core.web.helpers import requires_role, get_object_or_404
+from core.web.helpers import requires_permissions
+from mongoengine.errors import InvalidQueryError
 
 
 class UserAdminSearch(CrudSearchApi):
     template = 'user_api.html'
+    objectmanager = User
+
+    @requires_role('admin')
+    def post(self):
+        """Launches a simple search against the database
+
+        Overwrites the default behavior of CrudSearchApi to exclude
+        sensitive user details.
+
+        This endpoint is mostly used by paginators in Yeti.
+
+        :<json object params: JSON object specifying the ``page``, ``range`` and ``regex`` variables.
+        :<json integer params.page: Page or results to return (default: 1)
+        :<json integer params.range: How many results to return (default: 50)
+        :<json boolean params.regex: Set to true if the arrays in ``filter`` are to be treated as regular expressions (default: false)
+        :<json object filter: JSON object specifying keys to be matched in the database. Each key must contain an array of OR-matched values.
+
+        :reqheader Accept: must be set to ``application/json``
+        :reqheader Content-Type: must be set to ``application/json``
+
+        """
+        query = request.get_json(silent=True) or {}
+
+        try:
+            data = self.search(query)
+        except InvalidQueryError:
+            abort(400)
+
+        return render(data, self.template)
+
+class UserAdmin(FlaskView):
+
     objectmanager = User
 
     @route('/remove/<id>', methods=["POST"])
@@ -16,10 +53,6 @@ class UserAdminSearch(CrudSearchApi):
         user = get_object_or_404(User, id=id)
         user.delete()
         return render({"id": id})
-
-class UserAdmin(CrudApi):
-
-    objectmanager = User
 
     @route('/toggle/<id>', methods=["POST"])
     @requires_role('admin')
