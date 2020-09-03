4e80a25df54d4486f692b1cf1e7469515e2d5a48
Thomas Roccia <thomas.roccia@gmail.com>
2020-09-03 15:49:06 +0200

    fixing missing parentheses

--- tests/testfeeds.py
+++ tests/testfeeds.py
@@ -14,13 +14,13 @@ if __name__ == '__main__':
     Scheduler()
 
     if len(sys.argv) == 1:
-        print "Re-run using a feed name as argument"
+        print("Re-run using a feed name as argument")
         for f in Feed.objects():
-            print "  {}".format(f.name)
+            print("  {}".format(f.name))
 
     if len(sys.argv) > 1:
         name = sys.argv[1]
         f = Feed.objects.get(name=name)
-        print "Running {}...".format(f.name)
+        print("Running {}...".format(f.name))
         if update_feed(f.id):
-            print "{}: success!".format(f.name)
+            print("{}: success!".format(f.name))
