53c91fac5e38491ab7d88290915fad5337959048
dumprop <34004367+dumprop@users.noreply.github.com>
2021-08-15 22:49:00 +0300

    add botvrij feeds

---
+++ plugins/feeds/public/botvrij_sha1.py
@@ -0,0 +1,34 @@
+import logging
+from datetime import timedelta
+from core.errors import ObservableValidationError
+from core.feed import Feed
+from core.observables import Hash
+
+
+class BotvrijSHA1(Feed):
+
+    default_values = {
+        "frequency": timedelta(hours=12),
+        "name": "BotvrijSHA1",
+        "source": "https://www.botvrij.eu/data/ioclist.sha1",
+        "description": "File hashes that can be used when doing incident response.",
+    }
+
+    def update(self):
+        resp = self._make_request(sort=False)
+        lines = resp.content.decode("utf-8").split("\n")[6:-1]
+        for url in lines:
+            self.analyze(url.strip())
+
+    def analyze(self, line):
+        val, descr = line.split(" # sha1 - ")
+
+        context = {"source": self.name, "description": descr}
+
+        try:
+            obs = Hash.get_or_create(value=val)
+            obs.add_context(context)
+            obs.add_source(self.name)
+            obs.tag("botvrij")
+        except ObservableValidationError as e:
+            raise logging.error(e)
