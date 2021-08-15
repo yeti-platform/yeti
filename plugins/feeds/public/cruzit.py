fc281dc19b00c2f5e9e19f22c7a39f1a6e096063
dumprop <34004367+dumprop@users.noreply.github.com>
2021-08-15 22:53:20 +0300

    add cruzit feed

---
+++ plugins/feeds/public/cruzit.py
@@ -0,0 +1,36 @@
+import logging
+from datetime import timedelta
+from core.errors import ObservableValidationError
+from core.feed import Feed
+from core.observables import Ip
+
+
+class BlocklistdeAll(Feed):
+
+    default_values = {
+        "frequency": timedelta(hours=13),
+        "name": "Cruzit",
+        "source": "https://iplists.firehol.org/files/cruzit_web_attacks.ipset",
+        "description": "IPs of compromised machines scanning for vulnerabilities and DDOS attacks.",
+    }
+
+    def update(self):
+        resp = self._make_request(sort=False)
+        lines = resp.content.decode("utf-8").split("\n")[63:]
+        for line in lines:
+            self.analyze(line)
+
+    def analyze(self, line):
+        line = line.strip()
+
+        ip = line
+
+        context = {"source": self.name}
+
+        try:
+            obs = Ip.get_or_create(value=ip)
+            obs.add_context(context)
+            obs.add_source(self.name)
+            obs.tag("blocklistde")
+        except ObservableValidationError as e:
+            raise logging.error(e)
