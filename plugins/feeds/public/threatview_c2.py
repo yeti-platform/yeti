848455fb22fa50d190f18a6fe82369b5d7029fe7
dumprop <34004367+dumprop@users.noreply.github.com>
2021-08-27 18:56:31 +0300

    fix threatview script
    ip and hostname are supported

--- plugins/feeds/public/threatview_c2.py
+++ plugins/feeds/public/threatview_c2.py
@@ -2,37 +2,41 @@ import logging
 from datetime import timedelta
 from core.errors import ObservableValidationError
 from core.feed import Feed
-from core.observables import Ip
+from core.observables import Ip, Hostname
+import re
 
 
-class ThreatviewC2_IP(Feed):
+class ThreatviewC2(Feed):
 
     default_values = {
         "frequency": timedelta(hours=13),
-        "name": "ThreatviewC2_IP",
+        "name": "ThreatviewC2",
         "source": "https://threatview.io/Downloads/High-Confidence-CobaltstrikeC2_IP_feed.txt",
         "description": "Infrastructure hosting Command & Control Servers found during Proactive Hunt by Threatview.io",
     }
 
     def update(self):
         resp = self._make_request(sort=False)
-        lines = resp.content.decode("utf-8").split("\n")[2:]
+        lines = resp.content.decode("utf-8").split("\n")[2:-1]
         for line in lines:
             self.analyze(line)
 
     def analyze(self, line):
         line = line.strip()
 
-        ip = line
-
         context = {"source": self.name}
 
         try:
-            obs = Ip.get_or_create(value=ip)
+            if re.match(r'^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$', line):
+                obs = Ip.get_or_create(value=line)
+            elif re.match(r'^\w{1,}\.\w{2,}$', line):
+                obs = Hostname.get_or_create(value=line)
+            else:
+                return
             obs.add_context(context)
             obs.add_source(self.name)
             obs.tag("threatview")
             obs.tag("cobalt_strike")
             obs.tag("c2")
         except ObservableValidationError as e:
-            raise logging.error(e)
+            logging.error(e)
