9f7e792ff5749ed5e0d531f654a2012fc2025a8c
Sebastien Larinier <sebdraven@protonmail.com>
2021-11-26 15:38:19 +0100

    Update passive_dns.py
    finished review

--- plugins/feeds/public/passive_dns.py
+++ plugins/feeds/public/passive_dns.py
@@ -23,7 +23,7 @@ class PassiveDNS(Feed):
         url = yeti_config.get("passivedns", "url")
         pdns = api.Api_PDNS(url)
         pdns.connect(login, password)
-
+        
         r = pdns.get_alerts(limit=100)
 
         df = pd.DataFrame(r["data"])
@@ -36,16 +36,19 @@ class PassiveDNS(Feed):
         context_domain = dict(source=self.name)
         context_ip = dict(source=self.name)
 
-        domain_name = Hostname.get_or_create(value=item["Domain Name"])
+        domain_name = Hostname.get_or_create(value=item["Domain name"])
         ip = Ip.get_or_create(value=item["Current IP address"])
         infos_ip = pdns.get_reverse(item["Current IP address"])
-        infos_domain = pdns.get_records(item["Domain Name"])
-
-        company = Company.get_or_create(value=infos_domain["ip"]["organization"])
-        context_ip["ISP"] = infos_ip["ip"]["ISP"]
-        context_ip["country"] = infos_ip["ip"]["country"]
-        context_ip["last_updated"] = infos_ip["last_updated_at"]
-        context_ip["first_updated"] = infos_ip["first_updated_at"]
+        infos_domain = pdns.get_records(item["Domain name"])
+        
+        infos = list(filter(lambda x:x['domain_name'] == item["Domain name"] ,
+        infos_ip['resolution_list']))[0]
+        
+        company = Company.get_or_create(name=infos_domain["ip"]["location"]["organization"])
+        context_ip["ISP"] = infos_domain["ip"]["location"]["ISP"]
+        context_ip["country"] = infos_domain["ip"]["location"]["country"]
+        context_ip["last_updated"] = infos["last_updated_at"]
+        context_ip["first_updated"] = infos["first_updated_at"]
 
         ip.active_link_to(company, "compagny", self.name)
 
@@ -53,10 +56,10 @@ class PassiveDNS(Feed):
 
         context_domain["last updated"] = "%s : %s" % (
             ip.value,
-            infos_ip["last_updated_at"],
+            infos["last_updated_at"],
         )
 
-        ns_servers = list(filter(lambda x: x["type"] == "NS", infos_domain["records"]))
+        ns_servers = filter(lambda x: x["type"] == "NS", infos_domain["dn"]["records"])
         for name_server in ns_servers:
             ns_serv = Hostname.get_or_create(value=name_server["target"])
             ns_serv.active_link_to(domain_name, "NS", self.name, clean_old=False)
