6bce4e0453e6186b5a13912df2bf8bad188a25d8
mbonino <42541934+mbonino@users.noreply.github.com>
2021-12-28 12:45:41 +0100

    Lint

--- tests/testScheduledAnalytics.py
+++ tests/testScheduledAnalytics.py
@@ -1,12 +1,12 @@
+"""Run scheduled jobs."""
 import sys
 import logging
 from os import path
+from core.analytics import ScheduledAnalytics
+from core.scheduling import Scheduler
 
 YETI_ROOT = path.normpath(path.dirname(path.dirname(path.abspath(__file__))))
 sys.path.append(YETI_ROOT)
-
-from core.analytics import ScheduledAnalytics
-from core.scheduling import Scheduler
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Re-run using a analytic name as argument")
         for f in ScheduledAnalytics.objects():
-            print("  {}".format(f.name))
+            f"   {f.name}"
 
     if len(sys.argv) > 1:
         name = sys.argv[1]
         f = ScheduledAnalytics.objects.get(name=name)
-        print("Running {}...".format(f.name))
+        f"Running {f.name}..."
         f.analyze_outdated()
