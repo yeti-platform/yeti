d3f270c31dc31f8b2d76e7ba4729e9564a8999ff
Thomas Chopitea <tomchop@gmail.com>
2023-02-28 18:31:23 +0000

    Breaking inits

--- core/observables/__init__.py
+++ core/observables/__init__.py
@@ -1,17 +1,17 @@
 from mongoengine import *
 
-from core.observables.tag import ObservableTag, Tag
-from core.observables.observable import Observable
-from core.observables.ip import Ip
-from core.observables.asn import AutonomousSystem
-from core.observables.url import Url
-from core.observables.hostname import Hostname
-from core.observables.hash import Hash
-from core.observables.file import File
-from core.observables.certificate import Certificate, CertificateSubject
-from core.observables.email import Email
-from core.observables.text import Text
-from core.observables.bitcoin import Bitcoin
-from core.observables.path import Path
-from core.observables.mac_address import MacAddress
-from core.observables.helpers import register_certificate, register_observables
+# from core.observables.tag import ObservableTag, Tag
+# from core.observables.observable import Observable
+# from core.observables.ip import Ip
+# from core.observables.asn import AutonomousSystem
+# from core.observables.url import Url
+# from core.observables.hostname import Hostname
+# from core.observables.hash import Hash
+# from core.observables.file import File
+# from core.observables.certificate import Certificate, CertificateSubject
+# from core.observables.email import Email
+# from core.observables.text import Text
+# from core.observables.bitcoin import Bitcoin
+# from core.observables.path import Path
+# from core.observables.mac_address import MacAddress
+# from core.observables.helpers import register_certificate, register_observables
