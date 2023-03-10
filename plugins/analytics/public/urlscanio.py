f74efbed4c1dcf75b489e4d53ff074ebff461626
Sebdraven <sebdraven@protonmail.com>
2023-03-10 17:23:59 +0100

    Update urlscanio.py
    fixe urlscanio

--- plugins/analytics/public/urlscanio.py
+++ plugins/analytics/public/urlscanio.py
@@ -88,10 +88,9 @@ class UrlScanIoApi(object):
     @staticmethod
     def fetch(observable):
         types = {
-            "Ip": 'ip:"{}"',
-            "Hostname": 'domain:"{}"',
-            "Url": 'url:"{}"',
-            "Hash": 'hash:"{}"',
+            "ip": 'ip:"{}"',
+            "hostname": 'domain:"{}"',
+            "hash": 'hash:"{}"',
         }
 
         params = {"q": types[observable.type].format(observable.value)}
