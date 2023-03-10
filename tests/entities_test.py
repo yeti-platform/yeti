f2df4bfa8c557179673515f2eda233023514b738
Sebdraven <sebdraven@protonmail.com>
2023-03-10 14:58:09 +0100

    remove exploit_kit entities

--- tests/entities_test.py
+++ tests/entities_test.py
@@ -1,7 +1,6 @@
 import unittest
 import sys
 from os import path
-from datetime import timedelta
 from mongoengine import connect
 from pyeti import YetiApi
 import os
@@ -10,8 +9,6 @@ YETI_ROOT = path.normpath(path.dirname(path.dirname(path.abspath(__file__))))
 sys.path.append(YETI_ROOT)
 
 from core.config.config import yeti_config
-from core.entities.malware import Malware
-from core.entities import Exploit, ExploitKit, Actor, Campaign, Company, TTP
 
 from core.user import User
 
@@ -31,39 +28,39 @@ class EntityTest(unittest.TestCase):
 
     def test_malware(self):
         malware_name = "test_malware"
-        malware = self.yeti_client.entity_add(name=malware_name, type_entity="Malware")
+        malware = self.yeti_client.add_malware(name=malware_name)
         malware_added = self.yeti_client.entity_search(name=malware_name)
         self.assertEqual(malware_added[0]["name"], malware_name)
 
     def test_actor(self):
         actor_name = "test_actor"
-        actor = self.yeti_client.entity_add(name=actor_name, type_entity="Actor")
+        actor = self.yeti_client.add_actor(name=actor_name)
         actor_added = self.yeti_client.entity_search(name=actor_name)
         self.assertEqual(actor_added[0]["name"], actor_name)
 
 
     def test_campaign(self):
         campaign_name = "test_campaign"
-        campaign = self.yeti_client.entity_add(name=campaign_name, type_entity="Campaign")
+        campaign = self.yeti_client.add_campaign(name=campaign_name)
         campaign_added = self.yeti_client.entity_search(name=campaign_name)
         self.assertEqual(campaign_added[0]["name"], campaign_name)
 
     def test_company(self):
         company_name = "test_company"
-        company = self.yeti_client.entity_add(name=company_name, type_entity="Company")
+        company = self.yeti_client.add_company(name=company_name)
         company_added = self.yeti_client.entity_search(name=company_name)
         self.assertEqual(company_added[0]["name"], company_name)
 
 
     def test_exploit(self):
         exploit_name = "test_exploit"
-        exploit = self.yeti_client.entity_add(name=exploit_name, type_entity="Exploit")
+        exploit = self.yeti_client.add_exploit(name=exploit_name)
         exploit_added = self.yeti_client.entity_search(name=exploit_name)
         self.assertEqual(exploit_added[0]["name"], exploit_name)
 
     def test_ttp(self):
         ttp_name = "test_ttp"
-        ttp = self.yeti_client.entity_add(name=ttp_name, type_entity="TTP",killchain="")
+        ttp = self.yeti_client.add_ttp(name=ttp_name, killchain="Reconnaissance")
         ttp_added = self.yeti_client.entity_search(name=ttp_name)
         self.assertEqual(ttp_added[0]["name"], ttp_name)
 
