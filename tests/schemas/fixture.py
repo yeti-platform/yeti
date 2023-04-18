f304ca5425f4f19d061c89a58a81ff5ff8b108dc
Thomas Chopitea <tomchop@gmail.com>
2023-04-18 07:36:13 +0000

    Add fixture for more advanced testing

---
+++ tests/schemas/fixture.py
@@ -0,0 +1,30 @@
+import datetime
+
+from typing import Optional
+
+from core import database_arango
+from core.schemas.observable import Observable
+from core.schemas.entity import ThreatActor
+from core.schemas.tag import Tag
+
+import unittest
+
+class TagTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        database_arango.db.clear()
+        c2_google = Observable(value="c2.google.com", type="hostname").save()
+        www_google = Observable(value="www.google.com", type="hostname").save()
+        google = Observable(value="google.com", type="hostname").save()
+        google.link_to(www_google, 'domain', 'Domain')
+        google.link_to(c2_google, 'domain', 'Domain')
+        ta = ThreatActor(name="GoogleActor", relevant_tags=["google_sus"]).save()
+        ta.link_to(google, 'c2', 'C2 infrastructure')
+        www_google.tag(['web', 'google'])
+        c2_google.tag(['web', 'google'])
+        sus_google = Observable(value="sus.google.com", type="hostname").save()
+        sus_google.tag(['web', 'google', 'google_sus'])
+
+
+    def test_something(self):
+        self.assertEqual(1, 1)
