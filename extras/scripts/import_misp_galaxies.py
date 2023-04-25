5d3a3d5ac84fefbdbb297a9c0583d6cd553f5035
Sebdraven <sebdraven@protonmail.com>
2023-04-25 17:37:50 +0200

    Update import_misp_galaxies.py

--- extras/scripts/import_misp_galaxies.py
+++ extras/scripts/import_misp_galaxies.py
@@ -5,6 +5,7 @@ from pymispgalaxies import Clusters, Galaxies
 from core.entities.actor import Actor
 from pyeti import YetiApi
 from mongoengine import connect
+from core.entities.malware import Malware
 from core.user import User
 
 YETI_ROOT = path.normpath(path.dirname(path.dirname(path.abspath(__file__))))
@@ -16,51 +17,78 @@ from core.config.config import yeti_config
 class ImportMisp:
     def __init__(self) -> None:
         self.db = connect("yeti", host=yeti_config.mongodb.host)
+        self.clusters = Clusters()
 
     def add_ThreatActor(self):
-        clusters = Clusters()
-        cluster_threat_actors = clusters.get("threat-actor").cluster
-        for ta in cluster_threat_actors["values"]:
+       
+        cluster_threat_actors = self.clusters.get("threat-actor")
+        for name_ta, obj_dict in cluster_threat_actors.cluster_values.items():
             name = None
             description = ""
             tags = []
+            ta = obj_dict.to_dict()
             try:
                 if "description" in ta:
                     description = ta["description"]
                 name = ta["value"]
                 if "meta" in ta:
+                    meta = ta["meta"].to_dict()
                     if (
-                        "cfr-suspected-state-sponsor" in ta["meta"]
-                        and ta["meta"]["cfr-suspected-state-sponsor"]
+                        "cfr-suspected-state-sponsor" in meta
+                        and meta["cfr-suspected-state-sponsor"]
                     ):
                         country = "Country Supected: {}.\n".format(
-                            ta["meta"]["cfr-suspected-state-sponsor"]
+                            meta["cfr-suspected-state-sponsor"]
                         )
                         description = "{} {}".format(description, country)
                     if (
-                        "cfr-target-category" in ta["meta"]
-                        and ta["meta"]["cfr-target-category"]
+                        "cfr-target-category" in meta
+                        and meta["cfr-target-category"]
                     ):
                         target = "Target: {}.\n".format(
-                            ",".join(ta["meta"]["cfr-target-category"])
+                            ",".join(meta["cfr-target-category"])
                         )
                         description = "{} {}".format(description, target)
                     if (
-                        "cfr-suspected-victims" in ta["meta"]
-                        and ta["meta"]["cfr-suspected-victims"]
+                        "cfr-suspected-victims" in meta
+                        and meta["cfr-suspected-victims"]
                     ):
                         victims = "Victims: {}\n.".format(
-                            ",".join(ta["meta"]["cfr-suspected-victims"])
+                            ",".join(meta["cfr-suspected-victims"])
                         )
                         description = "{} {}".format(description, victims)
-                    if "synonyms" in ta["meta"]:
-                        for synonym in ta["meta"]["synonyms"]:
+                    if "synonyms" in meta:
+                        for synonym in meta["synonyms"]:
                             tags.append(synonym)
                     tags.append(name)
                 actor = Actor.get_or_create(name=name)
                 actor.description = description
                 actor.tags = tags
                 actor.save()
+            except Exception as e:
+                print(f"Error: {e}")
+    
+    def add_Malware(self, name_cluster="malpedia"):
+        cluster_malware = self.clusters.get(name_cluster)
+        for name, obj_dict in cluster_malware.cluster_values.items():
+            malware = obj_dict.to_dict()
+            name = None
+            description = ""
+            tags = []
+            try:
+                if "description" in malware:
+                    description = malware["description"]
+                name = malware["value"]
+                if "meta" in malware:
+                    meta = malware["meta"].to_dict()
+                    if "synonyms" in meta:
+                        for synonym in meta["synonyms"]:
+                            tags.append(synonym)
+                    tags.append(name)
+                malware_new = Malware.get_or_create(name=name)
+                malware_new.description = description
+                malware_new.tags = tags
+                malware_new.save()
             except Exception as e:
                 print(f"Error: {e}")
 
@@ -82,3 +110,4 @@ if __name__ == "__main__":
     args = parse_args()
     if args.type == "all":
         import_misp.add_ThreatActor()
+        import_misp.add_Malware()
