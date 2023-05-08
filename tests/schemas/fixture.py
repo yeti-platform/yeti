d7f255e0ed5371fe9e81753aedd62a9b06c27071
Thomas Chopitea <tomchop@gmail.com>
2023-05-08 17:41:39 +0000

    Add regex to fixture

--- tests/schemas/fixture.py
+++ tests/schemas/fixture.py
@@ -6,6 +6,7 @@ from core import database_arango
 from core.schemas.observable import Observable
 from core.schemas.entity import ThreatActor
 from core.schemas.tag import Tag
+from core.schemas.indicator import Regex
 
 import unittest
 
@@ -13,19 +14,21 @@ class TagTest(unittest.TestCase):
 
     def setUp(self) -> None:
         database_arango.db.clear()
-        ip_google = Observable(value="8.8.8.8", type="ip").save()
-        c2_google = Observable(value="c2.google.com", type="hostname").save()
-        www_google = Observable(value="www.google.com", type="hostname").save()
-        google = Observable(value="google.com", type="hostname").save()
-        google.link_to(www_google, 'domain', 'Domain')
-        google.link_to(c2_google, 'domain', 'Domain')
-        google.link_to(ip_google, 'ip', 'IP')
-        ta = ThreatActor(name="GoogleActor", relevant_tags=["google_sus"]).save()
-        ta.link_to(google, 'c2', 'C2 infrastructure')
-        www_google.tag(['web', 'google'])
-        c2_google.tag(['web', 'google'])
-        sus_google = Observable(value="sus.google.com", type="hostname").save()
-        sus_google.tag(['web', 'google', 'google_sus'])
+        ip_hacker = Observable(value="8.8.8.8", type="ip").save()
+        c2_hacker = Observable(value="c2.hacker.com", type="hostname").save()
+        www_hacker = Observable(value="www.hacker.com", type="hostname").save()
+        hacker = Observable(value="hacker.com", type="hostname").save()
+        hacker.link_to(www_hacker, 'domain', 'Domain')
+        hacker.link_to(c2_hacker, 'domain', 'Domain')
+        hacker.link_to(ip_hacker, 'ip', 'IP')
+        ta = ThreatActor(name="HackerActor", relevant_tags=["hacker_sus"]).save()
+        ta.link_to(hacker, 'c2', 'C2 infrastructure')
+        www_hacker.tag(['web', 'hacker'])
+        c2_hacker.tag(['web', 'hacker'])
+        sus_hacker = Observable(value="sus.hacker.com", type="hostname").save()
+        sus_hacker.tag(['web', 'hacker', 'hacker_sus'])
+
+        regex = Regex(name='Hacker regex', pattern="^hacker.*", location="network").save()
 
 
     def test_something(self):
