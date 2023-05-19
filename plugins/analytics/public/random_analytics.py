ec6c48510399cf6f894c3aba00ec022a49875827
Thomas Chopitea <tomchop@gmail.com>
2023-05-19 16:04:18 +0000

    Introduce two dummy tasks

---
+++ plugins/analytics/public/random_analytics.py
@@ -0,0 +1,29 @@
+from datetime import timedelta
+
+from core import taskmanager
+from core.schemas import task
+# from core.analytics import ScheduledAnalytics
+# from mongoengine import Q
+
+
+class PrintDomains(task.AnalyticsTask):
+    _defaults = {
+        "frequency": timedelta(hours=12),
+        "type": "analytics",
+        "description": "Extracts a domain from a URL",
+    }
+
+    ACTS_ON = ['hostname']  # act on all observables
+
+    # TODO Use server-side JS filter
+    # CUSTOM_FILTER = Q(tags__not__size=0)  # filter only tagged elements
+
+    # def bulk(self, observables):
+    #     for o in observables:
+    #         self.each(o)
+
+    def each(self, observable):
+        print(observable.value)
+
+
+taskmanager.TaskManager.register_task(PrintDomains)
