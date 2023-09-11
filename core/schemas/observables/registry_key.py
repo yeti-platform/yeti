d589df3b3fb37078234b81ff8835fc8aef8a5c3b
tomchop <tomchop@gmail.com>
2023-09-11 15:27:54 +0000

    Fix typo, add key

--- core/schemas/observables/registry_key.py
+++ core/schemas/observables/registry_key.py
@@ -2,7 +2,8 @@ from core.schemas.observable import Observable
 from core.schemas.observable import ObservableType
 
 class RegistryKey(Observable):
-    value: str
     type: ObservableType = ObservableType.registry_key
+    key: str
+    value: str
     hive: str | None = None
     path_file: str | None = None
