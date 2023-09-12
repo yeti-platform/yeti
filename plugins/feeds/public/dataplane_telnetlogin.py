046c11bbecfac0d6409a1fe5563f02c8c374e434
Sebdraven <sebdraven@protonmail.com>
2023-09-12 13:24:10 +0200

    update dataplane

--- plugins/feeds/public/dataplane_telnetlogin.py
+++ plugins/feeds/public/dataplane_telnetlogin.py
@@ -2,11 +2,11 @@
        Feed of Dataplane SSH bruteforce IPs and ASNs
 """
 import logging
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import pandas as pd
 
-from core.schemas import observable
+from core.schemas.observables import ipv4, asn
 from core.schemas import task
 from core import taskmanager
 
@@ -43,20 +43,19 @@ class DataplaneTelenetLogin(task.FeedTask):
             "last_seen": item["lastseen"],
         }
 
-        ip = observable.Observable.find(value=item["ipaddr"])
-        if not ip:
-            ip = observable.Observable(value=item["ipaddr"], type="ip").save()
+        ip_obs = ipv4.IPv4.find(value=item["ipaddr"])
+        if not ip_obs:
+            ip_obs = ipv4.IPv4(value=item["ipaddr"]).save()
         category = item["category"].lower()
         tags = ["dataplane", "bruteforce", "telnet", "scanning"]
         if category:
             tags.append(category)
-        ip.add_context(self.name, context_ip)
-        ip.tag(tags)
+        ip_obs.add_context(self.name, context_ip)
+        ip_obs.tag(tags)
 
-        asn_obs = observable.Observable.find(value=item["ASN"])
+        asn_obs = asn.ASN.find(value=item["ASN"])
         if not asn_obs:
-            asn_obs = observable.Observable(value=item["ASN"], type="asn").save()
-
+            asn_obs = asn.ASN(value=item["ASN"]).save()
         context_asn = {
             "source": self.name,
             "last_seen": item["lastseen"],
@@ -64,7 +63,7 @@ class DataplaneTelenetLogin(task.FeedTask):
         asn_obs.add_context(self.name, context_asn)
         asn_obs.tag(tags)
 
-        asn_obs.link_to(ip, "ASN_IP", self.name)
+        asn_obs.link_to(ip_obs, "ASN_IP", self.name)
 
 
 taskmanager.TaskManager.register_task(DataplaneTelenetLogin)
