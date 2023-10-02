fea2f3f9b1ec431e28c6dd3ca23dee690412038c
Sebastien Larinier <sebdraven@protonmail.com>
2023-10-02 14:17:20 +0200

    Update passive_total.py

--- plugins/analytics/public/passive_total.py
+++ plugins/analytics/public/passive_total.py
@@ -3,38 +3,40 @@ from datetime import datetime
 import requests
 from dateutil import parser
 
-from core.analytics import OneShotAnalytics
+from core.schemas import task
+from core import taskmanager
+from core.schemas.observable import ObservableType, Observable
 from core.config.config import yeti_config
-from core.observables import Observable, Hostname, Hash, Email, Text
+from core.schemas.observables import email, hostname, sha256
+from core.schemas.entity import Company, Phone, Note
+from core.schemas.entity import EntityTypes
 
 
-def whois_links(observable, whois):
-    links = set()
-
+def whois_links(observable: Observable, whois):
     to_extract = [
         {
             "field": "organization",
-            "type": Text,
+            "type": Company,
             "label": "Registrant Organization",
         },
         {
             "field": "registrar",
-            "type": Text,
+            "type": Company,
             "label": "Registrar",
         },
         {
             "field": "contactEmail",
-            "type": Email,
+            "type": email.Email,
             "label": "Registrant Email",
         },
         {
             "field": "name",
-            "type": Text,
+            "type": Company,
             "label": "Registrant Name",
         },
         {
             "field": "telephone",
-            "type": Text,
+            "type": Phone,
             "label": "Registrant Phone",
             "record_type": "phone",
         },
@@ -42,52 +44,33 @@ def whois_links(observable, whois):
 
     for field in to_extract:
         if field["field"] in whois and whois[field["field"]] != "N/A":
-            node = field["type"].get_or_create(value=whois[field["field"]])
-            if field["type"] == Text:
-                if "record_type" in field:
-                    node.update(record_type=field["record_type"])
-                else:
-                    node.update(record_type=field["field"])
-
-            links.update(
-                observable.active_link_to(node, field["label"], "PassiveTotal")
-            )
+            if field["Type"] == email.Email:
+                obs = field["type"](value=whois[field["field"]])
+                observable.link_to(obs, field["label"], "PassiveTotal")
+            else:
+                ent = field["type"].get_or_create(name=whois[field["field"]])
+                observable.link_to(ent, field["label"], "PassiveTotal")
 
     if "nameServers" in whois:
-        nameservers = []
         for ns in whois["nameServers"]:
             if ns not in ["No nameserver", "not.defined"]:
                 try:
-                    nameservers.append(Hostname.get_or_create(value=ns))
+                    ns_obs = hostname.Hostname(value=ns)
+                    observable.link_to(ns_obs, "NS record", "PassiveTotal")
                 except Exception as e:
                     print(e.with_traceback())
 
-        if nameservers:
-            links.update(
-                observable.active_link_to(nameservers, "NS record", "PassiveTotal")
-            )
-
-    return list(links)
-
 
 class PassiveTotalApi(object):
-    settings = {
-        "passivetotal_api_username": {
-            "name": "PassiveTotal API Username",
-            "description": "Username (email-address) used for PassiveTotal.",
-        },
-        "passivetotal_api_key": {
-            "name": "PassiveTotal API Key",
-            "description": "API Key provided by PassiveTotal.",
-        },
-    }
-
     API_URL = "https://api.passivetotal.org/v2"
 
     @staticmethod
-    def get(uri, settings, params={}):
+    def get(uri, params={}):
         url = PassiveTotalApi.API_URL + uri
-        auth = (settings["passivetotal_api_username"], settings["passivetotal_api_key"])
+        auth = (
+            yeti_config["passivetotal"]["username"],
+            yeti_config["passivetotal"]["api_key"],
+        )
 
         response = requests.get(
             url, auth=auth, params=params, proxies=yeti_config.proxy
@@ -97,183 +80,152 @@ class PassiveTotalApi(object):
         return response.json()
 
 
-class PassiveTotalPassiveDNS(OneShotAnalytics, PassiveTotalApi):
-    default_values = {
+class PassiveTotalPassiveDNS(task.AnalyticsTask, PassiveTotalApi):
+    _defaults = {
         "group": "PassiveTotal",
         "name": "PassiveTotal Passive DNS",
         "description": "Perform passive DNS (reverse) lookups on domain names or IP addresses.",
     }
 
-    ACTS_ON = ["Hostname", "Ip"]
+    acts_on: list[ObservableType] = [ObservableType.hostname, ObservableType.ip]
 
-    @staticmethod
-    def analyze(observable, results):
-        links = set()
-
+    def each(self, observable: Observable):
         params = {"query": observable.value}
 
-        data = PassiveTotalApi.get("/dns/passive", results.settings, params)
-
+        data = PassiveTotalApi.get("/dns/passive", params)
+        context = {"source": "PassiveTotal Passive DNS"}
         for record in data["results"]:
             first_seen = datetime.strptime(record["firstSeen"], "%Y-%m-%d %H:%M:%S")
             last_seen = datetime.strptime(record["lastSeen"], "%Y-%m-%d %H:%M:%S")
+            context["first_seen"] = first_seen
+            context["last_seen"] = last_seen
 
             new = Observable.add_text(record["resolve"])
-            if isinstance(observable, Hostname):
-                links.update(
-                    observable.link_to(
-                        new,
-                        "{} record".format(record["recordType"]),
-                        "PassiveTotal",
-                        first_seen,
-                        last_seen,
-                    )
+            if observable.type is ObservableType.hostname:
+                observable.link_to(
+                    new, "{} record".format(record["recordType"]), "PassiveTotal"
                 )
+
             else:
-                links.update(
-                    new.link_to(
-                        observable,
-                        "{} record".format(record["recordType"]),
-                        "PassiveTotal",
-                        first_seen,
-                        last_seen,
-                    )
+                new.link_to(
+                    observable,
+                    "{} record".format(record["recordType"]),
+                    "PassiveTotal",
                 )
+            new.add_context("PassiveTotal", context)
 
-        return list(links)
 
-
-class PassiveTotalMalware(OneShotAnalytics, PassiveTotalApi):
-    default_values = {
+class PassiveTotalMalware(task.AnalyticsTask, PassiveTotalApi):
+    _defaults = {
         "group": "PassiveTotal",
         "name": "Get Malware",
         "description": "Find malware related to domain names or IP addresses.",
     }
 
-    ACTS_ON = ["Hostname", "Ip"]
+    acts_on: list[ObservableType] = [ObservableType.hostname, ObservableType.ip]
 
-    @staticmethod
-    def analyze(observable, results):
-        links = set()
-
+    def each(self, observable: Observable):
         params = {"query": observable.value}
 
-        data = PassiveTotalApi.get("/enrichment/malware", results.settings, params)
+        data = PassiveTotalApi.get("/enrichment/malware", params)
 
         for record in data["results"]:
             collection_date = parser.parse(record["collectionDate"])
 
-            malware = Hash.get_or_create(value=record["sample"])
-            links.update(
-                malware.link_to(
-                    observable, "Contacted Host", record["source"], collection_date
-                )
+            malware_obs = sha256.SHA256(value=record["sample"])
+
+            malware_obs.link_to(observable, "Contact to", "PassiveTotal")
+            malware_obs.add_context(
+                "PassiveTotal", {"collection_date": collection_date}
             )
 
-        return list(links)
 
-
-class PassiveTotalSubdomains(OneShotAnalytics, PassiveTotalApi):
-    default_values = {
+class PassiveTotalSubdomains(task.AnalyticsTask, PassiveTotalApi):
+    _defaults = {
         "group": "PassiveTotal",
         "name": "Get Subdomains",
         "description": "Find all known subdomains.",
     }
 
-    ACTS_ON = ["Hostname"]
+    acts_on: list[ObservableType] = [ObservableType.hostname]
 
-    @staticmethod
-    def analyze(observable, results):
-        links = set()
+    def each(self, observable: Observable):
+        params = {"query": f"*.{observable.value}"}
 
-        params = {"query": "*.{}".format(observable.value)}
-
-        data = PassiveTotalApi.get("/enrichment/subdomains", results.settings, params)
+        data = PassiveTotalApi.get("/enrichment/subdomains", params)
 
         for record in data["subdomains"]:
-            subdomain = Hostname.get_or_create(
-                value="{}.{}".format(record, observable.value)
-            )
-            links.update(
-                observable.active_link_to(subdomain, "Subdomain", "PassiveTotal")
-            )
+            subdomain = hostname.Hostname(value=f"{record}.{observable.value}")
+
+            observable.link_to(subdomain, "Subdomain", "PassiveTotal")
 
         return list(links)
 
 
-class PassiveTotalWhois(OneShotAnalytics, PassiveTotalApi):
-    default_values = {
+class PassiveTotalWhois(task.AnalyticsTask, PassiveTotalApi):
+    _defaults = {
         "group": "PassiveTotal",
         "name": "PassiveTotal Whois",
         "description": "Get Whois information for a specific domain name.",
     }
 
-    ACTS_ON = ["Hostname"]
+    acts_on: list[ObservableType] = [ObservableType.hostname]
 
-    @staticmethod
-    def analyze(observable, results):
+    def each(self, observable: Observable):
         params = {
             "query": observable.value,
         }
 
-        data = PassiveTotalApi.get("/whois", results.settings, params)
+        data = PassiveTotalApi.get("/whois", params)
 
         context = {"source": "PassiveTotal Whois", "raw": data}
         observable.add_context(context)
 
-        return whois_links(observable, data)
+        whois_links(observable, data)
 
 
-class PassiveTotalReverseWhois(OneShotAnalytics, PassiveTotalApi):
-    default_values = {
+class PassiveTotalReverseWhois(task.AnalyticsTask, PassiveTotalApi):
+    _defaults = {
         "group": "PassiveTotal",
         "name": "PassiveTotal Reverse Whois",
         "description": "Find all known domain names for a specific whois field.",
     }
 
-    ACTS_ON = ["Email", "Text"]
+    acts_on: list[ObservableType] = [ObservableType.email]
 
-    @staticmethod
-    def analyze(observable, results):
+    def each(self, observable: Observable):
         links = set()
 
-        if isinstance(observable, Email):
+        if observable.type is ObservableType.email:
             field = "email"
-        elif observable.record_type:
-            field = observable.record_type
+        elif observable.type:
+            field = observable.type
         else:
             raise ValueError("Could not determine field for this observable")
 
         params = {"query": observable.value, "field": field}
 
-        data = PassiveTotalApi.get("/whois/search", results.settings, params)
+        data = PassiveTotalApi.get("/whois/search", params)
 
         for record in data["results"]:
-            domain = Hostname.get_or_create(value=record["domain"])
-            links.update(whois_links(domain, record))
-
-        return list(links)
+            domain = hostname.Hostname(value=record["domain"])
+            whois_links(domain, record)
 
 
-class PassiveTotalReverseNS(OneShotAnalytics, PassiveTotalApi):
-    default_values = {
+class PassiveTotalReverseNS(task.AnalyticsTask, PassiveTotalApi):
+    _defaults = {
         "group": "PassiveTotal",
         "name": "PassiveTotal Reverse NS",
         "description": "Find all known domain names for a specific NS server.",
     }
 
-    ACTS_ON = ["Hostname"]
+    acts_on: list[ObservableType] = [ObservableType.hostname]
 
-    @staticmethod
-    def analyze(observable, results):
-        links = set()
-
+    def each(self, observable):
         params = {"query": observable.value, "field": "nameserver"}
 
-        data = PassiveTotalApi.get("/whois/search", results.settings, params)
+        data = PassiveTotalApi.get("/whois/search", params)
 
         for record in data["results"]:
-            domain = Hostname.get_or_create(value=record["domain"])
-            links.update(whois_links(domain, record))
-
-        return list(links)
+            domain = hostname.Hostname(value=record["domain"])
+            whois_links(domain, record)
