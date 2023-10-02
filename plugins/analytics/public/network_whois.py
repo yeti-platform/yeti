48e641537d2e204b3c22b99af57e435a9a12f0ea
Sebastien Larinier <sebdraven@protonmail.com>
2023-10-02 13:22:21 +0200

    Update network_whois.py

--- plugins/analytics/public/network_whois.py
+++ plugins/analytics/public/network_whois.py
@@ -1,26 +1,24 @@
-from pprint import pformat
+
+from core.schemas import task
+from core import taskmanager
+from core.schemas.observable import ObservableType
+from core.schemas.observables import ipv4,email
+from core.schemas.entity import Company
 from ipwhois import IPWhois
-from core.analytics import OneShotAnalytics
-from core.entities import Company
-from core.observables import Email
-
-
-class NetworkWhois(OneShotAnalytics):
-    default_values = {
+class NetworkWhois(task.AnalyticsTask):
+    _defaults = {
         "name": "NetworkWhois",
         "description": "Perform a Network Whois request on the IP address and tries to"
         " extract relevant information.",
     }
 
-    ACTS_ON = "Ip"
+    acts_on:list[ObservableType] = [ObservableType.ip]
 
-    @staticmethod
-    def analyze(ip, results):
-        links = set()
-
+    def each(self,ip:ipv4.IPv4):
+    
         r = IPWhois(ip.value)
         result = r.lookup_whois()
-        results.update(raw=pformat(result))
+        
 
         # Let's focus on the most specific information
         # Which should be in the smallest subnet
@@ -35,35 +33,20 @@ class NetworkWhois(OneShotAnalytics):
 
         if smallest_subnet:
             # Create the company
-            company = Company.get_or_create(
-                name=smallest_subnet["description"].split("\n")[0]
-            )
-            links.update(ip.active_link_to(company, "hosting", "Network Whois"))
-
+            company = Company(name=smallest_subnet["description"].split("\n")[0])
+            
             # Link it to every email address referenced
             if smallest_subnet["emails"]:
                 for email_address in smallest_subnet["emails"]:
-                    email = Email.get_or_create(value=email_address)
-                    links.update(company.link_to(email, None, "Network Whois"))
+                    email_obs = email.Email(value=email_address)
+                    company.link_to(email_obs, "email-company","IPWhois")
 
             # Copy the subnet info into the main dict
             for key in smallest_subnet:
                 if smallest_subnet[key]:
                     result["net_{}".format(key)] = smallest_subnet[key]
 
-        # Add the network whois to the context if not already present
-        for context in ip.context:
-            if context["source"] == "network_whois":
-                break
-        else:
-            # Remove the nets info (the main one was copied)
-            result.pop("nets", None)
-            result.pop("raw", None)
-            result.pop("raw_referral", None)
-            result.pop("referral", None)
-            result.pop("query", None)
+         
+            ip.add_context("IPWhois", result)
 
-            result["source"] = "network_whois"
-            ip.add_context(result)
-
-        return list(links)
+taskmanager.TaskManager.register_task(NetworkWhois)
