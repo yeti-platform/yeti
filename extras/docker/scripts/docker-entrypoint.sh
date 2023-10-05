5d2bc9e365a5cb527546ccedb8e0ef4c326c83f7
tomchop <tomchop@gmail.com>
2023-10-05 21:17:36 +0200

    Remove queue name from celery command

--- extras/docker/scripts/docker-entrypoint.sh
+++ extras/docker/scripts/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 if [ "$1" = 'webserver' ]; then
     poetry run uvicorn core.web.webapp:app --reload --host 0.0.0.0
 elif  [ "$1" = 'tasks' ]; then
-    poetry run celery -A core.taskmanager worker --loglevel=INFO -Ofair --autoscale=10,2 --purge -Q analytics -n analytics
+    poetry run celery -A core.taskmanager worker --loglevel=INFO -Ofair --autoscale=10,2 --purge
 elif  [ "$1" = 'envshell' ]; then
     poetry shell
 fi
