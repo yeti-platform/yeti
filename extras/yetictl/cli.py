7cdd20d7d75e26fded0363ccad9f79dfa7e0783c
tomchop <tomchop@gmail.com>
2023-10-31 18:08:36 +0000

    format

--- extras/yetictl/cli.py
+++ extras/yetictl/cli.py
@@ -3,22 +3,24 @@ import click
 from core.schemas.user import UserSensitive, User
 
 
-
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 def list_users():
     users = User.list()
     for user in users:
-        click.echo(f"Username: {user.username} | API key: {user.api_key} | Admin: {user.admin}")
+        click.echo(
+            f"Username: {user.username} | API key: {user.api_key} | Admin: {user.admin}"
+        )
 
 
 @cli.command()
-@click.argument('username')
-@click.argument('password')
-@click.option('--admin', is_flag=True, default=False)
+@click.argument("username")
+@click.argument("password")
+@click.option("--admin", is_flag=True, default=False)
 def create_user(username: str, password: str, admin: bool = False) -> None:
     """Creates a new user in the system."""
     user = UserSensitive.find(username=username)
@@ -27,11 +29,13 @@ def create_user(username: str, password: str, admin: bool = False) -> None:
     user = UserSensitive(username=username, admin=admin)
     user.set_password(password)
     user.save()
-    click.echo(f"User {username} succesfully created! API key: {username}:{user.api_key}")
+    click.echo(
+        f"User {username} succesfully created! API key: {username}:{user.api_key}"
+    )
 
 
 @cli.command()
-@click.argument('username')
+@click.argument("username")
 def delete_user(username: str) -> None:
     """Deletes a user from the system."""
     user = UserSensitive.find(username=username)
@@ -42,8 +46,8 @@ def delete_user(username: str) -> None:
 
 
 @cli.command()
-@click.argument('username')
-@click.argument('new_password')
+@click.argument("username")
+@click.argument("new_password")
 def reset_password(username: str, new_password: str) -> None:
     """Resets a user's password."""
     user = UserSensitive.find(username=username)
@@ -52,7 +56,10 @@ def reset_password(username: str, new_password: str) -> None:
     user.set_password(new_password)
     user.reset_api_key()
     user.save()
-    click.echo(f"Password for {username} succesfully reset. New API key: {user.api_key}")
+    click.echo(
+        f"Password for {username} succesfully reset. New API key: {user.api_key}"
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
