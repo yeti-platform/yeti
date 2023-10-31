eef4fec53fd258526bc9ea5678074b6a3e4403e6
tomchop <tomchop@gmail.com>
2023-10-31 18:15:04 +0000

    Update docker entrypoint

--- extras/docker/scripts/docker-entrypoint.sh
+++ extras/docker/scripts/docker-entrypoint.sh
@@ -5,10 +5,10 @@ if [ "$1" = 'webserver' ]; then
     poetry run uvicorn core.web.webapp:app --reload --host 0.0.0.0
 elif  [ "$1" = 'tasks' ]; then
     poetry run celery -A core.taskmanager worker --loglevel=INFO --purge -B -P threads
-elif [ "$1" = 'createuser']; then
-    poetry run yetictl createuser "${@:2}"
-elif [ "$1" = 'resetpassword']; then
-    poetry run yetictl resetpassword "${@:2}"
+elif [ "$1" = 'create-user']; then
+    poetry run yetictl create-user "${@:2}"
+elif [ "$1" = 'reset-password']; then
+    poetry run yetictl reset-password "${@:2}"
 elif  [ "$1" = 'envshell' ]; then
     poetry shell
 else
