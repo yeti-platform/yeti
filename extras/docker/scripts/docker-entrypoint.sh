868be5a40c36ca963d6206019e7d1f991db1de0c
tomchop <tomchop@gmail.com>
2023-11-01 18:55:32 +0000

    update entrypoint

--- extras/docker/scripts/docker-entrypoint.sh
+++ extras/docker/scripts/docker-entrypoint.sh
@@ -6,9 +6,9 @@ if [[ "$1" = 'webserver' ]]; then
 elif [[ "$1" = 'tasks' ]]; then
     poetry run celery -A core.taskmanager worker --loglevel=INFO --purge -B -P threads
 elif [[ "$1" = 'create-user' ]]; then
-    poetry run python extras/yetictl/cli.py create-user "${@:2}"
+    poetry run python yetictl/cli.py create-user "${@:2}"
 elif [[ "$1" = 'reset-password' ]]; then
-    poetry run python extras/yetictl/cli.py reset-password "${@:2}"
+    poetry run python yetictl/cli.py reset-password "${@:2}"
 elif [[ "$1" = 'envshell' ]]; then
     poetry shell
 else
