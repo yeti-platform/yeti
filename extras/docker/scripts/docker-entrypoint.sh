07764766d69a870374103fe65ddba5c72f1247f2
tomchop <tomchop@gmail.com>
2023-11-01 18:35:29 +0000

    Bugfixes

--- extras/docker/scripts/docker-entrypoint.sh
+++ extras/docker/scripts/docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ "$1" = 'webserver' ]; then
+if [[ "$1" = 'webserver' ]]; then
     poetry run uvicorn core.web.webapp:app --reload --host 0.0.0.0
-elif  [ "$1" = 'tasks' ]; then
+elif [[ "$1" = 'tasks' ]]; then
     poetry run celery -A core.taskmanager worker --loglevel=INFO --purge -B -P threads
-elif [ "$1" = 'create-user']; then
-    poetry run yetictl create-user "${@:2}"
-elif [ "$1" = 'reset-password']; then
-    poetry run yetictl reset-password "${@:2}"
-elif  [ "$1" = 'envshell' ]; then
+elif [[ "$1" = 'create-user' ]]; then
+    poetry run python extras/yetictl/cli.py create-user "${@:2}"
+elif [[ "$1" = 'reset-password' ]]; then
+    poetry run python extras/yetictl/cli.py reset-password "${@:2}"
+elif [[ "$1" = 'envshell' ]]; then
     poetry shell
 else
     exec "$@"
