060a4f4394492ec70c7660d10e151b17f040d113
Fred Baguelin <frederic.baguelin@datadoghq.com>
2023-11-23 15:15:30 +0100

    Remove Celery code from taskmanager to avoid circular imports with loader

--- core/taskmanager.py
+++ core/taskmanager.py
@@ -1,60 +1,12 @@
 import datetime
-import importlib
-import inspect
-import json
 import logging
-import pathlib
-import pkgutil
 import traceback
 from typing import Type
 
-from celery import Celery
-from celery.utils.log import get_task_logger
-from core.config.config import yeti_config
 from core.schemas.task import ExportTask, Task, TaskParams, TaskStatus
 
-logger = get_task_logger(__name__)
-
-
-def get_plugins_packages():
-    plugins_list = set()
-    plugins_path = pathlib.Path(yeti_config.get('system', 'plugins_path'))
-    if not plugins_path.exists():
-        logger.warning(f"Plugins path {str(plugin_path.absolute())} does not exist")
-        return plugins_list
-    for module_info in pkgutil.walk_packages([str(plugins_path.absolute())], prefix=f"{plugins_path.name}."):
-        if not module_info.ispkg:
-            try:
-                module = importlib.import_module(module_info.name)
-                for _, obj in inspect.getmembers(module, inspect.isclass):
-                    if issubclass(obj, Task):
-                        plugins_list.add(module_info.name)
-            except Exception as error:
-                logger.warning(f"Cannot import plugin {module_info.name}\n{traceback.format_exc()}")
-    return plugins_list
-
-app = Celery(
-    "tasks",
-    broker=f"redis://{yeti_config.get('redis', 'host')}/",
-    worker_pool_restarts=True,
-    imports=get_plugins_packages()
-)
-
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    """Registers periodic tasks."""
-    for task in Task.list():
-        if not task.frequency:
-            continue
-        logger.info("Registering periodic task %s (%s)", task.name, task.frequency)
-        sender.add_periodic_task(
-            task.frequency,
-            run_task.s(task.name, '{}'),
-            name=f'Schedule for {task.name}')
-    return
 
 class TaskManager:
-
     _store = {}  # type: dict[str, Task]
 
     @classmethod
@@ -88,7 +40,7 @@ class TaskManager:
         """Loads tasks from the database and refreshes cache."""
         if task_name not in cls._store:
             # Only ExportTasks are registered dynamically
-            logging.info(f'Registering ExportTask {task_name}')
+            logging.info(f"Registering ExportTask {task_name}")
             cls.register_task(ExportTask, task_name=task_name)
 
         if task_name not in cls._store:
@@ -137,16 +89,3 @@ class TaskManager:
         task.last_run = datetime.datetime.now(datetime.timezone.utc)
         task.status_message = ""
         task.save()
-
-
-@app.task
-def run_task(task_name: str, params: str):
-    """Runs a task.
-
-    Args:
-        task_name: The name of a registered task to run.
-        params: A string-encoded JSON representation of a TaskParams object
-            (obtained through model_dump_json)
-    """
-    task_params = TaskParams(**json.loads(params))
-    TaskManager.run_task(task_name, task_params)
