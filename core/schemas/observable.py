29c835e1db31a959f69370afcaa5571c5fa0828b
Sebdraven <sebdraven@protonmail.com>
2023-12-01 16:04:04 +0100

    Update observable.py

--- core/schemas/observable.py
+++ core/schemas/observable.py
@@ -89,10 +89,13 @@ class Observable(BaseModel, database_arango.ArangoYetiConnector):
         return observable
 
     def add_context(
-        self, source: str, context: dict | None = None, skip_compare: set = set()
+        self, source: str, context: dict | None = None, skip_compare: set | None = None
     ) -> "Observable":
         """Adds context to an observable."""
-        compare_fields = set(context.keys()) - skip_compare - {"source"}
+        compare_fields = set(context.keys()) - {"source"}
+        if skip_compare:
+            compare_fields -= skip_compare
+            
         for idx, db_context in enumerate(list(self.context)):
             if db_context["source"] != source:
                 continue
