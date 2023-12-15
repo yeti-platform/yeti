392f9ca769a5b8605fc957a59cafb40e2135c712
Fred Baguelin <frederic.baguelin@datadoghq.com>
2023-12-16 00:16:52 +0100

    Enable user account as observable

--- core/schemas/observable.py
+++ core/schemas/observable.py
@@ -38,6 +38,7 @@ class ObservableType(str, Enum):
     tlsh = "tlsh"
     url = "url"
     user_agent = "user_agent"
+    user_account = "user_account"
 
 
 class Observable(BaseModel, database_arango.ArangoYetiConnector):
@@ -182,4 +183,4 @@ from core.schemas.observables import (asn, bitcoin_wallet, certificate, cidr,
                                       generic_observable, hostname, imphash,
                                       ipv4, ipv6, mac_address, md5, path,
                                       registry_key, sha1, sha256, ssdeep, tlsh,
-                                      url, user_agent)
+                                      url, user_account, user_agent)
