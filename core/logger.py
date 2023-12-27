a8a590c2523ec04e57abaa19bf735d36be3c8ba6
Fred Baguelin <frederic.baguelin@datadoghq.com>
2023-12-27 17:21:29 +0100

    Enhance logger

--- core/logger.py
+++ core/logger.py
@@ -23,6 +23,10 @@ class JsonFormatter(Formatter):
             json_record["method"] = record.__dict__["method"]
         if "body" in record.__dict__ and record.__dict__["body"]:
             json_record["body"] = json.loads(record.__dict__["body"].decode("utf-8"))
+        if "client" in record.__dict__:
+            json_record["client"] = record.__dict__["client"]
+        if "status_code" in record.__dict__:
+            json_record["status_code"] = record.__dict__["status_code"]
         if record.levelno == logging.ERROR and record.exc_info:
             json_record["err"] = self.formatException(record.exc_info)
         return json.dumps(json_record)
@@ -31,20 +35,24 @@ class JsonFormatter(Formatter):
 log_queue = queue.Queue(-1)
 queue_handler = QueueHandler(log_queue)
 
-logger = logging.getLogger()
+logger = logging.getLogger("yeti.audit.log")
+logger.propagate = False
 logger.addHandler(queue_handler)
 
-formatter = JsonFormatter()
+json_formatter = JsonFormatter()
 
+console_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(username)s - %(path)s - %(method)s - %(body)s - %(client)s - %(status_code)s"
+)
 console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
+console_handler.setFormatter(console_formatter)
 
 file_handler = logging.FileHandler("yeti.log")
-file_handler.setFormatter(formatter)
+file_handler.setFormatter(json_formatter)
 
 listener = QueueListener(log_queue, console_handler, file_handler)
 listener.start()
 logger.setLevel(logging.INFO)
 
-logging.getLogger("uvicorn.access").disabled = True
+#logging.getLogger("uvicorn.access").disabled = True
 
