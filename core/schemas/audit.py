0471a2d81108f2f46cf5637d5c03fb24fe894c7e
Fred Baguelin <frederic.baguelin@datadoghq.com>
2023-12-28 13:14:50 +0100

    Add AuditLog object

---
+++ core/schemas/audit.py
@@ -0,0 +1,30 @@
+import datetime
+from typing import ClassVar, Literal
+
+from core import database_arango
+from core.schemas.model import YetiModel
+from pydantic import computed_field
+
+
+class AuditLog(YetiModel, database_arango.ArangoYetiConnector):
+    _collection_name: ClassVar[str] = "auditlog"
+    _type_filter: ClassVar[str | None] = None
+    _root_type: Literal["auditlog"] = "auditlog"
+
+    @computed_field(return_type=Literal["auditlog"])
+    @property
+    def root_type(self):
+        return self._root_type
+
+
+    created: datetime.datetime
+    username: str
+    action: str
+    status: str
+    target: str
+    content: dict = {}
+    ip: str
+
+    @classmethod
+    def load(cls, object: dict) -> "AuditLog":
+        return cls(**object)
