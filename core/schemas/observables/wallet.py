913ee0fd0a5c970b8ec5d29719ff01554cfa22cf
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-01-15 12:20:21 +0100

    Add docstring to wallet

--- core/schemas/observables/wallet.py
+++ core/schemas/observables/wallet.py
@@ -4,6 +4,13 @@ from core.schemas import observable
 
 
 class Wallet(observable.Observable):
+    """Represents a wallet observable.
+
+    coin and address must be provided.
+    Value should be in the form <COIN>:<ADDRESS>.
+    """
+
+
     type: Literal[observable.ObservableType.wallet] = observable.ObservableType.wallet
     coin: str | None = None
     address: str | None = None
