59f6a9a80eda2dea664db5592193fbeea4229ec5
itsmvd <mvd@google.com>
2024-01-23 16:54:36 +0000

    Fix tests to use temp/test db instead of mocking various calls

--- tests/analytics_test.py
+++ tests/analytics_test.py
@@ -3,43 +3,58 @@ from unittest.mock import patch, MagicMock
 from plugins.analytics.public import censys
 from core import database_arango
 from censys.search import CensysHosts
-from core.schemas import indicator, observable
+from core.schemas import indicator
+from core.schemas.indicator import DiamondModel
+from core.schemas.observable import ObservableType
+from core.schemas import observable
+
 
 class AnalyticsTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        database_arango.db.connect(database="yeti_test")
+        database_arango.db.connect(database="yeti-test")
         database_arango.db.clear()
 
     @patch('plugins.analytics.public.censys.CensysHosts')
-    @patch('plugins.analytics.public.censys.indicator.Query.filter')
-    @patch('plugins.analytics.public.censys.Observable.add_text')
-    def test_censys_query(self, mock_add_text, mock_filter, mock_censys_hosts):
+    def test_censys_query(self, mock_censys_hosts):
         mock_hosts_api = MagicMock(spec=CensysHosts)
         mock_censys_hosts.return_value = mock_hosts_api
 
-        mock_query = MagicMock(spec=indicator.Query)
-        mock_query.pattern = 'test_pattern'
-        mock_query.relevant_tags = ['test_tag']
-        mock_query.link_to = MagicMock()
-        mock_filter.return_value = ([mock_query], 1)
+        censys_query = indicator.Query(
+            name='Censys test query name',
+            description='Censys test query description',
+            pattern='test_censys_query',
+            location='censys',
+            diamond=DiamondModel.infrastructure,
+            relevant_tags=['censys_query_tag'],
+            query_type=indicator.QueryType.censys).save()
 
-        mock_search_result = [{'ip': '192.0.2.1'}, {'ip': '203.0.113.2'}]
+
+        mock_search_result = [{'ip': '192.0.2.1'}, {'ip': '2001:db8:3333:4444:5555:6666:7777:8888'}]
         mock_hosts_api.search.return_value = [mock_search_result]
-
-        mock_observable_instance = MagicMock(spec=observable.Observable)
-        mock_observable_instance.tag = MagicMock()
-        mock_add_text.return_value = mock_observable_instance
 
         defaults = censys.CensysApiQuery._defaults.copy()
         analytics = censys.CensysApiQuery(**defaults)
         analytics.run()
 
         mock_censys_hosts.assert_called_once()
-        mock_filter.assert_called_once_with({'query_type': indicator.QueryType.censys})
-        mock_hosts_api.search.assert_called_once_with('test_pattern', fields=['ip'], pages=-1)
-        mock_observable_instance.tag.assert_called_with(['test_tag'])
-        mock_query.link_to.assert_called_with(mock_observable_instance, 'censys',f'IP found with Censys query: {mock_query}')
+        mock_hosts_api.search.assert_called_once_with('test_censys_query', fields=['ip'], pages=-1)
+
+        observables = observable.Observable.filter({"value": ''}, graph_queries=[('tags', 'tagged', 'outbound', 'name')])
+        observable_obj, _ = observables
+
+        self.assertEqual(observable_obj[0].value, '192.0.2.1')
+        self.assertEqual(observable_obj[0].type, ObservableType.ipv4)
+        self.assertEqual(set(observable_obj[0].tags.keys()), {'censys_query_tag'})
+
+        self.assertEqual(observable_obj[1].value, '2001:db8:3333:4444:5555:6666:7777:8888')
+        self.assertEqual(observable_obj[1].type, ObservableType.ipv6)
+        self.assertEqual(set(observable_obj[1].tags.keys()), {'censys_query_tag'})
+
+        query_neighbors = [o.value for o in censys_query.neighbors()[0].values()]
+        self.assertIn('192.0.2.1', query_neighbors)
+        self.assertIn('2001:db8:3333:4444:5555:6666:7777:8888', query_neighbors)
+
 
 if __name__ == '__main__':
     unittest.main()
