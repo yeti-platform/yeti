fa0b8ee4807edf3c4f8c7b15daa521e0ee48fb14
Thomas Chopitea <tomchop@gmail.com>
2024-01-30 12:56:13 +0000

    Format imports

--- plugins/analytics/public/expire_tags.py
+++ plugins/analytics/public/expire_tags.py
@@ -1,16 +1,12 @@
+import datetime
 from datetime import timedelta
-from typing import Optional
 
 from core import taskmanager
-from core.config.config import yeti_config
-from core.schemas import indicator, task
-from core.schemas.observable import Observable
-from core.schemas.tag import Tag
+from core.schemas import task
 from core.schemas.graph import TagRelationship
-import datetime
+
 
 class ExpireTags(task.AnalyticsTask):
-
     _defaults = {
         "name": "ExpireTags",
         "description": "Expires tags in observables",
