078abe7f8e1f4d2fd8303c17693b367a74786d2f
Sebdraven <sebdraven@protonmail.com>
2024-02-02 16:26:33 +0100

    Update import_data.py
    for merge

--- core/web/apiv2/import_data.py
+++ core/web/apiv2/import_data.py
@@ -2,15 +2,14 @@ import json
 
 from fastapi import APIRouter, File, UploadFile
 
-from core.common.misp_to_yeti import MispToYeti
-
 router = APIRouter()
 
 
 @router.post("/import_misp_json", tags=["import_misp_json"])
 async def import_misp_json(misp_file_json: UploadFile = File(...)):
-    contents = await misp_file_json.read()
-    data_json = json.loads(contents)
+    #contents = await misp_file_json.read()
+    #data_json = json.loads(contents)
 
-    converter = MispToYeti(data_json["Event"])
-    converter.misp_to_yeti()
+    #converter = MispToYeti(data_json["Event"])
+    #converter.misp_to_yeti()
+    return {"status":True}
