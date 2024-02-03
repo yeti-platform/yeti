c2b582c327fe62d3e93d96b1e1e745c3d3f97f8c
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-02-03 14:29:31 +0100

    Linting

--- plugins/feeds/public/miningpoolstats.py
+++ plugins/feeds/public/miningpoolstats.py
@@ -1,10 +1,10 @@
 import logging
 import re
-from datetime import datetime, timedelta
-from io import StringIO
+from datetime import timedelta
 from typing import ClassVar
 
 import requests
+
 from core import taskmanager
 from core.schemas import task
 from core.schemas.observable import Observable
@@ -19,7 +19,9 @@ class MiningPoolStats(task.FeedTask):
     }
 
     _SOURCE: ClassVar["str"] = "https://miningpoolstats.stream"
-    _USER_AGENT: ClassVar["str"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    _USER_AGENT: ClassVar[
+        "str"
+    ] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
     def run(self):
         data = {"query": "get_recent", "selector": "time"}
@@ -34,13 +36,13 @@ class MiningPoolStats(task.FeedTask):
             count += 1
             tags = ["cryptomining", "cryptominer", "cryptomining-pool", coin_name]
             for data in self._extract_pool_urls(coin_name):
-                url_value = data.get('url', '').strip()
+                url_value = data.get("url", "").strip()
                 url_obs = None
                 if url_value:
                     url_obs = url.Url(value=url_value).save()
                     url_obs.tag(tags)
                     url_obs.add_context(self.name, {"source": self.name})
-                pool_id = data.get('pool_id', '').strip()
+                pool_id = data.get("pool_id", "").strip()
                 if pool_id:
                     # some pool_id are not valid ipv4 or hostname, ex: superblockchain
                     try:
@@ -55,18 +57,26 @@ class MiningPoolStats(task.FeedTask):
         headers = {"User-Agent": self._USER_AGENT}
         response = requests.get(endpoint, headers=headers)
         if response.status_code != 200:
-            logging.debug(f"Can't fetch coin page {coin_name} - code: {response.status_code}, reason: {response.reason}")
+            logging.debug(
+                f"Can't fetch coin page {coin_name} - code: {response.status_code}, reason: {response.reason}"
+            )
             return
-        pattern = r'href="(https://data.miningpoolstats.stream/data/' + coin_name + r'.js\?t=[0-9]{10})"'
+        pattern = (
+            r'href="(https://data.miningpoolstats.stream/data/'
+            + coin_name
+            + r'.js\?t=[0-9]{10})"'
+        )
         m = re.search(pattern, response.text)
         if not m:
-            logging.debug(f"Can't find pools endpoint")
+            logging.debug("Can't find pools endpoint")
             return
         response = requests.get(m.group(1), headers=headers)
         if response.status_code != 200:
-            logging.debug(f"Can't fetch pool page - code: {response.status_code}, reason: {response.reason}")
+            logging.debug(
+                f"Can't fetch pool page - code: {response.status_code}, reason: {response.reason}"
+            )
             return
-        for data in response.json().get('data', []):
+        for data in response.json().get("data", []):
             yield data
         return
 
@@ -75,18 +85,25 @@ class MiningPoolStats(task.FeedTask):
         headers = {"User-Agent": self._USER_AGENT}
         response = requests.get("https://miningpoolstats.stream", headers=headers)
         if response.status_code != 200:
-            logging.debug(f"Can't fetch main page - code: {response.status_code}, reason: {response.reason}")
+            logging.debug(
+                f"Can't fetch main page - code: {response.status_code}, reason: {response.reason}"
+            )
             return None
-        m = re.search(r'href="(https://data.miningpoolstats.stream/data/coins_data.js\?t=[0-9]{10})"', response.text)
+        m = re.search(
+            r'href="(https://data.miningpoolstats.stream/data/coins_data.js\?t=[0-9]{10})"',
+            response.text,
+        )
         if not m:
-            logging.debug(f"Can't extract pages from response")
+            logging.debug("Can't extract pages from response")
             return None
         response = requests.get(m.group(1), headers=headers)
         if response.status_code != 200:
-            logging.debug(f"Can't fetch data - code: {response.status_code}, reason: {response.reason}")
+            logging.debug(
+                f"Can't fetch data - code: {response.status_code}, reason: {response.reason}"
+            )
             return None
-        for data in response.json().get('data', []):
-            coin_names.add(data.get('page'))
+        for data in response.json().get("data", []):
+            coin_names.add(data.get("page"))
         return coin_names
 
 
