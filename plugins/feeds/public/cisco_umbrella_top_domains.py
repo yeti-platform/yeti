109fd0b069c6f7ca8194e936bc6d6428e3695cdb
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-02-09 18:50:59 +0100

    Add Cisco Umbrella Top1M domains feed

---
+++ plugins/feeds/public/cisco_umbrella_top_domains.py
@@ -0,0 +1,39 @@
+import io
+from datetime import timedelta
+from typing import ClassVar
+
+from core import taskmanager
+from core.config.config import yeti_config
+from core.schemas import task
+from core.schemas.observables import hostname
+
+
+class CiscoUmbrellaTopDomains(task.FeedTask):
+    _defaults = {
+        "frequency": timedelta(hours=1),
+        "name": "CloudflareTopDomains",
+        "description": "Import Cloudflare top domains",
+    }
+    _SOURCE: ClassVar[
+        "str"
+    ] = "http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip"
+
+    def run(self):
+        top_domains = int(yeti_config.get("umbrella", "top_domains", 10000))
+        response = self._make_request(self._SOURCE, sort=False)
+        data = self._unzip_content(response.content)
+        context = {
+            "name": self.name,
+        }
+        feed = io.BytesIO(data)
+        top_domains = int(top_domains)
+        while top_domains > 0:
+            line = feed.readline().decode("utf-8").strip()
+            _, domain = line.split(",")
+            hostname_obs = hostname.Hostname(value=domain).save()
+            hostname_obs.add_context(self.name, context)
+            hostname_obs.tag(["cisco_umbrella", "top_domain"])
+            top_domains -= 1
+
+
+taskmanager.TaskManager.register_task(CiscoUmbrellaTopDomains)
