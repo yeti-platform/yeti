03c8a837fe8b2af255e10d77f149f7f95b9e21c9
Sebdraven <sebdraven@protonmail.com>
2024-03-01 15:34:49 +0100

    add invest for one event

--- core/common/misp_to_yeti.py
+++ core/common/misp_to_yeti.py
@@ -1,4 +1,4 @@
-from core.schemas import observable
+from core.schemas import entity, observable
 
 MISP_TYPES_TO_IMPORT = {
     "domain": observable.ObservableType.hostname,
@@ -26,42 +26,45 @@ class MispToYeti:
     def __init__(self, misp_event):
         self.misp_event = misp_event
 
-    def attr_misp_to_yeti(self, attribute: dict) -> observable.Observable:
+    def attr_misp_to_yeti(
+        self, invest: entity.Investigation, attribute: dict
+    ) -> observable.Observable: # type: ignore
         if attribute.get("type") in MISP_TYPES_TO_IMPORT:
             obs_yeti = observable.TYPE_MAPPING[
-                MISP_TYPES_TO_IMPORT[attribute.get("type")]
+                MISP_TYPES_TO_IMPORT[attribute.get("type")] # type: ignore
             ](value=attribute.get("value")).save()
             print(f"Attribute {attribute.get('value')} imported")
             return obs_yeti
 
     def add_context_by_misp(
         self, attribute_misp: dict, event: dict, obs_yeti: observable.Observable
-    ) -> dict:
+    ):
         context = {}
         event_id = attribute_misp.get("event_id")
-        context["Org"] = event.get("Org")["name"]
+        context["Org"] = event["Org"]["name"]
         context["event_id"] = event_id
         if attribute_misp.get("comment"):
             context["comment"] = attribute_misp.get("comment")
 
         obs_yeti.add_context("misp", context)
 
-    def obs_misp_to_yeti(self, object_misp: dict):
-        objs_type = object_misp.get("type")
+    def obs_misp_to_yeti(self,invest: entity.Investigation, object_misp: dict):
+        objs_type = object_misp["type"]
         links = []
-        for attr in object_misp.get("Attribute"):
-            obs_yeti = self.attr_misp_to_yeti(attr)
+        for attr in object_misp["Attribute"]:
+            obs_yeti = self.attr_misp_to_yeti(invest,attr)
             links.append(obs_yeti)
         obs_yeti = links.pop()
         for obj_to_link in links:
             obs_yeti.link_to(obj_to_link, f"linked_by_misp_{objs_type}", "misp")
 
     def misp_to_yeti(self):
-        for object_misp in self.misp_event.get("Object"):
-            self.obs_misp_to_yeti(object_misp)
+        invest = entity.Investigation(name=self.misp_event.get("info")).save()
+        for object_misp in self.misp_event["Object"]:
+            self.obs_misp_to_yeti(invest,object_misp)
 
-        for attribute_misp in self.misp_event.get("Attribute"):
-            obs_yeti = self.attr_misp_to_yeti(attribute_misp)
+        for attribute_misp in self.misp_event["Attribute"]:
+            obs_yeti = self.attr_misp_to_yeti(invest,attribute_misp)
             if obs_yeti:
                 self.add_context_by_misp(attribute_misp, self.misp_event, obs_yeti)
             else:
