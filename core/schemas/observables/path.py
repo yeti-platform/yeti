b65e5a6507a00e4d6b74b8186795eed0743a89e2
Sebdraven <sebdraven@protonmail.com>
2024-03-08 18:40:51 +0100

    fix pydantic

--- core/schemas/observables/path.py
+++ core/schemas/observables/path.py
@@ -6,10 +6,10 @@ from core.schemas import observable
 
 class Path(observable.Observable):
     type: Literal[observable.ObservableType.path] = observable.ObservableType.path
-    creation_time: datetime | None
-    modification_time: datetime | None
-    access_time: datetime | None
-    path_encoding: str | None
+    creation_time: datetime | None = None
+    modification_time: datetime | None = None
+    access_time: datetime | None = None
+    path_encoding: str | None = None
 
 
 observable.TYPE_MAPPING[observable.ObservableType.path] = Path
