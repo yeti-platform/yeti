6e6d6801ad6d668c9375bea37ded13bc76d0dc8e
Sebastien Larinier <sebdraven@protonmail.com>
2024-04-03 16:19:38 +0200

    Update tweetlive.py

--- plugins/feeds/public/tweetlive.py
+++ plugins/feeds/public/tweetlive.py
@@ -19,46 +19,45 @@ class TweetLive(task.FeedTask):
     _defaults = {
         "frequency": timedelta(days=1),
         "name": "TweetLive",
-        "description": "This feed contains tweets",
+        "description": "This feed contains IOCs in tweets of the list https://twitter.com/i/lists/1423693426437001224 ",
     }
     _SOURCE: ClassVar["str"] = "https://api.tweetfeed.live/v1/today"
 
     def run(self):
         r = self._make_request(self._SOURCE, sort=False)
+        if not r:
+            raise ValueError("Error fetching data")
 
-        if r:
-            data = r.json()
+        if r.status_code != 200:
+            raise ValueError(f"Error fetching data: {r.status_code}")
 
-            if not data:
-                raise ValueError("No data returned")
+        data = r.json()
 
-            df = pd.DataFrame(data)
-            df.fillna("")
-            df["date"] = pd.to_datetime(df["date"])
+        if not data:
+            raise ValueError("No data returned")
 
-            df = self._filter_observables_by_time(df, "date")
+        df = pd.DataFrame(data)
+        df.fillna("")
+        df["date"] = pd.to_datetime(df["date"])
+
+        df = self._filter_observables_by_time(df, "date")
 
         for _, line in df.iterrows():
             self.analyze(line)
 
     def analyze(self, item):
-        if item["type"] not in MAPPING:
-            return
-
-        obs_type = MAPPING[item["type"]]
+        obs_type = MAPPING.get(item["type"])
 
         if not obs_type:
-            raise (ValueError(f"Observable type {item['type']} not supported"))
+            raise ValueError(f"Observable type {item['type']} not supported")
 
-        obs = observable.TYPE_MAPPING["observable"](
-            value=item["value"], type=obs_type
-        ).save()
+        obs = observable.Observable(value=item["value"], type=obs_type).save()
 
         context = {}
 
-        if "tweet" in item:
+        if item["tweet"]:
             context["tweet"] = item["tweet"]
-        if "user" in item:
+        if item["user"]:
             context["user"] = item["user"]
 
         if item["tags"]:
