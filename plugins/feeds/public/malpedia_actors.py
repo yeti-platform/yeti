5972ebb9f0199b3e10a25ee49e85163efbb13f36
Sebastien Larinier <sebdraven@protonmail.com>
2024-06-13 12:40:54 +0200

    fixes error parsing

--- plugins/feeds/public/malpedia_actors.py
+++ plugins/feeds/public/malpedia_actors.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import timedelta
 from typing import ClassVar
 
@@ -13,9 +14,7 @@ class Malpedia_Actors(task.FeedTask):
         "source": "https://malpedia.caad.fkie.fraunhofer.de/",
     }
 
-    _SOURCE: ClassVar["str"] = (
-        "https://malpedia.caad.fkie.fraunhofer.de/api/get/actors"
-    )
+    _SOURCE: ClassVar["str"] = "https://malpedia.caad.fkie.fraunhofer.de/api/get/actors"
 
     def run(self):
         response = self._make_request(self._SOURCE)
@@ -24,27 +23,36 @@ class Malpedia_Actors(task.FeedTask):
         actors_json = response.json()
         for actor, entry in actors_json.items():
             self.analyze_entry_actor(actor, entry)
-    
+
     def analyze_entry_actor(self, name_actor, entry: dict):
         intrusion_set = entity.IntrusionSet(name=entry["value"])
 
         if entry.get("description"):
-            intrusion_set.description += "## Malpedia"
+            if intrusion_set.description:
+                intrusion_set.description += "## Malpedia\n\n"
+            else:
+                intrusion_set.description = "## Malpedia \n\n"
             intrusion_set.description += entry["description"]
         if entry.get("meta") and entry["meta"].get("refs"):
             intrusion_set.description += "\n\n"
             intrusion_set.description += "## Malpedia External references\n\n"
             for ref in entry["meta"]["refs"]:
                 intrusion_set.description += f"* {ref}\n"
-        if entry.get('synonyms'):
-            intrusion_set.aliases = entry['synonyms']
-        
-        intrusion_set.save()
+        if entry.get("meta") and entry["meta"].get("synonyms"):
+            intrusion_set.aliases = entry["meta"]["synonyms"]
+
+        intrusion_set = intrusion_set.save()
+
         tags = []
+
         if intrusion_set.aliases:
             tags += intrusion_set.aliases
         tags.append(intrusion_set.name)
         tags.append(name_actor)
-        intrusion_set.tag(tags)
+        try:
+            intrusion_set.tag(tags)
+        except Exception as e:
+            logging.error(f"Error tagging actor {name_actor}: {e}")
+
 
 taskmanager.TaskManager.register_task(Malpedia_Actors)
