ec8571f4dbf027eab8aa14bbcfa4da718477d335
Sebastien Larinier <sebdraven@protonmail.com>
2024-06-14 17:00:14 +0200

    add yaraify

---
+++ plugins/feeds/public/yaraify.py
@@ -0,0 +1,57 @@
+import logging
+from datetime import timedelta
+from io import BytesIO
+from typing import ClassVar, Iterable
+from zipfile import ZipFile
+
+import yara
+import time
+from core import taskmanager
+from core.schemas import indicator, task
+from core.schemas.observable import file, sha1, md5, sha256, ssdeep, imphash, tlsh
+
+
+class Yaraify(task.FeedTask):
+    _defaults = {
+        "frequency": timedelta(days=1),
+        "name": "Yaraify",
+        "description": "This feed contains yara rules",
+        "source": "",
+    }
+
+    _SOURCE_ALL_RULES: ClassVar["str"] = (
+        "https://yaraify.abuse.ch/yarahub/yaraify-rules.zip"
+    )
+    _SOURCE_API: ClassVar["str"] = "https://yaraify-api.abuse.ch/api/v1/"
+
+    def run(self):
+        response = self._make_request(self._SOURCE_ALL_RULES)
+        if not response:
+            return
+        zip_file = BytesIO(response.content)
+        with ZipFile(zip_file) as zfile:
+            for name in zfile.namelist():
+                if name.endswith(".yar"):
+                    self.analyze_entry(zfile.read(name).decode("utf-8"))
+
+    def analyze_entry(self, entry: str):
+        logging.debug(f"Yaraify: {entry}")
+        try:
+            yara_rules = yara.compile(source=entry)
+        except yara.SyntaxError as e:
+            logging.error(f"Error compiling yara rule: {e}")
+            return
+        for r in yara_rules:
+            ind_obj = indicator.Indicator(
+                name=f"{r.identifier}",
+                pattern=entry,
+                type=indicator.IndicatorType.yara,
+                diamond=indicator.DiamondModel.capability,
+            )
+
+            ind_obj.description = f"{r.meta.get('description', 'N/A')}"
+
+            ind_obj.save()
+
+
+taskmanager.TaskManager.register_task(Yaraify)
