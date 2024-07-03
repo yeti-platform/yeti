b1f3f4917f4e89ec9ff1ec281c2226f4b777b493
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-07-03 13:13:38 +0200

    Fix Ruff lint error

--- plugins/feeds/public/viriback_tracker.py
+++ plugins/feeds/public/viriback_tracker.py
@@ -3,6 +3,7 @@ from io import StringIO
 from typing import ClassVar
 
 import pandas as pd
+
 from core import taskmanager
 from core.schemas import task
 from core.schemas.observables import ipv4, url
