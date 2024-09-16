b15222ea84b27aed098ffa258dcfb5227ddcc5d1
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-09-17 00:23:20 +0200

    Add load_observables function and handle guess type

--- core/schemas/__init__.py
+++ core/schemas/__init__.py
@@ -1,25 +1,32 @@
 import importlib
 import inspect
+import logging
 from pathlib import Path
 
 import aenum
 
 from core.schemas import observable
 
-print("Registering observable types")
+logger = logging.getLogger(__name__)
 
-for observable_file in Path(__file__).parent.glob("observables/**/*.py"):
-    if observable_file.stem == "__init__":
-        continue
-    print(f"Registering observable type {observable_file.stem}")
-    if observable_file.parent.stem == "observables":
-        module_name = f"core.schemas.observables.{observable_file.stem}"
-    elif observable_file.parent.stem == "private":
-        module_name = f"core.schemas.observables.private.{observable_file.stem}"
-    aenum.extend_enum(
-        observable.ObservableType, observable_file.stem, observable_file.stem
-    )
-    module = importlib.import_module(module_name)
-    for _, obj in inspect.getmembers(module, inspect.isclass):
-        if issubclass(obj, observable.Observable):
-            observable.TYPE_MAPPING[observable_file.stem] = obj
+logger.info("Registering observable types")
+
+def load_observables():
+    for observable_file in Path(__file__).parent.glob("observables/**/*.py"):
+        if observable_file.stem == "__init__":
+            continue
+        logger.info(f"Registering observable type {observable_file.stem}")
+        if observable_file.parent.stem == "observables":
+            module_name = f"core.schemas.observables.{observable_file.stem}"
+        elif observable_file.parent.stem == "private":
+            module_name = f"core.schemas.observables.private.{observable_file.stem}"
+        aenum.extend_enum(
+            observable.ObservableType, observable_file.stem, observable_file.stem
+        )
+        module = importlib.import_module(module_name)
+        for _, obj in inspect.getmembers(module, inspect.isclass):
+            if issubclass(obj, observable.Observable):
+                observable.TYPE_MAPPING[observable_file.stem] = obj
+    aenum.extend_enum(observable.ObservableType, "guess", "guess")
+
+load_observables()
