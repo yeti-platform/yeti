87f453e6582f0f010708b23d8e94a19e3b9493c2
Max Fusco <max.fusco@datadoghq.com>
2024-09-30 08:06:07 -0400

    Replace `LocalStorageClient.__file_path()` with `LocalStorageClient._file_path()`

--- core/clients/file_storage/classes/local_storage.py
+++ core/clients/file_storage/classes/local_storage.py
@@ -13,17 +13,17 @@ class LocalStorageClient(FileStorageClient):
 
         logging.info(f"Initialized local storage client with path {self.path}")
 
-    def __file_path(self, file_name: str) -> pathlib.Path:
+    def _file_path(self, file_name: str) -> pathlib.Path:
         return self.path.joinpath(file_name)
     
     def file_path(self, file_name: str) -> str:
-        return str(self.__file_path(file_name))
+        return str(self._file_path(file_name))
 
     def get_file(self, file_name: str) -> bytes:
-        return self.__file_path(file_name).read_bytes()        
+        return self._file_path(file_name).read_bytes()        
 
     def put_file(self, file_name: str, contents: bytes) -> None:
-        self.__file_path(file_name).write_bytes(contents)
+        self._file_path(file_name).write_bytes(contents)
 
     def delete_file(self, file_name: str) -> None:
         os.remove(self.file_path(file_name))
