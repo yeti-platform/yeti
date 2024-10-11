85fc4c0a6c6bbf987492f0e0eb09432ddedf7b28
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-10-11 14:40:37 +0200

    Force create producer when connection can't be established

--- core/events/producer.py
+++ core/events/producer.py
@@ -6,21 +6,29 @@ from core.events.message import Message, MessageType
 
 class EventProducer:
     def __init__(self):
-        self.conn = Connection(f"redis://{yeti_config.get('redis', 'host')}/")
-        self.channel = self.conn.channel()
-        self.exchange = Exchange("events", type="direct")
-        self.producer = Producer(
-            exchange=self.exchange,
-            channel=self.channel,
-            routing_key="events",
-            serializer="json",
-        )
-        self.queue = Queue(name="events", exchange=self.exchange, routing_key="events")
-        self.queue.maybe_bind(self.conn)
-        self.queue.declare()
+        try:
+            self.conn = Connection(f"redis://{yeti_config.get('redis', 'host')}/")
+            self.channel = self.conn.channel()
+            self.exchange = Exchange("events", type="direct")
+            self.producer = Producer(
+                exchange=self.exchange,
+                channel=self.channel,
+                routing_key="events",
+                serializer="json",
+            )
+            self.queue = Queue(
+                name="events", exchange=self.exchange, routing_key="events"
+            )
+            self.queue.maybe_bind(self.conn)
+            self.queue.declare()
+        except Exception as e:
+            print(f"Error creating message producer: {e}")
+            self.producer = None
 
     # Message is validated on consumer end
     def publish_event(self, event: str, object_id: str):
+        if not self.producer:
+            return
         message = {
             "type": MessageType.event,
             "data": {"event": event, "object_id": object_id},
@@ -29,6 +37,8 @@ class EventProducer:
         self.publish_log(f"New event published: {event}:{object_id}")
 
     def publish_log(self, log: str | dict):
+        if not self.producer:
+            return
         message = {"type": MessageType.log, "data": {"log": log}}
         self.producer.publish(message)
 
