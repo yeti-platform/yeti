f6b5ea58fb73a369a29e73f6bb11a8c190d2869a
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-10-11 14:30:45 +0200

    Update DomainExtract to acts on new.observables.url events and handles ipv4

--- plugins/inline/domain_extract.py
+++ plugins/inline/domain_extract.py
@@ -9,17 +9,19 @@ class DomainExtract(task.InlineTask):
     _defaults = {
         "name": "DomainExtactInline",
         "description": "Extract domain from new URL observable.",
+        "acts_on": ["new.observables.url"],
     }
 
     def run(self, params: dict) -> None:
-        event = params.get("event", "")
-        if event.startswith("new.observables.url"):
-            _, id = event.split(":", 1)
-            url = observable.Observable.get(id)
+        if url_id := params.get("id", ""):
+            url = observable.Observable.get(url_id)
             logging.info(f"Extracting hostname from: {url.value}")
             o = urlparse(url.value)
-            hostname = observable.Hostname(value=o.hostname).save()
-            url.link_to(hostname, "hostname", "Extracted hostname from URL")
+            if observable.IPv4.is_valid(o.hostname):
+                extracted_obs = observable.IPv4(value=o.hostname).save()
+            else:
+                extracted_obs = observable.Hostname(value=o.hostname).save()
+            url.link_to(extracted_obs, "hostname", "Extracted hostname from URL")
         return
 
 
