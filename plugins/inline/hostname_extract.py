de7ba74e3901ebf42af00c64757269ed6e8f0797
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-10-17 11:19:21 +0200

    Update HostnameExtract to latest messages events

--- plugins/inline/hostname_extract.py
+++ plugins/inline/hostname_extract.py
@@ -1,27 +1,26 @@
-import logging
 from urllib.parse import urlparse
 
 from core import taskmanager
+from core.events.message import Message
 from core.schemas import observable, task
 
 
-class HostnameExtract(task.InlineTask):
+class HostnameExtract(task.EventTask):
     _defaults = {
         "name": "HostnameExtact",
         "description": "Extract hostname (domain or ip) from new URL observable.",
-        "acts_on": ["new.observables.url"],
+        "acts_on": "(new|update):observable:url",
     }
 
-    def run(self, params: dict) -> None:
-        if url_id := params.get("id", ""):
-            url = observable.Url.get(url_id)
-            logging.info(f"Extracting hostname from: {url.value}")
-            o = urlparse(url.value)
-            if observable.IPv4.is_valid(o.hostname):
-                extracted_obs = observable.IPv4(value=o.hostname)
-            else:
-                extracted_obs = observable.Hostname(value=o.hostname)
-            url.link_to(extracted_obs.save(), "hostname", "Extracted hostname from URL")
+    def run(self, message: Message) -> None:
+        url = message.data.yeti_object
+        self.logger.info(f"Extracting hostname from: {url.value}")
+        o = urlparse(url.value)
+        if observable.IPv4.is_valid(o.hostname):
+            extracted_obs = observable.IPv4(value=o.hostname)
+        else:
+            extracted_obs = observable.Hostname(value=o.hostname)
+        url.link_to(extracted_obs.save(), "hostname", "Extracted hostname from URL")
         return
 
 
