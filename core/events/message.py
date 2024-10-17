0a3208aa4b865af2f9e121c01ca686ab46edaa36
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-10-17 11:14:30 +0200

    Refactor messages

--- core/events/message.py
+++ core/events/message.py
@@ -1,25 +1,97 @@
-from enum import IntEnum, auto
+from __future__ import annotations
 
-from pydantic import BaseModel, Field
+from enum import Enum, IntEnum, auto
+from typing import TYPE_CHECKING, Annotated, Any, Literal, Type, Union
+
+from pydantic import BaseModel, Discriminator, Field
+from pydantic import Tag as PydanticTag
+
+if TYPE_CHECKING:
+    from core.schemas import (
+        dfiq,
+        entity,
+        graph,
+        indicator,
+        observable,
+        tag,
+        task,
+        template,
+        user,
+    )
 
 
-class MessageType(IntEnum):
-    event = auto()
-    log = auto()
+class MessageType(str, Enum):
+    event = "event"
+    log = "log"
 
 
-event_pattern = r"^(new|update)\.(observables|entities|indicators|relationship)\.\w+|(new|update)\.(tags|tasks)$"
+class EventMessage(str, Enum):
+    new = "new"
+    update = "update"
+    delete = "delete"
 
 
-class EventData(BaseModel):
-    event: str = Field(pattern=event_pattern)
-    object_id: str
+ObservableObjectTypes = Annotated[
+    "observable.ObservableTypes", Field(discriminator="type")
+]
+TaskObjectTypes = Annotated["task.TaskTypes", Field(discriminator="type")]
+EntityObjectTypes = Annotated["entity.EntityTypes", Field(discriminator="type")]
+IndicatorObjectTypes = Annotated[
+    "indicator.IndicatorTypes", Field(discriminator="type")
+]
+UserTypes = Union["user.UserSensitive", "user.User"]
 
 
-class LogData(BaseModel):
+def yeti_object_discriminator(v):
+    if isinstance(v, dict):
+        return v.get("root_type", None)
+    elif isinstance(v, BaseModel):
+        return getattr(v, "root_type", None)
+    return None
+
+
+YetiObjectTypes = Annotated[
+    Union[
+        Annotated[ObservableObjectTypes, PydanticTag("observable")],
+        Annotated["dfiq.DFIQTypes", PydanticTag("dfiq")],
+        Annotated[TaskObjectTypes, PydanticTag("task")],
+        Annotated[EntityObjectTypes, PydanticTag("entity")],
+        Annotated[IndicatorObjectTypes, PydanticTag("indicator")],
+        Annotated[UserTypes, PydanticTag("user")],
+        Annotated["tag.Tag", PydanticTag("tag")],
+        Annotated["template.Template", PydanticTag("template")],
+        Annotated["graph.Relationship", PydanticTag("relationship")],
+        Annotated["graph.TagRelationship", PydanticTag("tag_relationship")],
+    ],
+    Field(discriminator=Discriminator(yeti_object_discriminator)),
+]
+
+
+class ObjectEvent(BaseModel):
+    type: EventMessage
+    yeti_object: YetiObjectTypes
+
+
+class LinkEvent(BaseModel):
+    type: EventMessage
+    source_object: YetiObjectTypes
+    target_object: YetiObjectTypes
+    relationship: "graph.Relationship"
+
+
+class TagLinkEvent(BaseModel):
+    type: EventMessage
+    tagged_object: YetiObjectTypes
+    tag_object: "tag.Tag"
+
+
+class LogMessage(BaseModel):
     log: str | dict
+
+
+EventMessageTypes = Union[ObjectEvent, LinkEvent, TagLinkEvent]
 
 
 class Message(BaseModel):
     type: MessageType
-    data: LogData | EventData
+    data: LogMessage | EventMessageTypes
