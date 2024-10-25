31758d0602c35d78c2888faeb7abb43add17933d
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-10-25 12:00:45 +0200

    Remove docker_image.py file. Merged in container_image.py

--- core/schemas/observables/docker_image.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal
-
-from core.schemas import observable
-
-
-class DockerImage(observable.Observable):
-    type: Literal[observable.ObservableType.docker_image] = (
-        observable.ObservableType.docker_image
-    )
