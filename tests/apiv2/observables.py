8e33f4f5d59805141934d6f958ada2b694cd1cc0
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-11-07 11:16:10 +0100

    Add <tom_chop.me> in test data cases

--- tests/apiv2/observables.py
+++ tests/apiv2/observables.py
@@ -24,6 +24,7 @@ class ObservableTest(unittest.TestCase):
         ("d41d8cd98f00b204e9800998ecf8427e", "md5"),
         ("da39a3ee5e6b4b0d3255bfef95601890afd80709", "sha1"),
         ("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", "sha256"),
+        ("tom_chop.me", "hostname"),
     ]
 
     OBSERVABLE_TEST_DATA_FILE = "tests/observable_test_data/iocs.txt"
