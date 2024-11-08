b884265847e201f33fffc84efe6df28318968428
Fred Baguelin <frederic.baguelin@datadoghq.com>
2024-11-08 09:22:34 +0100

    Call UploadFile sync read method

--- core/web/apiv2/dfiq.py
+++ core/web/apiv2/dfiq.py
@@ -97,7 +97,7 @@ def config() -> DFIQConfigResponse:
 def from_archive(archive: UploadFile) -> dict[str, int]:
     """Uncompresses a ZIP archive and processes the DFIQ content inside it."""
     tempdir = tempfile.TemporaryDirectory()
-    contents = archive.read()
+    contents = archive.file.read()
     ZipFile(BytesIO(contents)).extractall(path=tempdir.name)
     total_added = dfiq.read_from_data_directory(f"{tempdir.name}/*/*.yaml")
     return {"total_added": total_added}
