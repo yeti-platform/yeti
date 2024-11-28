a03159589504a084c857517a4e0acffcef0b1075
Thomas Chopitea <tomchop@gmail.com>
2024-11-28 10:30:22 +0000

    Test is_valid bit is set appropriately after migration

--- tests/migration.py
+++ tests/migration.py
@@ -29,5 +29,19 @@ class ArangoMigrationTest(unittest.TestCase):
                 "created": "2024-11-14T11:58:49.757379Z",
             }
         )
+        observable_col.insert(
+            {
+                "value": "test.com123",
+                "type": "hostname",
+                "root_type": "observable",
+                "created": "2024-11-14T11:58:49.757379Z",
+            }
+        )
         migration_manager.migrate_to_latest(stop_at=2)
         self.assertEqual(migration_manager.db_version, 2)
+        obs = list(observable_col.all())
+        self.assertEqual(len(obs), 2)
+        self.assertEqual(obs[0]["value"], "test.com")
+        self.assertEqual(obs[0]["is_valid"], True)
+        self.assertEqual(obs[1]["value"], "test.com123")
+        self.assertEqual(obs[1]["is_valid"], False)
