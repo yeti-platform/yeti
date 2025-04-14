bb792c29a0fdf9fb43d5330dc8b5b5838438942a
Thomas Chopitea <tomchop@gmail.com>
2025-04-14 15:43:37 +0000

    Move to uv

--- extras/git/ruff-precommit-check.sh
+++ extras/git/ruff-precommit-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script gives a poor error message due to https://github.com/microsoft/vscode/issues/189924
 # but still blocks the commit from happening, so sorta WAI
-poetry run ruff check . && poetry run ruff format . --check
+uv run ruff check . && uv run ruff format . --check
 
 # Check the exit status of the previous command
 
